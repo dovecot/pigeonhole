2fcb69aaccb191c75c3c05f3460cf55fba47d7c6
Stephan Bosch <stephan@rename-it.nl>
2008-08-12 19:04:37 +0200

    Include: forgot to add new file to the repository.

---
+++ src/lib-sieve/plugins/include/ext-include-limits.h
@@ -0,0 +1,9 @@
+#ifndef __EXT_INCLUDE_LIMITS_H
+#define __EXT_INCLUDE_LIMITS_H
+
+#include "sieve-common.h"
+
+#define EXT_INCLUDE_MAX_NESTING_LEVEL 10
+#define EXT_INCLUDE_MAX_INCLUDES      255
+
+#endif /* __EXT_INCLUDE_LIMITS_H */
