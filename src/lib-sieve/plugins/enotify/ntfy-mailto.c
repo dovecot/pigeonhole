979b0e8e3b653ac888a9f1ee03468787dd55abe3
Stephan Bosch <stephan@rename-it.nl>
2008-11-30 01:30:14 +0100

    Enotify: implemented coarse mailto URI parsing.

--- src/lib-sieve/plugins/enotify/ntfy-mailto.c
+++ src/lib-sieve/plugins/enotify/ntfy-mailto.c
@@ -32,22 +32,125 @@ const struct sieve_enotify_method mailto_notify = {
 };
 
 /*
- * 
+ * Mailto URI parsing
+ */
+ 
+/* FIXME: much of this implementation will be common to other URI schemes. This
+ *        should be merged into a common implementation.
  */
 
+static inline int _decode_hex_digit(char digit)
+{
+	switch ( digit ) {
+	case '0': case '1': case '2': case '3': case '4': 
+	case '5': case '6': case '7': case '8': case '9': 
+		return (int) digit - '0';
+
+	case 'a': case 'b': case 'c': case 'd': case 'e': case 'f':
+		return (int) digit - 'a' + 0x0a;
+		
+	case 'A': case 'B': case 'C': case 'D': case 'E': case 'F':
+		return (int) digit - 'a' + 0x0A;
+	}
+	
+	return -1;
+}
+
+static bool _parse_hex_value(const char **in, char *out)
+{
+	char value;
+		
+	if ( **in == '\0' || (value=_decode_hex_digit(**in)) < 0 )
+		return FALSE;
+	
+	*out = value << 4;
+	(*in)++;
+	
+	if ( **in == '\0' || (value=_decode_hex_digit(**in)) < 0 )
+		return FALSE;	
+
+	*out |= value;
+	(*in)++;
+	return TRUE;	
+}
+
+static bool uri_extract_part
+(const char **uri_p, char delim, string_t *part, const char **error_r)
+{
+	const char *p = *uri_p;
+	
+	while ( *p != '\0' && *p != delim ) {
+		if ( *p == '%' ) {
+			char ch;
+			
+			p++;
+			
+			if ( !_parse_hex_value(&p, &ch) ) {
+				*error_r = "invalid % encoding";
+				return FALSE;
+			}
+			
+			str_append_c(part, ch);
+		} else {
+			str_append_c(part, *p);
+			p++;
+		}
+	}
+	
+	p++;
+	*uri_p = p;
+	return TRUE;
+}
+
 static bool ntfy_mailto_parse_uri
-(const char *uri_body, const char **recipient_r, const char ***headers_r,
+(const char *uri_body, const char ***recipients_r, const char ***headers_r,
 	const char **error_r)
 {
-	if ( recipient_r != NULL )
-		*recipient_r = "stephan@rename-it.nl";
-		
-	if ( headers_r != NULL )
-		*headers_r = NULL;
-	
-	/* Scheme already parsed, starting parse after colon */
+	string_t *to = t_str_new(128);
+	string_t *hfield = t_str_new(128);
 
-	/* First parse e-mail address */
+	const char *p = uri_body;
+	
+	/* 
+   * mailtoURI   = "mailto:" [ to ] [ hfields ]
+   * to          = [ addr-spec *("%2C" addr-spec ) ]
+   * hfields     = "?" hfield *( "&" hfield )
+   * hfield      = hfname "=" hfvalue
+   * hfname      = *qchar
+   * hfvalue     = *qchar
+   * addr-spec   = local-part "@" domain
+   * local-part  = dot-atom / quoted-string
+   * qchar       = unreserved / pct-encoded / some-delims
+   * some-delims = "!" / "$" / "'" / "(" / ")" / "*"
+   *               / "+" / "," / ";" / ":" / "@"
+   *
+	 * Scheme 'mailto:' already parsed, starting parse after colon
+	 */
+
+	/* First extract to-part by searching for '?' and decoding % items
+	 */
+
+	if ( !uri_extract_part(&p, '?', to, error_r) )
+		return FALSE;	
+
+	/* Parse to part */
+	
+	// ....
+	
+	/* Extract hfield items */	
+	
+	while ( *p != '\0' ) {		
+		/* Extract hfield item by searching for '&' and decoding '%' items */
+		if ( !uri_extract_part(&p, '&', hfield, error_r) )
+			return FALSE;		
+			
+		/* Add header to list */
+	
+		// ....
+		
+		/* Reset for next header */
+		str_truncate(hfield, 0);
+	}
 	
 	return TRUE;
 }
