124df19636f591bf4578da190b719892c6413337
Stephan Bosch <stephan@rename-it.nl>
2008-12-28 01:48:32 +0100

    Enotify: added enotify extension to default compile.

--- src/lib-sieve/sieve-extensions.c
+++ src/lib-sieve/sieve-extensions.c
@@ -78,14 +78,13 @@ extern const struct sieve_extension copy_extension;
 extern const struct sieve_extension include_extension;
 extern const struct sieve_extension body_extension;
 extern const struct sieve_extension variables_extension;
+extern const struct sieve_extension enotify_extension;
 
 /*
  * Extensions under development
  */
 
-#ifdef HAVE_SIEVE_ENOTIFY
-extern const struct sieve_extension enotify_extension;
-#endif
+// None
 
 /*
  * List of native extensions
@@ -103,16 +102,13 @@ const struct sieve_extension *sieve_core_extensions[] = {
 	&encoded_character_extension,
 
 	/* Extensions under development */
-#ifdef HAVE_SIEVE_ENOTIFY
-	&enotify_extension,
-#endif
 	
 	/* 'Plugins' */
 	&vacation_extension, &subaddress_extension, 
 	&comparator_i_ascii_numeric_extension, 
 	&relational_extension, &regex_extension, &imapflags_extension,
 	&copy_extension, &include_extension, &body_extension,
-	&variables_extension
+	&variables_extension, &enotify_extension
 };
 
 const unsigned int sieve_core_extensions_count =
