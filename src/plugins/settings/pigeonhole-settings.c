16b506829c179665bf16c6a10fcfe3fde7f8f2af
Stephan Bosch <stephan@rename-it.nl>
2014-10-29 22:00:31 +0100

    Added dummy settings plugin that adds a Pigeonhole version banner to doveconf output.

---
+++ src/plugins/settings/pigeonhole-settings.c
@@ -0,0 +1,12 @@
+/* Copyright (c) 2012-2013 Dovecot Oy, see the included COPYING file */
+
+#include "lib.h"
+#include "pigeonhole-config.h"
+#include "pigeonhole-version.h"
+
+/* This is currently just a dummy plugin that adds a Pigeonhole
+ * version banner the doveconf output.
+ */
+
+const char *pigeonhole_settings_version = DOVECOT_ABI_VERSION;
+const char *pigeonhole_settings_doveconf_banner = "Pigeonhole version "PIGEONHOLE_VERSION_FULL;
