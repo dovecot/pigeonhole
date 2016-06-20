5931b69e13a38aada025311b4a00b77c998fc30c
Stephan Bosch <stephan@rename-it.nl>
2016-06-20 20:53:47 +0200

    managesieve-login: proxy: Did not handle zero return value from managesieve_parser_read_args() correctly.

--- src/managesieve-login/managesieve-proxy.c
+++ src/managesieve-login/managesieve-proxy.c
@@ -142,8 +142,8 @@ static int proxy_input_auth_challenge
 	(void)i_stream_read(input);
 	ret = managesieve_parser_read_args(parser, 1, 0, &args);
 
-	if ( ret >= 1 ) {
-		if ( managesieve_arg_get_string(&args[0], &challenge) ) {
+	if ( ret >= 0 ) {
+		if ( ret > 0 && managesieve_arg_get_string(&args[0], &challenge) ) {
 			*challenge_r = t_strdup(challenge);
 		} else {
 			client_log_err(&client->common, t_strdup_printf("proxy: "
@@ -156,7 +156,7 @@ static int proxy_input_auth_challenge
 		/* Parser needs more data (not possible on mem stream) */
 		i_unreached();
 
-	} else if ( ret < 0 ) {
+	} else {
 		const char *error_str = managesieve_parser_get_error(parser, &fatal);
 		error_str = (error_str != NULL ? error_str : "unknown (bug)" );
 
@@ -264,7 +264,12 @@ static int proxy_input_capability
 	(void)i_stream_read(input);
 	ret = managesieve_parser_read_args(parser, 2, 0, &args);
 
-	if ( ret >= 1 ) {
+	if ( ret == 0 ) {
+		client_log_err(&client->common, t_strdup_printf("proxy: "
+			"Remote returned with invalid capability/greeting line: %s",
+			str_sanitize(line,160)));
+		fatal = TRUE;
+	} else if ( ret > 0 ) {
 		if ( args[0].type == MANAGESIEVE_ARG_ATOM ) {
 			*resp_r = proxy_read_response(args);
 
@@ -314,7 +319,7 @@ static int proxy_input_capability
 		/* Parser needs more data (not possible on mem stream) */
 		i_unreached();
 
-	} else if ( ret < 0 ) {
+	} else {
 		const char *error_str = managesieve_parser_get_error(parser, &fatal);
 		error_str = (error_str != NULL ? error_str : "unknown (bug)" );
 
