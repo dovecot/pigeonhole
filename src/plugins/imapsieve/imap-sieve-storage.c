53eee4d9644ce3cb2272d318498102093512a9e5
Stephan Bosch <stephan@dovecot.fi>
2016-08-13 17:26:56 +0200

    imapsieve plugin: Fixed potential segfault bug caused by shadowing a parameter with a local variable.
    Renamed the local variable.
    Found by Coverity.

--- src/plugins/imapsieve/imap-sieve-storage.c
+++ src/plugins/imapsieve/imap-sieve-storage.c
@@ -215,7 +215,7 @@ static int imap_sieve_mailbox_get_script_real
 	   server METADATA */
 	} else {
 		struct mail_namespace *ns;
-		struct mailbox *box;
+		struct mailbox *inbox;
 
 		imap_sieve_mailbox_debug(box,
 			"Mailbox attribute /shared/"
@@ -223,13 +223,13 @@ static int imap_sieve_mailbox_get_script_real
 			"not found");
 
 		ns = mail_namespace_find_inbox(user->namespaces);
-		box = mailbox_alloc(ns->list, "INBOX",
+		inbox = mailbox_alloc(ns->list, "INBOX",
 			MAILBOX_FLAG_READONLY);
-		ret = mailbox_attribute_get(box,
+		ret = mailbox_attribute_get(inbox,
 			MAIL_ATTRIBUTE_TYPE_SHARED,
 			MAILBOX_ATTRIBUTE_PREFIX_DOVECOT_PVT_SERVER
 			MAILBOX_ATTRIBUTE_IMAPSIEVE_SCRIPT, &value);
-		mailbox_free(&box);
+		mailbox_free(&inbox);
 
 		if (ret <= 0) {
 			if (ret < 0) {
