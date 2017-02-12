9f7bbf7811fa885d95d793c3ad17056b68bdfc25
Stephan Bosch <stephan.bosch@dovecot.fi>
2017-02-12 14:01:41 +0100

    imapsieve plugin: Make sure "INBOX" is upper case in static mailbox rules.
    Otherwise, the mailbox name would never match, since matching is performed case-sensitively and Dovecot only returns the upper-cased "INBOX".
    This change prevents confusing administrators that for some reason choose to use a different capitalization of that mailbox name.

--- src/plugins/imapsieve/imap-sieve-storage.c
+++ src/plugins/imapsieve/imap-sieve-storage.c
@@ -842,17 +842,23 @@ imap_sieve_mailbox_rules_init(struct mail_user *user)
 			(user, str_c(identifier));
 		if (setval == NULL || *setval == '\0')
 			break;
+		setval = ph_t_str_trim(setval, "\t ");
+		if (strcasecmp(setval, "INBOX") == 0)
+			setval = t_str_ucase(setval);
 
 		mbrule = p_new(user->pool,
 			struct imap_sieve_mailbox_rule, 1);
 		mbrule->index = i;
-		mbrule->mailbox = ph_p_str_trim(user->pool, setval, "\t ");
+		mbrule->mailbox = p_strdup(user->pool, setval);
 
 		str_truncate(identifier, id_len);
 		str_append(identifier, "_from");
 		setval = mail_user_plugin_getenv(user, str_c(identifier));
 		if (setval != NULL && *setval != '\0') {
-			mbrule->from = ph_p_str_trim(user->pool, setval, "\t ");
+			setval = ph_t_str_trim(setval, "\t ");
+			if (strcasecmp(setval, "INBOX") == 0)
+				setval = t_str_ucase(setval);
+			mbrule->from = p_strdup(user->pool, setval);
 			if (strcmp(mbrule->from, "*") == 0)
 				mbrule->from = NULL;
 		}
