f3a9fe01559135c3b6fa34c30d48a2f66673e703
Stephan Bosch <stephan.bosch@dovecot.fi>
2018-03-03 11:42:29 +0100

    lib-sieve: util: mail-raw: Implemented mail_raw_open_stream().
    This allows using a seekable stream as mail directly.

--- src/lib-sieve/util/mail-raw.h
+++ src/lib-sieve/util/mail-raw.h
@@ -18,6 +18,8 @@ struct mail_raw {
 struct mail_user *mail_raw_user_create
 	(struct master_service *service, struct mail_user *mail_user);
 
+struct mail_raw *mail_raw_open_stream
+	(struct mail_user *ruser, struct istream *input);
 struct mail_raw *mail_raw_open_file
 	(struct mail_user *ruser, const char *path);
 struct mail_raw *mail_raw_open_data
