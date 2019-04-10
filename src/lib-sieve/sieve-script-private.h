12514b424b700a42a2306efc10959a35f6c8fa40
Stephan Bosch <stephan.bosch@dovecot.fi>
2019-04-10 20:40:57 +0200

    lib-sieve: Reformat sieve-script-private.h.

--- src/lib-sieve/sieve-script-private.h
+++ src/lib-sieve/sieve-script-private.h
@@ -11,44 +11,40 @@
 struct sieve_script_vfuncs {
 	void (*destroy)(struct sieve_script *script);
 
-	int (*open)
-		(struct sieve_script *script, enum sieve_error *error_r);
+	int (*open)(struct sieve_script *script, enum sieve_error *error_r);
 
-	int (*get_stream)
-		(struct sieve_script *script, struct istream **stream_r,
-			enum sieve_error *error_r);
-	
+	int (*get_stream)(struct sieve_script *script,
+			  struct istream **stream_r, enum sieve_error *error_r);
+
 	/* binary */
-	int (*binary_read_metadata)
-		(struct sieve_script *_script, struct sieve_binary_block *sblock,
-			sieve_size_t *offset);
-	void (*binary_write_metadata)
-		(struct sieve_script *script, struct sieve_binary_block *sblock);
-	bool (*binary_dump_metadata)
-		(struct sieve_script *script, struct sieve_dumptime_env *denv,
-			struct sieve_binary_block *sblock, sieve_size_t *offset);
-	struct sieve_binary *(*binary_load)
-		(struct sieve_script *script, enum sieve_error *error_r);
-	int (*binary_save)
-		(struct sieve_script *script, struct sieve_binary *sbin,
-			bool update, enum sieve_error *error_r);
-	const char *(*binary_get_prefix)
-		(struct sieve_script *script);
+	int (*binary_read_metadata)(struct sieve_script *_script,
+				    struct sieve_binary_block *sblock,
+				    sieve_size_t *offset);
+	void (*binary_write_metadata)(struct sieve_script *script,
+				      struct sieve_binary_block *sblock);
+	bool (*binary_dump_metadata)(struct sieve_script *script,
+				     struct sieve_dumptime_env *denv,
+				     struct sieve_binary_block *sblock,
+				     sieve_size_t *offset);
+	struct sieve_binary *(*binary_load)(struct sieve_script *script,
+					    enum sieve_error *error_r);
+	int (*binary_save)(struct sieve_script *script,
+			   struct sieve_binary *sbin, bool update,
+			   enum sieve_error *error_r);
+	const char *(*binary_get_prefix)(struct sieve_script *script);
 
 	/* management */
-	int (*rename)
-		(struct sieve_script *script, const char *newname);
+	int (*rename)(struct sieve_script *script, const char *newname);
 	int (*delete)(struct sieve_script *script);
 	int (*is_active)(struct sieve_script *script);
 	int (*activate)(struct sieve_script *script);
 
 	/* properties */
-	int (*get_size)
-		(const struct sieve_script *script, uoff_t *size_r);
+	int (*get_size)(const struct sieve_script *script, uoff_t *size_r);
 
 	/* matching */
-	bool (*equals)
-		(const struct sieve_script *script, const struct sieve_script *other);
+	bool (*equals)(const struct sieve_script *script,
+		       const struct sieve_script *other);
 };
 
 struct sieve_script {
@@ -69,10 +65,10 @@ struct sieve_script {
 	bool open:1;
 };
 
-void sieve_script_init
-(struct sieve_script *script, struct sieve_storage *storage,
-	const struct sieve_script *script_class, const char *location,
-	const char *name);
+void sieve_script_init(struct sieve_script *script,
+		       struct sieve_storage *storage,
+		       const struct sieve_script *script_class,
+		       const char *location, const char *name);
 
 /*
  * Built-in script drivers
@@ -87,33 +83,26 @@ extern const struct sieve_script sieve_ldap_script;
  * Error handling
  */
 
-void sieve_script_set_error
-	(struct sieve_script *script, enum sieve_error error,
-		const char *fmt, ...) ATTR_FORMAT(3, 4);
-void sieve_script_set_internal_error
-	(struct sieve_script *script);
-void sieve_script_set_critical
-	(struct sieve_script *script, const char *fmt, ...)
-		ATTR_FORMAT(2, 3);
+void sieve_script_set_error(struct sieve_script *script, enum sieve_error error,
+			    const char *fmt, ...) ATTR_FORMAT(3, 4);
+void sieve_script_set_internal_error(struct sieve_script *script);
+void sieve_script_set_critical(struct sieve_script *script,
+			       const char *fmt, ...) ATTR_FORMAT(2, 3);
 
-void sieve_script_sys_error
-	(struct sieve_script *script, const char *fmt, ...)
-		ATTR_FORMAT(2, 3);
-void sieve_script_sys_warning
-	(struct sieve_script *script, const char *fmt, ...)
-		ATTR_FORMAT(2, 3);
-void sieve_script_sys_info
-	(struct sieve_script *script, const char *fmt, ...)
-		ATTR_FORMAT(2, 3);
-void sieve_script_sys_debug
-	(struct sieve_script *script, const char *fmt, ...)
-		ATTR_FORMAT(2, 3);
+void sieve_script_sys_error(struct sieve_script *script, const char *fmt, ...)
+			    ATTR_FORMAT(2, 3);
+void sieve_script_sys_warning(struct sieve_script *script, const char *fmt, ...)
+			      ATTR_FORMAT(2, 3);
+void sieve_script_sys_info(struct sieve_script *script, const char *fmt, ...)
+			   ATTR_FORMAT(2, 3);
+void sieve_script_sys_debug(struct sieve_script *script, const char *fmt, ...)
+			    ATTR_FORMAT(2, 3);
 
 /*
  * Script sequence
  */
 
-void sieve_script_sequence_init
-(struct sieve_script_sequence *seq, struct sieve_storage *storage);
+void sieve_script_sequence_init(struct sieve_script_sequence *seq,
+				struct sieve_storage *storage);
 
 #endif
