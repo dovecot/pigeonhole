b79f8fb47b27ab89676dfbfb91d991bd23a27273
Stephan Bosch <stephan.bosch@dovecot.fi>
2019-04-24 16:50:03 +0200

    lib-sieve: Reformat sieve-parser.h.

--- src/lib-sieve/sieve-parser.h
+++ src/lib-sieve/sieve-parser.h
@@ -7,9 +7,10 @@
 
 struct sieve_parser;
 
-struct sieve_parser *sieve_parser_create
-	(struct sieve_script *script, struct sieve_error_handler *ehandler,
-		enum sieve_error *error_r);
+struct sieve_parser *
+sieve_parser_create(struct sieve_script *script,
+		    struct sieve_error_handler *ehandler,
+		    enum sieve_error *error_r);
 void sieve_parser_free(struct sieve_parser **parser);
 bool sieve_parser_run(struct sieve_parser *parser, struct sieve_ast **ast);
 
