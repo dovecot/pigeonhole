d3a0bda41532cb01f16ca26653a42c0f5a7c32af
Stephan Bosch <stephan.bosch@dovecot.fi>
2019-04-24 18:38:42 +0200

    plugins: lda-sieve: Reformat lda-sieve-log.h.

--- src/plugins/lda-sieve/lda-sieve-log.h
+++ src/plugins/lda-sieve/lda-sieve-log.h
@@ -1,8 +1,8 @@
 #ifndef LDA_SIEVE_LOG_H
 #define LDA_SIEVE_LOG_H
 
-struct sieve_error_handler *lda_sieve_log_ehandler_create
-	(struct sieve_error_handler *parent,
-		struct mail_deliver_context *mdctx);
+struct sieve_error_handler *
+lda_sieve_log_ehandler_create(struct sieve_error_handler *parent,
+			      struct mail_deliver_context *mdctx);
 
 #endif
