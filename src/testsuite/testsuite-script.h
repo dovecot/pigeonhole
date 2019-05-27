75a5b17f36e69933b6060bd6436aadb80a8fd921
Stephan Bosch <stephan.bosch@dovecot.fi>
2019-05-27 21:01:23 +0200

    testsuite: Reformat testsuite-script.h.

--- src/testsuite/testsuite-script.h
+++ src/testsuite/testsuite-script.h
@@ -8,15 +8,15 @@ void testsuite_script_deinit(void);
 
 bool testsuite_script_is_subtest(const struct sieve_runtime_env *renv);
 
-bool testsuite_script_compile
-	(const struct sieve_runtime_env *renv, const char *script);
-bool testsuite_script_run
-	(const struct sieve_runtime_env *renv);
-bool testsuite_script_multiscript
-	(const struct sieve_runtime_env *renv,
-		ARRAY_TYPE (const_string) *scriptfiles);
+bool testsuite_script_compile(const struct sieve_runtime_env *renv,
+			      const char *script);
+bool testsuite_script_run(const struct sieve_runtime_env *renv);
+bool testsuite_script_multiscript(const struct sieve_runtime_env *renv,
+				  ARRAY_TYPE (const_string) *scriptfiles);
 
-struct sieve_binary *testsuite_script_get_binary(const struct sieve_runtime_env *renv);
-void testsuite_script_set_binary(const struct sieve_runtime_env *renv, struct sieve_binary *sbin);
+struct sieve_binary *
+testsuite_script_get_binary(const struct sieve_runtime_env *renv);
+void testsuite_script_set_binary(const struct sieve_runtime_env *renv,
+				 struct sieve_binary *sbin);
 
 #endif
