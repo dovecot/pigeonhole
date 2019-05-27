159e8e9e04741c8cceb7ca614afdfef0155bf374
Stephan Bosch <stephan.bosch@dovecot.fi>
2019-05-27 20:50:04 +0200

    testsuite: Reformat testsuite-result.h.

--- src/testsuite/testsuite-result.h
+++ src/testsuite/testsuite-result.h
@@ -4,8 +4,7 @@
 void testsuite_result_init(void);
 void testsuite_result_deinit(void);
 
-void testsuite_result_reset
-	(const struct sieve_runtime_env *renv);
+void testsuite_result_reset(const struct sieve_runtime_env *renv);
 
 struct sieve_result *testsuite_result_get(void);
 
@@ -13,10 +12,10 @@ struct sieve_result_iterate_context *testsuite_result_iterate_init(void);
 
 bool testsuite_result_execute(const struct sieve_runtime_env *renv);
 
-void testsuite_result_print
-	(const struct sieve_runtime_env *renv ATTR_UNUSED);
+void testsuite_result_print(const struct sieve_runtime_env *renv ATTR_UNUSED);
 
-struct sieve_stringlist *testsuite_result_stringlist_create
-	(const struct sieve_runtime_env *renv, int index);
+struct sieve_stringlist *
+testsuite_result_stringlist_create(const struct sieve_runtime_env *renv,
+				   int index);
 
 #endif
