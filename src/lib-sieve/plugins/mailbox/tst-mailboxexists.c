ca727928f29e6f837fb373d2de29d483ee60ed76
Stephan Bosch <stephan.bosch@dovecot.fi>
2019-05-29 00:54:28 +0200

    lib-sieve: plugins: mailbox: Reformat tst-mailboxexists.c.

--- src/lib-sieve/plugins/mailbox/tst-mailboxexists.c
+++ src/lib-sieve/plugins/mailbox/tst-mailboxexists.c
@@ -26,10 +26,12 @@
  *    mailboxexists <mailbox-names: string-list>
  */
 
-static bool tst_mailboxexists_validate
-	(struct sieve_validator *valdtr, struct sieve_command *tst);
-static bool tst_mailboxexists_generate
-	(const struct sieve_codegen_env *cgenv, struct sieve_command *ctx);
+static bool
+tst_mailboxexists_validate(struct sieve_validator *valdtr,
+			   struct sieve_command *tst);
+static bool
+tst_mailboxexists_generate(const struct sieve_codegen_env *cgenv,
+			   struct sieve_command *ctx);
 
 const struct sieve_command_def mailboxexists_test = {
 	.identifier = "mailboxexists",
@@ -46,10 +48,12 @@ const struct sieve_command_def mailboxexists_test = {
  * Mailboxexists operation
  */
 
-static bool tst_mailboxexists_operation_dump
-	(const struct sieve_dumptime_env *denv, sieve_size_t *address);
-static int tst_mailboxexists_operation_execute
-	(const struct sieve_runtime_env *renv, sieve_size_t *address);
+static bool
+tst_mailboxexists_operation_dump(const struct sieve_dumptime_env *denv,
+				 sieve_size_t *address);
+static int
+tst_mailboxexists_operation_execute(const struct sieve_runtime_env *renv,
+				    sieve_size_t *address);
 
 const struct sieve_operation_def mailboxexists_operation = {
 	.mnemonic = "MAILBOXEXISTS",
@@ -67,40 +71,41 @@ struct _validate_context {
 	struct sieve_command *tst;
 };
 
-static int tst_mailboxexists_mailbox_validate
-(void *context, struct sieve_ast_argument *arg)
+static int
+tst_mailboxexists_mailbox_validate(void *context,
+				   struct sieve_ast_argument *arg)
 {
 	struct _validate_context *valctx =
 		(struct _validate_context *)context;
 
-	if ( sieve_argument_is_string_literal(arg) ) {
+	if (sieve_argument_is_string_literal(arg)) {
 		const char *mailbox = sieve_ast_argument_strc(arg), *error;
 
-		if ( !sieve_mailbox_check_name(mailbox, &error) ) {
-			sieve_argument_validate_warning
-				(valctx->valdtr, arg, "%s test: "
-					"invalid mailbox name `%s' specified: %s",
-					sieve_command_identifier(valctx->tst),
-					str_sanitize(mailbox, 256), error);
+		if (!sieve_mailbox_check_name(mailbox, &error)) {
+			sieve_argument_validate_warning(
+				valctx->valdtr, arg, "%s test: "
+				"invalid mailbox name `%s' specified: %s",
+				sieve_command_identifier(valctx->tst),
+				str_sanitize(mailbox, 256), error);
 		}
 	}
 
 	return 1;
 }
 
-static bool tst_mailboxexists_validate
-(struct sieve_validator *valdtr, struct sieve_command *tst)
+static bool
+tst_mailboxexists_validate(struct sieve_validator *valdtr,
+			   struct sieve_command *tst)
 {
 	struct sieve_ast_argument *arg = tst->first_positional;
 	struct sieve_ast_argument *aarg; 
 	struct _validate_context valctx;
 
-	if ( !sieve_validate_positional_argument
-		(valdtr, tst, arg, "mailbox-names", 1, SAAT_STRING_LIST) ) {
+	if (!sieve_validate_positional_argument(
+		valdtr, tst, arg, "mailbox-names", 1, SAAT_STRING_LIST))
 		return FALSE;
-	}
 
-	if ( !sieve_validator_argument_activate(valdtr, tst, arg, FALSE) )
+	if (!sieve_validator_argument_activate(valdtr, tst, arg, FALSE))
 		return FALSE;
 
 	aarg = arg;
@@ -108,16 +113,18 @@ static bool tst_mailboxexists_validate
 	valctx.valdtr = valdtr;
 	valctx.tst = tst;
 
-	return ( sieve_ast_stringlist_map(&aarg,
-		(void*)&valctx, tst_mailboxexists_mailbox_validate) >= 0 );
+	return (sieve_ast_stringlist_map(
+		&aarg, (void*)&valctx,
+		tst_mailboxexists_mailbox_validate) >= 0);
 }
 
 /*
  * Test generation
  */
 
-static bool tst_mailboxexists_generate
-(const struct sieve_codegen_env *cgenv, struct sieve_command *tst)
+static bool
+tst_mailboxexists_generate(const struct sieve_codegen_env *cgenv,
+			   struct sieve_command *tst)
 {
 	sieve_operation_emit(cgenv->sblock, tst->ext, &mailboxexists_operation);
 
@@ -129,22 +136,23 @@ static bool tst_mailboxexists_generate
  * Code dump
  */
 
-static bool tst_mailboxexists_operation_dump
-(const struct sieve_dumptime_env *denv, sieve_size_t *address)
+static bool
+tst_mailboxexists_operation_dump(const struct sieve_dumptime_env *denv,
+				 sieve_size_t *address)
 {
 	sieve_code_dumpf(denv, "MAILBOXEXISTS");
 	sieve_code_descend(denv);
 
-	return
-		sieve_opr_stringlist_dump(denv, address, "mailbox-names");
+	return sieve_opr_stringlist_dump(denv, address, "mailbox-names");
 }
 
 /*
  * Code execution
  */
 
-static int tst_mailboxexists_operation_execute
-(const struct sieve_runtime_env *renv, sieve_size_t *address)
+static int
+tst_mailboxexists_operation_execute(const struct sieve_runtime_env *renv,
+				    sieve_size_t *address)
 {
 	struct sieve_stringlist *mailbox_names;
 	string_t *mailbox_item;
@@ -157,72 +165,78 @@ static int tst_mailboxexists_operation_execute
 	 */
 
 	/* Read notify uris */
-	if ( (ret=sieve_opr_stringlist_read
-		(renv, address, "mailbox-names", &mailbox_names)) <= 0 )
+	if ((ret = sieve_opr_stringlist_read(renv, address, "mailbox-names",
+					     &mailbox_names)) <= 0)
 		return ret;
 
 	/*
 	 * Perform operation
 	 */
 
-	if ( sieve_runtime_trace_active(renv, SIEVE_TRLVL_TESTS) ) {
+	if (sieve_runtime_trace_active(renv, SIEVE_TRLVL_TESTS)) {
 		sieve_runtime_trace(renv, 0, "mailboxexists test");
 		sieve_runtime_trace_descend(renv);
 
 		trace = sieve_runtime_trace_active(renv, SIEVE_TRLVL_MATCHING);
 	}
 
-	if ( renv->scriptenv->user != NULL ) {
+	if (renv->scriptenv->user != NULL) {
 		int ret;
 
 		mailbox_item = NULL;
-		while ( (ret=sieve_stringlist_next_item(mailbox_names, &mailbox_item)) > 0 )
-			{
+		while ((ret = sieve_stringlist_next_item(mailbox_names,
+							 &mailbox_item)) > 0) {
 			struct mail_namespace *ns;
 			const char *mailbox = str_c(mailbox_item);
 			struct mailbox *box;
 
 			/* Find the namespace */
-			ns = mail_namespace_find(renv->scriptenv->user->namespaces, mailbox);
-			if ( ns == NULL) {
-				if ( trace ) {
-					sieve_runtime_trace(renv, 0, "mailbox `%s' not found",
+			ns = mail_namespace_find(
+				renv->scriptenv->user->namespaces, mailbox);
+			if (ns == NULL) {
+				if (trace) {
+					sieve_runtime_trace(
+						renv, 0,
+						"mailbox `%s' not found",
 						str_sanitize(mailbox, 80));
 				}
-
 				all_exist = FALSE;
 				break;
 			}
 
 			/* Open the box */
 			box = mailbox_alloc(ns->list, mailbox, 0);
-			if ( mailbox_open(box) < 0 ) {
-				if ( trace ) {
-					sieve_runtime_trace(renv, 0, "mailbox `%s' cannot be opened",
+			if (mailbox_open(box) < 0) {
+				if (trace) {
+					sieve_runtime_trace(
+						renv, 0,
+						"mailbox `%s' cannot be opened",
 						str_sanitize(mailbox, 80));
 				}
-
 				all_exist = FALSE;
 				mailbox_free(&box);
 				break;
 			}
 
 			/* Also fail when it is readonly */
-			if ( mailbox_is_readonly(box) ) {
-				if ( trace ) {
-					sieve_runtime_trace(renv, 0, "mailbox `%s' is read-only",
+			if (mailbox_is_readonly(box)) {
+				if (trace) {
+					sieve_runtime_trace(
+						renv, 0,
+						"mailbox `%s' is read-only",
 						str_sanitize(mailbox, 80));
 				}
-
 				all_exist = FALSE;
 				mailbox_free(&box);
 				break;
 			}
 
-			/* FIXME: check acl for 'p' or 'i' ACL permissions as required by RFC */
+			/* FIXME: check acl for 'p' or 'i' ACL permissions as
+			   required by RFC */
 
-			if ( trace ) {
-				sieve_runtime_trace(renv, 0, "mailbox `%s' exists",
+			if (trace) {
+				sieve_runtime_trace(
+					renv, 0, "mailbox `%s' exists",
 					str_sanitize(mailbox, 80));
 			}
 
@@ -230,17 +244,21 @@ static int tst_mailboxexists_operation_execute
 			mailbox_free(&box);
 		}
 
-		if ( ret < 0 ) {
-			sieve_runtime_trace_error(renv, "invalid mailbox name item");
+		if (ret < 0) {
+			sieve_runtime_trace_error(
+				renv, "invalid mailbox name item");
 			return SIEVE_EXEC_BIN_CORRUPT;
 		}
 	}
 
-	if ( trace ) {
-		if ( all_exist )
-			sieve_runtime_trace(renv, 0, "all mailboxes are available");
-		else
-			sieve_runtime_trace(renv, 0, "some mailboxes are unavailable");
+	if (trace) {
+		if (all_exist) {
+			sieve_runtime_trace(renv, 0,
+					    "all mailboxes are available");
+		} else {
+			sieve_runtime_trace(renv, 0,
+					    "some mailboxes are unavailable");
+		}
 	}
 
 	sieve_interpreter_set_test_result(renv->interp, all_exist);
