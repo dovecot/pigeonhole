4135e8f485c79a108fc96c1a644698408961de43
Stephan Bosch <stephan.bosch@dovecot.fi>
2019-05-29 01:14:51 +0200

    lib-sieve: plugins: metadata: Reformat tst-metadata.c.

--- src/lib-sieve/plugins/metadata/tst-metadata.c
+++ src/lib-sieve/plugins/metadata/tst-metadata.c
@@ -31,13 +31,16 @@
 
 /* Forward declarations */
 
-static bool tst_metadata_registered
-	(struct sieve_validator *valdtr, const struct sieve_extension *ext,
-		struct sieve_command_registration *cmd_reg);
-static bool tst_metadata_validate
-	(struct sieve_validator *valdtr, struct sieve_command *tst);
-static bool tst_metadata_generate
-	(const struct sieve_codegen_env *cgenv, struct sieve_command *cmd);
+static bool
+tst_metadata_registered(struct sieve_validator *valdtr,
+			const struct sieve_extension *ext,
+			struct sieve_command_registration *cmd_reg);
+static bool
+tst_metadata_validate(struct sieve_validator *valdtr,
+		      struct sieve_command *tst);
+static bool
+tst_metadata_generate(const struct sieve_codegen_env *cgenv,
+		      struct sieve_command *cmd);
 
 /* Metadata test
  *
@@ -82,10 +85,12 @@ const struct sieve_command_def servermetadata_test = {
  * Opcode definitions
  */
 
-static bool tst_metadata_operation_dump
-	(const struct sieve_dumptime_env *denv, sieve_size_t *address);
-static int tst_metadata_operation_execute
-	(const struct sieve_runtime_env *renv, sieve_size_t *address);
+static bool
+tst_metadata_operation_dump(const struct sieve_dumptime_env *denv,
+			    sieve_size_t *address);
+static int
+tst_metadata_operation_execute(const struct sieve_runtime_env *renv,
+			       sieve_size_t *address);
 
 /* Metadata operation */
 
@@ -111,15 +116,16 @@ const struct sieve_operation_def servermetadata_operation = {
  * Test registration
  */
 
-static bool tst_metadata_registered
-(struct sieve_validator *valdtr,
-	const struct sieve_extension *ext ATTR_UNUSED,
-	struct sieve_command_registration *cmd_reg)
+static bool
+tst_metadata_registered(struct sieve_validator *valdtr,
+			const struct sieve_extension *ext ATTR_UNUSED,
+			struct sieve_command_registration *cmd_reg)
 {
 	/* The order of these is not significant */
-	sieve_comparators_link_tag(valdtr, cmd_reg, SIEVE_MATCH_OPT_COMPARATOR);
-	sieve_match_types_link_tags(valdtr, cmd_reg, SIEVE_MATCH_OPT_MATCH_TYPE);
-
+	sieve_comparators_link_tag(valdtr, cmd_reg,
+				   SIEVE_MATCH_OPT_COMPARATOR);
+	sieve_match_types_link_tags(valdtr, cmd_reg,
+				    SIEVE_MATCH_OPT_MATCH_TYPE);
 	return TRUE;
 }
 
@@ -127,8 +133,8 @@ static bool tst_metadata_registered
  * Test validation
  */
 
-static bool tst_metadata_validate
-(struct sieve_validator *valdtr, struct sieve_command *tst)
+static bool
+tst_metadata_validate(struct sieve_validator *valdtr, struct sieve_command *tst)
 {
 	struct sieve_ast_argument *arg = tst->first_positional;
 	const struct sieve_match_type mcht_default =
@@ -139,91 +145,93 @@ static bool tst_metadata_validate
 	const char *error;
 
 	/* mailbox */
-	if ( sieve_command_is(tst, metadata_test) ) {
-		if ( !sieve_validate_positional_argument
-			(valdtr, tst, arg, "mailbox", arg_index++, SAAT_STRING) ) {
-			return FALSE;
-		}
-
-		if ( !sieve_validator_argument_activate(valdtr, tst, arg, FALSE) )
+	if (sieve_command_is(tst, metadata_test)) {
+		if (!sieve_validate_positional_argument(
+			valdtr, tst, arg, "mailbox", arg_index++,
+			SAAT_STRING))
 			return FALSE;
 
-		/* Check name validity when mailbox argument is not a variable */
-		if ( sieve_argument_is_string_literal(arg) ) {
-			const char *mailbox = sieve_ast_argument_strc(arg), *error;
+		if (!sieve_validator_argument_activate(valdtr, tst, arg, FALSE))
+			return FALSE;
 
-			if ( !sieve_mailbox_check_name(mailbox, &error) ) {
-				sieve_argument_validate_warning
-					(valdtr, arg, "%s test: "
-						"invalid mailbox name `%s' specified: %s",
-						sieve_command_identifier(tst),
-						str_sanitize(mailbox, 256), error);
+		/* Check name validity when mailbox argument is not a variable
+		 */
+		if (sieve_argument_is_string_literal(arg)) {
+			const char *mailbox = sieve_ast_argument_strc(arg);
+			const char *error;
+
+			if (!sieve_mailbox_check_name(mailbox, &error)) {
+				sieve_argument_validate_warning(
+					valdtr, arg, "%s test: "
+					"invalid mailbox name `%s' specified: %s",
+					sieve_command_identifier(tst),
+					str_sanitize(mailbox, 256), error);
 			}
 		}
-
 		arg = sieve_ast_argument_next(arg);
 	}
 
 	/* annotation-name */
-	if ( !sieve_validate_positional_argument
-		(valdtr, tst, arg, "annotation-name", arg_index++, SAAT_STRING) ) {
-		return FALSE;
-	}
-
-	if ( !sieve_validator_argument_activate(valdtr, tst, arg, FALSE) )
+	if (!sieve_validate_positional_argument(valdtr, tst, arg,
+						"annotation-name", arg_index++,
+						SAAT_STRING))
 		return FALSE;
 
-	if ( sieve_argument_is_string_literal(arg) ) {
+	if (!sieve_validator_argument_activate(valdtr, tst, arg, FALSE))
+		return FALSE;
+
+	if (sieve_argument_is_string_literal(arg)) {
 		string_t *aname = sieve_ast_argument_str(arg);
 
-		if ( !imap_metadata_verify_entry_name(str_c(aname), &error) ) {
+		if (!imap_metadata_verify_entry_name(str_c(aname), &error)) {
 			char *lcerror = t_strdup_noconst(error);
+
 			lcerror[0] = i_tolower(lcerror[0]);
-			sieve_argument_validate_warning
-				(valdtr, arg, "%s test: "
-					"specified annotation name `%s' is invalid: %s",
-					sieve_command_identifier(tst),
-					str_sanitize(str_c(aname), 256), lcerror);
+			sieve_argument_validate_warning(
+				valdtr, arg, "%s test: "
+				"specified annotation name `%s' is invalid: %s",
+				sieve_command_identifier(tst),
+				str_sanitize(str_c(aname), 256), lcerror);
 		}
 	}
 
 	arg = sieve_ast_argument_next(arg);
 
 	/* key-list */
-	if ( !sieve_validate_positional_argument
-		(valdtr, tst, arg, "key-list", arg_index++, SAAT_STRING_LIST) ) {
+	if (!sieve_validate_positional_argument(valdtr, tst, arg,
+						"key-list", arg_index++,
+						SAAT_STRING_LIST))
 		return FALSE;
-	}
 
-	if ( !sieve_validator_argument_activate(valdtr, tst, arg, FALSE) )
+	if (!sieve_validator_argument_activate(valdtr, tst, arg, FALSE))
 		return FALSE;
 
 	/* Validate the key argument to a specified match type */
-	return sieve_match_type_validate
-		(valdtr, tst, arg, &mcht_default, &cmp_default);
+	return sieve_match_type_validate(valdtr, tst, arg,
+					 &mcht_default, &cmp_default);
 }
 
 /*
  * Test generation
  */
 
-static bool tst_metadata_generate
-(const struct sieve_codegen_env *cgenv, struct sieve_command *tst)
+static bool
+tst_metadata_generate(const struct sieve_codegen_env *cgenv,
+		      struct sieve_command *tst)
 {
-	if ( sieve_command_is(tst, metadata_test) ) {
-		sieve_operation_emit
-			(cgenv->sblock, tst->ext, &metadata_operation);
-	} else if ( sieve_command_is(tst, servermetadata_test) ) {
-		sieve_operation_emit
-			(cgenv->sblock, tst->ext, &servermetadata_operation);
+	if (sieve_command_is(tst, metadata_test)) {
+		sieve_operation_emit(cgenv->sblock, tst->ext,
+				     &metadata_operation);
+	} else if (sieve_command_is(tst, servermetadata_test)) {
+		sieve_operation_emit(cgenv->sblock, tst->ext,
+				     &servermetadata_operation);
 	} else {
 		i_unreached();
 	}
 
  	/* Generate arguments */
-	if ( !sieve_generate_arguments(cgenv, tst, NULL) )
+	if (!sieve_generate_arguments(cgenv, tst, NULL))
 		return FALSE;
-
 	return TRUE;
 }
 
@@ -231,12 +239,13 @@ static bool tst_metadata_generate
  * Code dump
  */
 
-static bool tst_metadata_operation_dump
-(const struct sieve_dumptime_env *denv, sieve_size_t *address)
+static bool
+tst_metadata_operation_dump(const struct sieve_dumptime_env *denv,
+			    sieve_size_t *address)
 {
 	bool metadata = sieve_operation_is(denv->oprtn, metadata_operation);
 
-	if ( metadata )
+	if (metadata)
 		sieve_code_dumpf(denv, "METADATA");
 	else
 		sieve_code_dumpf(denv, "SERVERMETADATA");
@@ -244,15 +253,13 @@ static bool tst_metadata_operation_dump
 	sieve_code_descend(denv);
 
 	/* Handle any optional arguments */
-	if ( sieve_match_opr_optional_dump(denv, address, NULL) != 0 )
+	if (sieve_match_opr_optional_dump(denv, address, NULL) != 0)
  		return FALSE;
-
-	if ( metadata && !sieve_opr_string_dump(denv, address, "mailbox") )
+	if (metadata && !sieve_opr_string_dump(denv, address, "mailbox"))
 		return FALSE;
 
-	return
-		sieve_opr_string_dump(denv, address, "annotation-name") &&
-		sieve_opr_stringlist_dump(denv, address, "key list");
+	return (sieve_opr_string_dump(denv, address, "annotation-name") &&
+		sieve_opr_stringlist_dump(denv, address, "key list"));
 }
 
 /*
@@ -267,9 +274,10 @@ static inline const char *_lc_error(const char *error)
 	return lcerror;
 }
 
-static int tst_metadata_get_annotation
-(const struct sieve_runtime_env *renv, const char *mailbox,
-	const char *aname, const char **annotation_r)
+static int
+tst_metadata_get_annotation(const struct sieve_runtime_env *renv,
+			    const char *mailbox, const char *aname,
+			    const char **annotation_r)
 {
 	struct mail_user *user = renv->scriptenv->user;
 	struct mailbox *box;
@@ -279,10 +287,10 @@ static int tst_metadata_get_annotation
 
 	*annotation_r = NULL;
 
-	if ( user == NULL )
+	if (user == NULL)
 		return SIEVE_EXEC_OK;
 
-	if ( mailbox != NULL ) {
+	if (mailbox != NULL) {
 		struct mail_namespace *ns;
 		ns = mail_namespace_find(user->namespaces, mailbox);
 		box = mailbox_alloc(ns->list, mailbox, 0);
@@ -298,29 +306,32 @@ static int tst_metadata_get_annotation
 		enum mail_error error_code;
 		const char *error;
 
-		error = imap_metadata_transaction_get_last_error
-			(imtrans, &error_code);
+		error = imap_metadata_transaction_get_last_error(
+			imtrans, &error_code);
 
-		sieve_runtime_error(renv, NULL, "%s test: "
+		sieve_runtime_error(
+			renv, NULL, "%s test: "
 			"failed to retrieve annotation `%s': %s%s",
 			(mailbox != NULL ? "metadata" : "servermetadata"),
 			str_sanitize(aname, 256), _lc_error(error),
-			(error_code == MAIL_ERROR_TEMP ? " (temporary failure)" : ""));
+			(error_code == MAIL_ERROR_TEMP ?
+			 " (temporary failure)" : ""));
 
-		status = ( error_code == MAIL_ERROR_TEMP ?
-			SIEVE_EXEC_TEMP_FAILURE : SIEVE_EXEC_FAILURE );
+		status = (error_code == MAIL_ERROR_TEMP ?
+			  SIEVE_EXEC_TEMP_FAILURE : SIEVE_EXEC_FAILURE);
 
 	} else if (avalue.value != NULL) {
 		*annotation_r = avalue.value;
 	}
 	(void)imap_metadata_transaction_commit(&imtrans, NULL, NULL);
-	if ( box != NULL )
+	if (box != NULL)
 		mailbox_free(&box);
 	return status;
 }
 
-static int tst_metadata_operation_execute
-(const struct sieve_runtime_env *renv, sieve_size_t *address)
+static int
+tst_metadata_operation_execute(const struct sieve_runtime_env *renv,
+			       sieve_size_t *address)
 {
 	bool metadata = sieve_operation_is(renv->oprtn, metadata_operation);
 	struct sieve_match_type mcht =
@@ -337,38 +348,43 @@ static int tst_metadata_operation_execute
 	 */
 
 	/* Handle match-type and comparator operands */
-	if ( sieve_match_opr_optional_read
-		(renv, address, NULL, &ret, &cmp, &mcht) < 0 )
+	if (sieve_match_opr_optional_read(renv, address, NULL,
+					  &ret, &cmp, &mcht) < 0)
 		return ret;
 
 	/* Read mailbox */
-	if ( metadata ) {
-		if ( (ret=sieve_opr_string_read(renv, address, "mailbox", &mailbox)) <= 0 )
+	if (metadata) {
+		if ((ret = sieve_opr_string_read(renv, address, "mailbox",
+						 &mailbox)) <= 0)
 			return ret;
 	}
 
 	/* Read annotation-name */
-	if ( (ret=sieve_opr_string_read
-		(renv, address, "annotation-name", &aname)) <= 0 )
+	if ((ret = sieve_opr_string_read(renv, address, "annotation-name",
+					 &aname)) <= 0)
 		return ret;
 
 	/* Read key-list */
-	if ( (ret=sieve_opr_stringlist_read
-		(renv, address, "key-list", &key_list)) <= 0 )
+	if ((ret = sieve_opr_stringlist_read(renv, address, "key-list",
+					     &key_list)) <= 0)
 		return ret;
 
 	/*
 	 * Perform operation
 	 */
 
-	if ( metadata )
-		sieve_runtime_trace(renv, SIEVE_TRLVL_TESTS, "metadata test");
-	else
-		sieve_runtime_trace(renv, SIEVE_TRLVL_TESTS, "servermetadata test");
+	if (metadata) {
+		sieve_runtime_trace(renv, SIEVE_TRLVL_TESTS,
+				    "metadata test");
+	} else {
+		sieve_runtime_trace(renv, SIEVE_TRLVL_TESTS,
+				    "servermetadata test");
+	}
 	sieve_runtime_trace_descend(renv);
 
-	if ( !imap_metadata_verify_entry_name(str_c(aname), &error) ) {
-		sieve_runtime_warning(renv, NULL, "%s test: "
+	if (!imap_metadata_verify_entry_name(str_c(aname), &error)) {
+		sieve_runtime_warning(
+			renv, NULL, "%s test: "
 			"specified annotation name `%s' is invalid: %s",
 			(metadata ? "metadata" : "servermetadata"),
 			str_sanitize(str_c(aname), 256), _lc_error(error));
@@ -376,37 +392,42 @@ static int tst_metadata_operation_execute
 		return SIEVE_EXEC_OK;
 	}
 
-	if ( metadata ) {
-		if ( !sieve_mailbox_check_name(str_c(mailbox), &error) ) {
-			sieve_runtime_warning(renv, NULL, "metadata test: "
+	if (metadata) {
+		if (!sieve_mailbox_check_name(str_c(mailbox), &error)) {
+			sieve_runtime_warning(
+				renv, NULL, "metadata test: "
 				"invalid mailbox name `%s' specified: %s",
 				str_sanitize(str_c(mailbox), 256), error);
 			sieve_interpreter_set_test_result(renv->interp, FALSE);
 			return SIEVE_EXEC_OK;
 		}
 
-		sieve_runtime_trace(renv, SIEVE_TRLVL_TESTS,
+		sieve_runtime_trace(
+			renv, SIEVE_TRLVL_TESTS,
 			"retrieving annotation `%s' from mailbox `%s'",
 			str_sanitize(str_c(aname), 256),
 			str_sanitize(str_c(mailbox), 80));
 	} else {
-		sieve_runtime_trace(renv, SIEVE_TRLVL_TESTS,
+		sieve_runtime_trace(
+			renv, SIEVE_TRLVL_TESTS,
 			"retrieving server annotation `%s'",
 			str_sanitize(str_c(aname), 256));
 	}
 
 	/* Get annotation */
-	if ( (ret=tst_metadata_get_annotation
-		(renv, (metadata ? str_c(mailbox) : NULL), str_c(aname), &annotation))
-			== SIEVE_EXEC_OK ) {
+	if ((ret = tst_metadata_get_annotation(
+		renv, (metadata ? str_c(mailbox) : NULL),
+		str_c(aname), &annotation)) == SIEVE_EXEC_OK) {
 		/* Perform match */
-		if ( annotation != NULL ) {
+		if (annotation != NULL) {
 			/* Create value stringlist */
-			value_list = sieve_single_stringlist_create_cstr(renv, annotation, FALSE);
+			value_list = sieve_single_stringlist_create_cstr(
+				renv, annotation, FALSE);
 
 			/* Perform match */
-			if ( (match=sieve_match
-				(renv, &mcht, &cmp, value_list, key_list, &ret)) < 0 )
+			if ((match = sieve_match(renv, &mcht, &cmp,
+						 value_list, key_list,
+						 &ret)) < 0)
 				return ret;
 		} else {
 			match = 0;
