e75d0fe3f0f5e6c0b09a3625092c7428d6e2601f
Stephan Bosch <stephan.bosch@open-xchange.com>
2019-10-16 22:59:17 +0200

    managesieve: Reformat managesieve-client.h.

--- src/managesieve/managesieve-client.h
+++ src/managesieve/managesieve-client.h
@@ -21,11 +21,11 @@ struct client_command_context {
 };
 
 struct managesieve_module_register {
-    unsigned int id;
+	unsigned int id;
 };
 
 union managesieve_module_context {
-    struct managesieve_module_register *reg;
+	struct managesieve_module_register *reg;
 };
 extern struct managesieve_module_register managesieve_module_register;
 
@@ -70,8 +70,8 @@ struct client {
 	bool output_pending:1;
 	bool handling_input:1;
 	bool anvil_sent:1;
-	bool input_skip_line:1; /* skip all the data until we've
-					   found a new line */
+	bool input_skip_line:1; /* skip all the data until we've found a new
+	                           line */
 };
 
 extern struct client *managesieve_clients;
@@ -79,10 +79,11 @@ extern unsigned int managesieve_client_count;
 
 /* Create new client with specified input/output handles. socket specifies
    if the handle is a socket. */
-struct client *client_create
-	(int fd_in, int fd_out, const char *session_id, struct mail_user *user,
-		struct mail_storage_service_user *service_user,
-		const struct managesieve_settings *set);
+struct client *
+client_create(int fd_in, int fd_out, const char *session_id,
+	      struct mail_user *user,
+	      struct mail_storage_service_user *service_user,
+	      const struct managesieve_settings *set);
 void client_destroy(struct client *client, const char *reason);
 
 void client_dump_capability(struct client *client);
@@ -95,22 +96,22 @@ void client_disconnect_with_error(struct client *client, const char *msg);
    -1 if error */
 int client_send_line(struct client *client, const char *data);
 
-void client_send_response(struct client *client,
-  const char *oknobye, const char *resp_code, const char *msg);
+void client_send_response(struct client *client, const char *oknobye,
+			  const char *resp_code, const char *msg);
 
 #define client_send_ok(client, msg) \
-  client_send_response(client, "OK", NULL, msg)
+	client_send_response(client, "OK", NULL, msg)
 #define client_send_no(client, msg) \
-  client_send_response(client, "NO", NULL, msg)
+	client_send_response(client, "NO", NULL, msg)
 #define client_send_bye(client, msg) \
-  client_send_response(client, "BYE", NULL, msg)
+	client_send_response(client, "BYE", NULL, msg)
 
 #define client_send_okresp(client, resp_code, msg) \
-  client_send_response(client, "OK", resp_code, msg)
+	client_send_response(client, "OK", resp_code, msg)
 #define client_send_noresp(client, resp_code, msg) \
-  client_send_response(client, "NO", resp_code, msg)
+	client_send_response(client, "NO", resp_code, msg)
 #define client_send_byeresp(cmd, resp_code, msg) \
-  client_send_response(client, "BYE", resp_code, msg)
+	client_send_response(client, "BYE", resp_code, msg)
 
 /* Send BAD command error to client. msg can be NULL. */
 void client_send_command_error(struct client_command_context *cmd,
@@ -118,20 +119,19 @@ void client_send_command_error(struct client_command_context *cmd,
 
 /* Send storage or sieve related errors to the client */
 void client_send_storage_error(struct client *client,
-             struct sieve_storage *storage);
+			       struct sieve_storage *storage);
 
 /* Read a number of arguments. Returns TRUE if everything was read or
    FALSE if either needs more data or error occurred. */
-bool client_read_args
-	(struct client_command_context *cmd, unsigned int count, unsigned int flags,
-		bool no_more, const struct managesieve_arg **args_r);
+bool client_read_args(struct client_command_context *cmd, unsigned int count,
+		      unsigned int flags, bool no_more,
+		      const struct managesieve_arg **args_r);
 /* Reads a number of string arguments. ... is a list of pointers where to
    store the arguments. */
-bool client_read_string_args
-	(struct client_command_context *cmd, bool no_more, unsigned int count, ...);
+bool client_read_string_args(struct client_command_context *cmd, bool no_more,
+			     unsigned int count, ...);
 
-static inline bool client_read_no_args
-(struct client_command_context *cmd)
+static inline bool client_read_no_args(struct client_command_context *cmd)
 {
 	return client_read_args(cmd, 0, 0, TRUE, NULL);
 }
