24bd78e0f5406fbd8053b2367bdc48f356c2a4c6
Stephan Bosch <stephan.bosch@open-xchange.com>
2019-10-20 14:00:59 +0200

    managesieve: Reformat managesieve-quota.h.

--- src/managesieve/managesieve-quota.h
+++ src/managesieve/managesieve-quota.h
@@ -3,9 +3,8 @@
 
 uint64_t managesieve_quota_max_script_size(struct client *client);
 
-bool managesieve_quota_check_validsize
-	(struct client *client, size_t size);
-bool managesieve_quota_check_all
-	(struct client *client, const char *scriptname, size_t size);
+bool managesieve_quota_check_validsize(struct client *client, size_t size);
+bool managesieve_quota_check_all(struct client *client,
+				 const char *scriptname, size_t size);
 
 #endif
