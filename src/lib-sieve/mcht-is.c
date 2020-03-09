43e64a9cf6d508f4e2062f15b8e373d23a3f9558
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-03-09 10:20:45 +0100

    lib-sieve: Reformat mcht-is.c

--- src/lib-sieve/mcht-is.c
+++ src/lib-sieve/mcht-is.c
@@ -16,17 +16,17 @@
  * Forward declarations
  */
 
-static int mcht_is_match_key
-	(struct sieve_match_context *mctx, const char *val, size_t val_size,
-		const char *key, size_t key_size);
+static int
+mcht_is_match_key(struct sieve_match_context *mctx,
+		  const char *val, size_t val_size,
+		  const char *key, size_t key_size);
 
 /*
  * Match-type object
  */
 
 const struct sieve_match_type_def is_match_type = {
-	SIEVE_OBJECT("is",
-		&match_type_operand, SIEVE_MATCH_TYPE_IS),
+	SIEVE_OBJECT("is", &match_type_operand, SIEVE_MATCH_TYPE_IS),
 	.match_key = mcht_is_match_key
 };
 
@@ -34,17 +34,21 @@ const struct sieve_match_type_def is_match_type = {
  * Match-type implementation
  */
 
-static int mcht_is_match_key
-(struct sieve_match_context *mctx ATTR_UNUSED,
-	const char *val, size_t val_size,
-	const char *key, size_t key_size)
+static int
+mcht_is_match_key(struct sieve_match_context *mctx ATTR_UNUSED,
+		  const char *val, size_t val_size,
+		  const char *key, size_t key_size)
 {
-	if ( val_size == 0 )
-		return ( key_size == 0 ? 1 : 0 );
+	if (val_size == 0)
+		return (key_size == 0 ? 1 : 0);
 
-	if ( mctx->comparator->def != NULL && mctx->comparator->def->compare != NULL )
+	if (mctx->comparator->def != NULL &&
+	    mctx->comparator->def->compare != NULL) {
 		return (mctx->comparator->def->compare(mctx->comparator,
-			val, val_size, key, key_size) == 0 ? 1 : 0 );
+						       val, val_size,
+						       key, key_size) == 0 ?
+			1 : 0 );
+	}
 
 	return 0;
 }
