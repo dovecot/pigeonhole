225185039e46985c719d5b29856064f8704d572f
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-03-09 23:23:18 +0100

    lib-sieve: Reformat sieve-address-source.c

--- src/lib-sieve/sieve-address-source.c
+++ src/lib-sieve/sieve-address-source.c
@@ -11,9 +11,8 @@
 
 #include "sieve-address-source.h"
 
-bool sieve_address_source_parse
-(pool_t pool, const char *value,
-	struct sieve_address_source *asrc)
+bool sieve_address_source_parse(pool_t pool, const char *value,
+				struct sieve_address_source *asrc)
 {
 	struct smtp_address *address;
 	const char *error;
@@ -24,21 +23,23 @@ bool sieve_address_source_parse
 	value = t_str_trim(value, "\t ");
 	value = t_str_lcase(value);
 	val_len = strlen(value);
-	if ( val_len > 0 ) {
-		if ( strcmp(value, "default") == 0 ) {
+	if (val_len > 0) {
+		if (strcmp(value, "default") == 0) {
 			asrc->type = SIEVE_ADDRESS_SOURCE_DEFAULT;
-		} else if ( strcmp(value, "sender") == 0 ) {
+		} else if (strcmp(value, "sender") == 0) {
 			asrc->type = SIEVE_ADDRESS_SOURCE_SENDER;
-		} else if ( strcmp(value, "recipient") == 0 ) {
+		} else if (strcmp(value, "recipient") == 0) {
 			asrc->type = SIEVE_ADDRESS_SOURCE_RECIPIENT;
-		} else if ( strcmp(value, "orig_recipient") == 0 ) {
+		} else if (strcmp(value, "orig_recipient") == 0) {
 			asrc->type = SIEVE_ADDRESS_SOURCE_ORIG_RECIPIENT;
-		} else if ( strcmp(value, "user_email") == 0 ) {
+		} else if (strcmp(value, "user_email") == 0) {
 			asrc->type = SIEVE_ADDRESS_SOURCE_USER_EMAIL;
-		} else if ( strcmp(value, "postmaster") == 0 ) {
+		} else if (strcmp(value, "postmaster") == 0) {
 			asrc->type = SIEVE_ADDRESS_SOURCE_POSTMASTER;
-		} else if ( smtp_address_parse_path(pool_datastack_create(), value,
-			SMTP_ADDRESS_PARSE_FLAG_ALLOW_EMPTY, &address, &error) >= 0 ) {
+		} else if (smtp_address_parse_path(
+			pool_datastack_create(), value,
+			SMTP_ADDRESS_PARSE_FLAG_ALLOW_EMPTY,
+			&address, &error) >= 0) {
 			asrc->type = SIEVE_ADDRESS_SOURCE_EXPLICIT;
 			asrc->address = smtp_address_clone(pool, address);
 		} else {
@@ -48,17 +49,17 @@ bool sieve_address_source_parse
 	return TRUE;
 }
 
-bool sieve_address_source_parse_from_setting
-(struct sieve_instance *svinst, pool_t pool,
-	const char *setting, struct sieve_address_source *asrc)
+bool sieve_address_source_parse_from_setting(struct sieve_instance *svinst,
+					     pool_t pool, const char *setting,
+					     struct sieve_address_source *asrc)
 {
 	const char *value;
 
 	value = sieve_setting_get(svinst, setting);
-	if ( value == NULL )
+	if (value == NULL)
 		return FALSE;
 
-	if ( !sieve_address_source_parse(pool, value, asrc) ) {
+	if (!sieve_address_source_parse(pool, value, asrc)) {
 		e_warning(svinst->event, "Invalid value for setting '%s': '%s'",
 			  setting, value);
 		return FALSE;
@@ -66,22 +67,21 @@ bool sieve_address_source_parse_from_setting
 	return TRUE;
 }
 
-int sieve_address_source_get_address
-(struct sieve_address_source *asrc,
-	struct sieve_instance *svinst,
-	const struct sieve_script_env *senv,
-	struct sieve_message_context *msgctx,
-	enum sieve_execute_flags flags,
-	const struct smtp_address **addr_r)
+int sieve_address_source_get_address(struct sieve_address_source *asrc,
+				     struct sieve_instance *svinst,
+				     const struct sieve_script_env *senv,
+				     struct sieve_message_context *msgctx,
+				     enum sieve_execute_flags flags,
+				     const struct smtp_address **addr_r)
 {
 	enum sieve_address_source_type type = asrc->type;
 
-	if ( type == SIEVE_ADDRESS_SOURCE_USER_EMAIL &&
-		svinst->user_email == NULL )
+	if (type == SIEVE_ADDRESS_SOURCE_USER_EMAIL &&
+	    svinst->user_email == NULL)
 		type = SIEVE_ADDRESS_SOURCE_RECIPIENT;
 
-	if ( (flags & SIEVE_EXECUTE_FLAG_NO_ENVELOPE) != 0 ) {
-		switch ( type ) {
+	if ((flags & SIEVE_EXECUTE_FLAG_NO_ENVELOPE) != 0) {
+		switch (type) {
 		case SIEVE_ADDRESS_SOURCE_SENDER:
 		case SIEVE_ADDRESS_SOURCE_RECIPIENT:
 		case SIEVE_ADDRESS_SOURCE_ORIG_RECIPIENT:
@@ -93,7 +93,7 @@ int sieve_address_source_get_address
 		}
 	}
 
-	switch ( type ) {
+	switch (type) {
 	case SIEVE_ADDRESS_SOURCE_SENDER:
 		*addr_r = sieve_message_get_sender(msgctx);
 		return 1;
