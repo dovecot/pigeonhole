1ed36096b6fcae23ad7daee23a04d8b6271191bd
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-03-09 10:03:57 +0100

    lib-sieve: Reformat cmp-i-ascii-casemap.c

--- src/lib-sieve/cmp-i-ascii-casemap.c
+++ src/lib-sieve/cmp-i-ascii-casemap.c
@@ -18,20 +18,22 @@
  * Forward declarations
  */
 
-static int cmp_i_ascii_casemap_compare
-	(const struct sieve_comparator *cmp,
-		const char *val1, size_t val1_size, const char *val2, size_t val2_size);
-static bool cmp_i_ascii_casemap_char_match
-	(const struct sieve_comparator *cmp, const char **val1, const char *val1_end,
-		const char **val2, const char *val2_end);
+static int
+cmp_i_ascii_casemap_compare(const struct sieve_comparator *cmp,
+			    const char *val1, size_t val1_size,
+			    const char *val2, size_t val2_size);
+static bool
+cmp_i_ascii_casemap_char_match(const struct sieve_comparator *cmp,
+			       const char **val1, const char *val1_end,
+			       const char **val2, const char *val2_end);
 
 /*
  * Comparator object
  */
 
 const struct sieve_comparator_def i_ascii_casemap_comparator = {
-	SIEVE_OBJECT("i;ascii-casemap",
-		&comparator_operand, SIEVE_COMPARATOR_I_ASCII_CASEMAP),
+	SIEVE_OBJECT("i;ascii-casemap", &comparator_operand,
+		     SIEVE_COMPARATOR_I_ASCII_CASEMAP),
 	.flags =
 		SIEVE_COMPARATOR_FLAG_ORDERING |
 		SIEVE_COMPARATOR_FLAG_EQUALITY |
@@ -46,38 +48,37 @@ const struct sieve_comparator_def i_ascii_casemap_comparator = {
  * Comparator implementation
  */
 
-static int cmp_i_ascii_casemap_compare(
-	const struct sieve_comparator *cmp ATTR_UNUSED,
-	const char *val1, size_t val1_size, const char *val2, size_t val2_size)
+static int
+cmp_i_ascii_casemap_compare(const struct sieve_comparator *cmp ATTR_UNUSED,
+			    const char *val1, size_t val1_size,
+			    const char *val2, size_t val2_size)
 {
 	size_t size = I_MIN(val1_size, val2_size);
 	int ret = strncasecmp(val1, val2, size);
-	return ret != 0 ? ret : (int)val1_size - (int)val2_size;
+
+	return (ret != 0 ? ret : (int)val1_size - (int)val2_size);
 }
 
-static bool cmp_i_ascii_casemap_char_match
-	(const struct sieve_comparator *cmp ATTR_UNUSED,
-		const char **val, const char *val_end,
-		const char **key, const char *key_end)
+static bool
+cmp_i_ascii_casemap_char_match(const struct sieve_comparator *cmp ATTR_UNUSED,
+			       const char **val, const char *val_end,
+			       const char **key, const char *key_end)
 {
 	const char *val_begin = *val;
 	const char *key_begin = *key;
 
-	while ( i_tolower(**val) == i_tolower(**key) &&
-		*val < val_end && *key < key_end ) {
+	while (i_tolower(**val) == i_tolower(**key) &&
+	       *val < val_end && *key < key_end) {
 		(*val)++;
 		(*key)++;
 	}
 
-	if ( *key < key_end ) {
+	if (*key < key_end) {
 		/* Reset */
 		*val = val_begin;
 		*key = key_begin;
 
 		return FALSE;
 	}
-
 	return TRUE;
 }
-
-
