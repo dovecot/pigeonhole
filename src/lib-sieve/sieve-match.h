65877c0a825f12870acce53d2a73b99ab949b36a
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-03-12 00:49:06 +0100

    lib-sieve: Reformat sieve-match.h

--- src/lib-sieve/sieve-match.h
+++ src/lib-sieve/sieve-match.h
@@ -28,23 +28,21 @@ struct sieve_match_context {
  */
 
 /* Manual value iteration (for when multiple matches are allowed) */
-struct sieve_match_context *sieve_match_begin
-	(const struct sieve_runtime_env *renv,
-		const struct sieve_match_type *mcht,
-		const struct sieve_comparator *cmp);
-int sieve_match_value
-	(struct sieve_match_context *mctx, const char *value, size_t value_size,
-		struct sieve_stringlist *key_list);
+struct sieve_match_context *
+sieve_match_begin(const struct sieve_runtime_env *renv,
+		  const struct sieve_match_type *mcht,
+		  const struct sieve_comparator *cmp);
+int sieve_match_value(struct sieve_match_context *mctx,
+		      const char *value, size_t value_size,
+		      struct sieve_stringlist *key_list);
 int sieve_match_end(struct sieve_match_context **mctx, int *exec_status);
 
 /* Default matching operation */
-int sieve_match
-	(const struct sieve_runtime_env *renv,
+int sieve_match(const struct sieve_runtime_env *renv,
 		const struct sieve_match_type *mcht,
 		const struct sieve_comparator *cmp,
 		struct sieve_stringlist *value_list,
-		struct sieve_stringlist *key_list,
-		int *exec_status);
+		struct sieve_stringlist *key_list, int *exec_status);
 
 /*
  * Read matching operands
@@ -57,12 +55,13 @@ enum sieve_match_opt_operand {
 	SIEVE_MATCH_OPT_LAST
 };
 
-int sieve_match_opr_optional_dump
-	(const struct sieve_dumptime_env *denv, sieve_size_t *address, int *opt_code);
+int sieve_match_opr_optional_dump(const struct sieve_dumptime_env *denv,
+				  sieve_size_t *address, int *opt_code);
 
-int sieve_match_opr_optional_read
-	(const struct sieve_runtime_env *renv, sieve_size_t *address, int *opt_code,
-		int *exec_status, struct sieve_comparator *cmp,
-		struct sieve_match_type *mcht);
+int sieve_match_opr_optional_read(const struct sieve_runtime_env *renv,
+				  sieve_size_t *address, int *opt_code,
+				  int *exec_status,
+				  struct sieve_comparator *cmp,
+				  struct sieve_match_type *mcht);
 
 #endif
