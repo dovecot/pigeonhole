3dab700e5e101620b56a3eea6a566c9ed3d408ba
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-03-11 23:41:23 +0100

    lib-sieve: Reformat sieve-commands.c

--- src/lib-sieve/sieve-commands.c
+++ src/lib-sieve/sieve-commands.c
@@ -22,18 +22,22 @@
 
 /* Forward declarations */
 
-static bool arg_number_generate
-	(const struct sieve_codegen_env *cgenv, struct sieve_ast_argument *arg,
-		struct sieve_command *context);
-static bool arg_string_generate
-	(const struct sieve_codegen_env *cgenv, struct sieve_ast_argument *arg,
-		struct sieve_command *context);
-static bool arg_string_list_validate
-	(struct sieve_validator *valdtr, struct sieve_ast_argument **arg,
-		struct sieve_command *context);
-static bool arg_string_list_generate
-	(const struct sieve_codegen_env *cgenv, struct sieve_ast_argument *arg,
-		struct sieve_command *context);
+static bool
+arg_number_generate(const struct sieve_codegen_env *cgenv,
+		    struct sieve_ast_argument *arg,
+		    struct sieve_command *context);
+static bool
+arg_string_generate(const struct sieve_codegen_env *cgenv,
+		    struct sieve_ast_argument *arg,
+		    struct sieve_command *context);
+static bool
+arg_string_list_validate(struct sieve_validator *valdtr,
+			 struct sieve_ast_argument **arg,
+			 struct sieve_command *context);
+static bool
+arg_string_list_generate(const struct sieve_codegen_env *cgenv,
+			 struct sieve_ast_argument *arg,
+			 struct sieve_command *context);
 
 /* Argument objects */
 
@@ -55,86 +59,85 @@ const struct sieve_argument_def string_list_argument = {
 
 /* Argument implementations */
 
-static bool arg_number_generate
-(const struct sieve_codegen_env *cgenv, struct sieve_ast_argument *arg,
-	struct sieve_command *cmd ATTR_UNUSED)
+static bool
+arg_number_generate(const struct sieve_codegen_env *cgenv,
+		    struct sieve_ast_argument *arg,
+		    struct sieve_command *cmd ATTR_UNUSED)
 {
 	sieve_opr_number_emit(cgenv->sblock, sieve_ast_argument_number(arg));
-
 	return TRUE;
 }
 
-static bool arg_string_generate
-(const struct sieve_codegen_env *cgenv, struct sieve_ast_argument *arg,
-	struct sieve_command *cmd ATTR_UNUSED)
+static bool
+arg_string_generate(const struct sieve_codegen_env *cgenv,
+		    struct sieve_ast_argument *arg,
+		    struct sieve_command *cmd ATTR_UNUSED)
 {
 	sieve_opr_string_emit(cgenv->sblock, sieve_ast_argument_str(arg));
-
 	return TRUE;
 }
 
-static bool arg_string_list_validate
-(struct sieve_validator *valdtr, struct sieve_ast_argument **arg,
-	struct sieve_command *cmd)
+static bool
+arg_string_list_validate(struct sieve_validator *valdtr,
+			 struct sieve_ast_argument **arg,
+			 struct sieve_command *cmd)
 {
 	struct sieve_ast_argument *stritem;
 
 	stritem = sieve_ast_strlist_first(*arg);
-	while ( stritem != NULL ) {
-		if ( !sieve_validator_argument_activate(valdtr, cmd, stritem, FALSE) )
+	while (stritem != NULL) {
+		if (!sieve_validator_argument_activate(valdtr, cmd,
+						       stritem, FALSE))
 			return FALSE;
 
 		stritem = sieve_ast_strlist_next(stritem);
 	}
-
 	return TRUE;
 }
 
-static bool emit_string_list_operand
-(const struct sieve_codegen_env *cgenv, const struct sieve_ast_argument *strlist,
-	struct sieve_command *cmd)
+static bool
+emit_string_list_operand(const struct sieve_codegen_env *cgenv,
+			 const struct sieve_ast_argument *strlist,
+			 struct sieve_command *cmd)
 {
 	void *list_context;
 	struct sieve_ast_argument *stritem;
 
-	sieve_opr_stringlist_emit_start
-		(cgenv->sblock, sieve_ast_strlist_count(strlist), &list_context);
+	sieve_opr_stringlist_emit_start(cgenv->sblock,
+					sieve_ast_strlist_count(strlist),
+					&list_context);
 
 	stritem = sieve_ast_strlist_first(strlist);
-	while ( stritem != NULL ) {
-		if ( !sieve_generate_argument(cgenv, stritem, cmd) )
+	while (stritem != NULL) {
+		if (!sieve_generate_argument(cgenv, stritem, cmd))
 			return FALSE;
 
 		stritem = sieve_ast_strlist_next(stritem);
 	}
 
 	sieve_opr_stringlist_emit_end(cgenv->sblock, list_context);
-
 	return TRUE;
 }
 
-static bool arg_string_list_generate
-(const struct sieve_codegen_env *cgenv, struct sieve_ast_argument *arg,
-	struct sieve_command *cmd)
+static bool
+arg_string_list_generate(const struct sieve_codegen_env *cgenv,
+			 struct sieve_ast_argument *arg,
+			 struct sieve_command *cmd)
 {
-	if ( sieve_ast_argument_type(arg) == SAAT_STRING ) {
-		return ( sieve_generate_argument(cgenv, arg, cmd) );
-
-	} else if ( sieve_ast_argument_type(arg) == SAAT_STRING_LIST ) {
+	if (sieve_ast_argument_type(arg) == SAAT_STRING) {
+		return sieve_generate_argument(cgenv, arg, cmd);
+	} else if (sieve_ast_argument_type(arg) == SAAT_STRING_LIST) {
 		bool result = TRUE;
 
-		if ( sieve_ast_strlist_count(arg) == 1 )
-			return ( sieve_generate_argument
-				(cgenv, sieve_ast_strlist_first(arg), cmd) );
-		else {
-			T_BEGIN {
-				result=emit_string_list_operand(cgenv, arg, cmd);
-			} T_END;
-		}
+		if (sieve_ast_strlist_count(arg) == 1) {
+			return sieve_generate_argument(
+				cgenv, sieve_ast_strlist_first(arg), cmd);
+		} else T_BEGIN {
+			result = emit_string_list_operand(cgenv, arg, cmd);
+		} T_END;
 
 		return result;
 	}
-
 	return FALSE;
 }
 
@@ -150,8 +153,8 @@ struct sieve_arg_catenated_string {
 	struct sieve_ast_arg_list *str_parts;
 };
 
-struct sieve_arg_catenated_string *sieve_arg_catenated_string_create
-(struct sieve_ast_argument *orig_arg)
+struct sieve_arg_catenated_string *
+sieve_arg_catenated_string_create(struct sieve_ast_argument *orig_arg)
 {
 	pool_t pool = sieve_ast_pool(orig_arg->ast);
 	struct sieve_ast_arg_list *arglist;
@@ -161,13 +164,13 @@ struct sieve_arg_catenated_string *sieve_arg_catenated_string_create
 
 	catstr = p_new(pool, struct sieve_arg_catenated_string, 1);
 	catstr->str_parts = arglist;
-	(orig_arg)->argument->data = (void *) catstr;
+	(orig_arg)->argument->data = (void *)catstr;
 
 	return catstr;
 }
 
-void sieve_arg_catenated_string_add_element
-(struct sieve_arg_catenated_string *catstr,
+void sieve_arg_catenated_string_add_element(
+	struct sieve_arg_catenated_string *catstr,
 	struct sieve_ast_argument *element)
 {
 	sieve_ast_arg_list_add(catstr->str_parts, element);
@@ -177,28 +180,28 @@ void sieve_arg_catenated_string_add_element
 #define _cat_string_count(catstr) __AST_LIST_COUNT((catstr)->str_parts)
 #define _cat_string_next(item) __AST_LIST_NEXT(item)
 
-bool sieve_arg_catenated_string_generate
-(const struct sieve_codegen_env *cgenv, struct sieve_ast_argument *arg,
-	struct sieve_command *cmd)
+bool sieve_arg_catenated_string_generate(const struct sieve_codegen_env *cgenv,
+					 struct sieve_ast_argument *arg,
+					 struct sieve_command *cmd)
 {
 	struct sieve_arg_catenated_string *catstr =
-		(struct sieve_arg_catenated_string *) arg->argument->data;
+		(struct sieve_arg_catenated_string *)arg->argument->data;
 	struct sieve_ast_argument *strpart;
 
-	if ( _cat_string_count(catstr) == 1 )
+	if (_cat_string_count(catstr) == 1)
 		sieve_generate_argument(cgenv, _cat_string_first(catstr), cmd);
 	else {
-		sieve_opr_catenated_string_emit(cgenv->sblock, _cat_string_count(catstr));
+		sieve_opr_catenated_string_emit(cgenv->sblock,
+						_cat_string_count(catstr));
 
 		strpart = _cat_string_first(catstr);
-		while ( strpart != NULL ) {
-			if ( !sieve_generate_argument(cgenv, strpart, cmd) )
+		while (strpart != NULL) {
+			if (!sieve_generate_argument(cgenv, strpart, cmd))
 				return FALSE;
 
 			strpart = _cat_string_next(strpart);
 		}
 	}
-
 	return TRUE;
 }
 
@@ -206,9 +209,10 @@ bool sieve_arg_catenated_string_generate
  * Argument creation
  */
 
-struct sieve_argument *sieve_argument_create
-(struct sieve_ast *ast, const struct sieve_argument_def *def,
-	const struct sieve_extension *ext, int id_code)
+struct sieve_argument *
+sieve_argument_create(struct sieve_ast *ast,
+		      const struct sieve_argument_def *def,
+		      const struct sieve_extension *ext, int id_code)
 {
 	struct sieve_argument *arg;
 	pool_t pool;
@@ -246,30 +250,27 @@ const unsigned int sieve_core_commands_count = N_ELEMENTS(sieve_core_commands);
  * Command context
  */
 
-struct sieve_command *sieve_command_prev
-(struct sieve_command *cmd)
+struct sieve_command *sieve_command_prev(struct sieve_command *cmd)
 {
 	struct sieve_ast_node *node = sieve_ast_node_prev(cmd->ast_node);
 
-	if ( node != NULL ) {
+	if (node != NULL)
 		return node->command;
-	}
-
 	return NULL;
 }
 
-struct sieve_command *sieve_command_parent
-(struct sieve_command *cmd)
+struct sieve_command *sieve_command_parent(struct sieve_command *cmd)
 {
 	struct sieve_ast_node *node = sieve_ast_node_parent(cmd->ast_node);
 
-	return ( node != NULL ? node->command : NULL );
+	return (node != NULL ? node->command : NULL);
 }
 
-struct sieve_command *sieve_command_create
-(struct sieve_ast_node *cmd_node, const struct sieve_extension *ext,
-	const struct sieve_command_def *cmd_def,
-	struct sieve_command_registration *cmd_reg)
+struct sieve_command *
+sieve_command_create(struct sieve_ast_node *cmd_node,
+		     const struct sieve_extension *ext,
+		     const struct sieve_command_def *cmd_def,
+		     struct sieve_command_registration *cmd_reg)
 {
 	struct sieve_command *cmd;
 
@@ -285,29 +286,34 @@ struct sieve_command *sieve_command_create
 	return cmd;
 }
 
-const char *sieve_command_def_type_name
-(const struct sieve_command_def *cmd_def)
+const char *sieve_command_def_type_name(const struct sieve_command_def *cmd_def)
 {
-	switch ( cmd_def->type ) {
-	case SCT_NONE: return "command of unspecified type (bug)";
-	case SCT_TEST: return "test";
-	case SCT_COMMAND: return "command";
-	case SCT_HYBRID: return "command or test";
+	switch (cmd_def->type) {
+	case SCT_NONE:
+		return "command of unspecified type (bug)";
+	case SCT_TEST:
+		return "test";
+	case SCT_COMMAND:
+		return "command";
+	case SCT_HYBRID:
+		return "command or test";
 	default:
 		break;
 	}
 	return "??COMMAND-TYPE??";
 }
 
-const char *sieve_command_type_name
-	(const struct sieve_command *cmd)
+const char *sieve_command_type_name(const struct sieve_command *cmd)
 {
-	switch ( cmd->def->type ) {
-	case SCT_NONE: return "command of unspecified type (bug)";
-	case SCT_TEST: return "test";
-	case SCT_COMMAND: return "command";
+	switch (cmd->def->type) {
+	case SCT_NONE:
+		return "command of unspecified type (bug)";
+	case SCT_TEST:
+		return "test";
+	case SCT_COMMAND:
+		return "command";
 	case SCT_HYBRID:
-		if ( cmd->ast_node->type == SAT_TEST )
+		if (cmd->ast_node->type == SAT_TEST)
 			return "test";
 		return "command";
 	default:
@@ -316,57 +322,61 @@ const char *sieve_command_type_name
 	return "??COMMAND-TYPE??";
 }
 
-struct sieve_ast_argument *sieve_command_add_dynamic_tag
-(struct sieve_command *cmd, const struct sieve_extension *ext,
-	const struct sieve_argument_def *tag, int id_code)
+struct sieve_ast_argument *
+sieve_command_add_dynamic_tag(struct sieve_command *cmd,
+			      const struct sieve_extension *ext,
+			      const struct sieve_argument_def *tag,
+			      int id_code)
 {
 	struct sieve_ast_argument *arg;
 
-	if ( cmd->first_positional != NULL )
-		arg = sieve_ast_argument_tag_insert
-			(cmd->first_positional, tag->identifier, cmd->ast_node->source_line);
-	else
-		arg = sieve_ast_argument_tag_create
-			(cmd->ast_node, tag->identifier, cmd->ast_node->source_line);
+	if (cmd->first_positional != NULL) {
+		arg = sieve_ast_argument_tag_insert(cmd->first_positional,
+						    tag->identifier,
+						    cmd->ast_node->source_line);
+	} else {
+		arg = sieve_ast_argument_tag_create(cmd->ast_node,
+						    tag->identifier,
+						    cmd->ast_node->source_line);
+	}
 
-	arg->argument = sieve_argument_create(cmd->ast_node->ast, tag, ext, id_code);
-
+	arg->argument = sieve_argument_create(cmd->ast_node->ast, tag, ext,
+					      id_code);
 	return arg;
 }
 
-struct sieve_ast_argument *sieve_command_find_argument
-(struct sieve_command *cmd, const struct sieve_argument_def *arg_def)
+struct sieve_ast_argument *
+sieve_command_find_argument(struct sieve_command *cmd,
+			    const struct sieve_argument_def *arg_def)
 {
-	struct sieve_ast_argument *arg = sieve_ast_argument_first(cmd->ast_node);
+	struct sieve_ast_argument *arg =
+		sieve_ast_argument_first(cmd->ast_node);
 
 	/* Visit tagged and optional arguments */
-	while ( arg != NULL ) {
-		if ( arg->argument != NULL && arg->argument->def == arg_def )
+	while (arg != NULL) {
+		if (arg->argument != NULL && arg->argument->def == arg_def)
 			return arg;
 
 		arg = sieve_ast_argument_next(arg);
 	}
-
 	return arg;
 }
 
 /* Use this function with caution. The command commits to exiting the block.
- * When it for some reason does not, the interpretation will break later on,
- * because exiting jumps are not generated when they would otherwise be
- * necessary.
+   When it for some reason does not, the interpretation will break later on,
+   because exiting jumps are not generated when they would otherwise be
+   necessary.
  */
-void sieve_command_exit_block_unconditionally
-	(struct sieve_command *cmd)
+void sieve_command_exit_block_unconditionally(struct sieve_command *cmd)
 {
 	struct sieve_command *parent = sieve_command_parent(cmd);
 
 	/* Only the first unconditional exit is of importance */
-	if ( parent != NULL && parent->block_exit_command == NULL )
+	if (parent != NULL && parent->block_exit_command == NULL)
 		parent->block_exit_command = cmd;
 }
 
-bool sieve_command_block_exits_unconditionally
-	(struct sieve_command *cmd)
+bool sieve_command_block_exits_unconditionally(struct sieve_command *cmd)
 {
 	return ( cmd->block_exit_command != NULL );
 }
@@ -377,27 +387,26 @@ bool sieve_command_block_exits_unconditionally
 
 /* NOTE: this may be moved */
 
-static int _verify_header_name_item
-(void *context, struct sieve_ast_argument *header)
+static int
+_verify_header_name_item(void *context, struct sieve_ast_argument *header)
 {
-	struct sieve_validator *valdtr = (struct sieve_validator *) context;
+	struct sieve_validator *valdtr = (struct sieve_validator *)context;
 	string_t *name = sieve_ast_argument_str(header);
 
-	if ( sieve_argument_is_string_literal(header) &&
-		!rfc2822_header_field_name_verify(str_c(name), str_len(name)) ) {
-		sieve_argument_validate_warning
-			(valdtr, header, "specified header field name '%s' is invalid",
-				str_sanitize(str_c(name), 80));
-
+	if (sieve_argument_is_string_literal(header) &&
+	    !rfc2822_header_field_name_verify(str_c(name), str_len(name))) {
+		sieve_argument_validate_warning(
+			valdtr, header,
+			"specified header field name '%s' is invalid",
+			str_sanitize(str_c(name), 80));
 		return 0;
 	}
-
 	return 1;
 }
 
-bool sieve_command_verify_headers_argument
-(struct sieve_validator *valdtr, struct sieve_ast_argument *headers)
+bool sieve_command_verify_headers_argument(struct sieve_validator *valdtr,
+					   struct sieve_ast_argument *headers)
 {
-	return ( sieve_ast_stringlist_map
-		(&headers, (void *) valdtr, _verify_header_name_item) >= 0 );
+	return (sieve_ast_stringlist_map(&headers, (void *)valdtr,
+					 _verify_header_name_item) >= 0);
 }
