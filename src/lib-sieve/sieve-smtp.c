44d2c29e454654bea0cb6e149756b07f708aa032
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-03-12 03:42:18 +0100

    lib-sieve: Reformat sieve-smtp.c

--- src/lib-sieve/sieve-smtp.c
+++ src/lib-sieve/sieve-smtp.c
@@ -8,30 +8,29 @@
 struct sieve_smtp_context {
 	const struct sieve_script_env *senv;
 	void *handle;
-	
+
 	bool sent:1;
 };
 
-bool sieve_smtp_available
-(const struct sieve_script_env *senv)
+bool sieve_smtp_available(const struct sieve_script_env *senv)
 {
-	return ( senv->smtp_start != NULL && senv->smtp_add_rcpt != NULL &&
-		senv->smtp_send != NULL && senv->smtp_finish != NULL );
+	return (senv->smtp_start != NULL && senv->smtp_add_rcpt != NULL &&
+		senv->smtp_send != NULL && senv->smtp_finish != NULL);
 }
 
-struct sieve_smtp_context *sieve_smtp_start
-(const struct sieve_script_env *senv,
-	const struct smtp_address *mail_from)
+struct sieve_smtp_context *
+sieve_smtp_start(const struct sieve_script_env *senv,
+		 const struct smtp_address *mail_from)
 {
 	struct sieve_smtp_context *sctx;
 	void *handle;
 
-	if ( !sieve_smtp_available(senv) )
+	if (!sieve_smtp_available(senv))
 		return NULL;
 
 	handle = senv->smtp_start(senv, mail_from);
 	i_assert( handle != NULL );
-	
+
 	sctx = i_new(struct sieve_smtp_context, 1);
 	sctx->senv = senv;
 	sctx->handle = handle;
@@ -39,16 +38,14 @@ struct sieve_smtp_context *sieve_smtp_start
 	return sctx;
 }
 
-void sieve_smtp_add_rcpt
-(struct sieve_smtp_context *sctx,
-	const struct smtp_address *rcpt_to)
+void sieve_smtp_add_rcpt(struct sieve_smtp_context *sctx,
+			 const struct smtp_address *rcpt_to)
 {
 	i_assert(!sctx->sent);
 	sctx->senv->smtp_add_rcpt(sctx->senv, sctx->handle, rcpt_to);
 }
 
-struct ostream *sieve_smtp_send
-(struct sieve_smtp_context *sctx)
+struct ostream *sieve_smtp_send(struct sieve_smtp_context *sctx)
 {
 	i_assert(!sctx->sent);
 	sctx->sent = TRUE;
@@ -56,11 +53,11 @@ struct ostream *sieve_smtp_send
 	return sctx->senv->smtp_send(sctx->senv, sctx->handle);
 }
 
-struct sieve_smtp_context *sieve_smtp_start_single
-(const struct sieve_script_env *senv,
-	const struct smtp_address *rcpt_to,
-		const struct smtp_address *mail_from,
-	struct ostream **output_r)
+struct sieve_smtp_context *
+sieve_smtp_start_single(const struct sieve_script_env *senv,
+			const struct smtp_address *rcpt_to,
+			const struct smtp_address *mail_from,
+			struct ostream **output_r)
 {
 	struct sieve_smtp_context *sctx;
 
@@ -71,8 +68,7 @@ struct sieve_smtp_context *sieve_smtp_start_single
 	return sctx;
 }
 
-void sieve_smtp_abort
-(struct sieve_smtp_context *sctx)
+void sieve_smtp_abort(struct sieve_smtp_context *sctx)
 {
 	const struct sieve_script_env *senv = sctx->senv;
 	void *handle = sctx->handle;
@@ -82,8 +78,7 @@ void sieve_smtp_abort
 	senv->smtp_abort(senv, handle);
 }
 
-int sieve_smtp_finish
-(struct sieve_smtp_context *sctx, const char **error_r)
+int sieve_smtp_finish(struct sieve_smtp_context *sctx, const char **error_r)
 {
 	const struct sieve_script_env *senv = sctx->senv;
 	void *handle = sctx->handle;
@@ -91,4 +86,3 @@ int sieve_smtp_finish
 	i_free(sctx);
 	return senv->smtp_finish(senv, handle, error_r);
 }
-
