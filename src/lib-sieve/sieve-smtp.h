4bf96aa9286c917cb073d42161c6a6d8ae43181d
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-03-12 03:43:31 +0100

    lib-sieve: Reformat sieve-smtp.h

--- src/lib-sieve/sieve-smtp.h
+++ src/lib-sieve/sieve-smtp.h
@@ -3,29 +3,24 @@
 
 #include "sieve-common.h"
 
-bool sieve_smtp_available
-	(const struct sieve_script_env *senv);
+bool sieve_smtp_available(const struct sieve_script_env *senv);
 
 struct sieve_smtp_context;
 
-struct sieve_smtp_context *sieve_smtp_start
-	(const struct sieve_script_env *senv,
-		const struct smtp_address *mail_from);
-void sieve_smtp_add_rcpt
-	(struct sieve_smtp_context *sctx,
-		const struct smtp_address *rcpt_to);
-struct ostream *sieve_smtp_send
-	(struct sieve_smtp_context *sctx);
+struct sieve_smtp_context *
+sieve_smtp_start(const struct sieve_script_env *senv,
+		 const struct smtp_address *mail_from);
+void sieve_smtp_add_rcpt(struct sieve_smtp_context *sctx,
+			 const struct smtp_address *rcpt_to);
+struct ostream *sieve_smtp_send(struct sieve_smtp_context *sctx);
 
-struct sieve_smtp_context *sieve_smtp_start_single
-	(const struct sieve_script_env *senv,
-		const struct smtp_address *rcpt_to,
-		const struct smtp_address *mail_from,
-		struct ostream **output_r);
+struct sieve_smtp_context *
+sieve_smtp_start_single(const struct sieve_script_env *senv,
+			const struct smtp_address *rcpt_to,
+			const struct smtp_address *mail_from,
+			struct ostream **output_r);
 
-void sieve_smtp_abort
-	(struct sieve_smtp_context *sctx);
-int sieve_smtp_finish
-	(struct sieve_smtp_context *sctx, const char **error_r);
+void sieve_smtp_abort(struct sieve_smtp_context *sctx);
+int sieve_smtp_finish(struct sieve_smtp_context *sctx, const char **error_r);
 
 #endif
