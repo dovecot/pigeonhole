3d7bf5edc18dac7d30710cfb4943e5a99d92294c
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-03-12 02:31:53 +0100

    lib-sieve: Reformat sieve-objects.c

--- src/lib-sieve/sieve-objects.c
+++ src/lib-sieve/sieve-objects.c
@@ -13,42 +13,41 @@
  * Object coding
  */
 
-void sieve_opr_object_emit
-(struct sieve_binary_block *sblock, const struct sieve_extension *ext,
-	const struct sieve_object_def *obj_def)
+void sieve_opr_object_emit(struct sieve_binary_block *sblock,
+			   const struct sieve_extension *ext,
+			   const struct sieve_object_def *obj_def)
 {
 	struct sieve_extension_objects *objs =
-		(struct sieve_extension_objects *) obj_def->operand->interface;
+		(struct sieve_extension_objects *)obj_def->operand->interface;
 
-	(void) sieve_operand_emit(sblock, ext, obj_def->operand);
-
-	if ( objs->count > 1 ) {
-		(void) sieve_binary_emit_byte(sblock, obj_def->code);
-	}
+	(void)sieve_operand_emit(sblock, ext, obj_def->operand);
+	if (objs->count > 1)
+		(void)sieve_binary_emit_byte(sblock, obj_def->code);
 }
 
-bool sieve_opr_object_read_data
-(struct sieve_binary_block *sblock, const struct sieve_operand *operand,
-	const struct sieve_operand_class *opclass, sieve_size_t *address,
-	struct sieve_object *obj)
+bool sieve_opr_object_read_data(struct sieve_binary_block *sblock,
+				const struct sieve_operand *operand,
+				const struct sieve_operand_class *opclass,
+				sieve_size_t *address, struct sieve_object *obj)
 {
 	const struct sieve_extension_objects *objs;
 	unsigned int obj_code;
 
-	if ( operand == NULL || operand->def->class != opclass )
+	if (operand == NULL || operand->def->class != opclass)
 		return FALSE;
 
 	objs = (struct sieve_extension_objects *) operand->def->interface;
-	if ( objs == NULL )
+	if (objs == NULL)
 		return FALSE;
 
-	if ( objs->count > 1 ) {
-		if ( !sieve_binary_read_byte(sblock, address, &obj_code) )
+	if (objs->count > 1) {
+		if (!sieve_binary_read_byte(sblock, address, &obj_code))
 			return FALSE;
 
-		if ( obj_code < objs->count ) {
+		if (obj_code < objs->count) {
 			const struct sieve_object_def *const *objects =
-				(const struct sieve_object_def *const *) objs->objects;
+				(const struct sieve_object_def *const *)
+					objs->objects;
 
 			obj->def = objects[obj_code];
 			obj->ext = operand->ext;
@@ -56,55 +55,49 @@ bool sieve_opr_object_read_data
 		}
 	}
 
-	obj->def = (const struct sieve_object_def *) objs->objects;
+	obj->def = (const struct sieve_object_def *)objs->objects;
 	obj->ext = operand->ext;
 	return TRUE;
 }
 
-bool sieve_opr_object_read
-(const struct sieve_runtime_env *renv,
-	const struct sieve_operand_class *opclass, sieve_size_t *address,
-	struct sieve_object *obj)
+bool sieve_opr_object_read(const struct sieve_runtime_env *renv,
+			   const struct sieve_operand_class *opclass,
+			   sieve_size_t *address, struct sieve_object *obj)
 {
 	struct sieve_operand operand;
 
-	if ( !sieve_operand_read(renv->sblock, address, NULL, &operand) ) {
+	if (!sieve_operand_read(renv->sblock, address, NULL, &operand))
 		return FALSE;
-	}
 
-	return sieve_opr_object_read_data
-		(renv->sblock, &operand, opclass, address, obj);
+	return sieve_opr_object_read_data(renv->sblock, &operand, opclass,
+					  address, obj);
 }
 
-bool sieve_opr_object_dump
-(const struct sieve_dumptime_env *denv,
-	const struct sieve_operand_class *opclass, sieve_size_t *address,
-	struct sieve_object *obj)
+bool sieve_opr_object_dump(const struct sieve_dumptime_env *denv,
+			   const struct sieve_operand_class *opclass,
+			   sieve_size_t *address, struct sieve_object *obj)
 {
 	struct sieve_operand operand;
 	struct sieve_object obj_i;
 	const char *class;
 
-	if ( obj == NULL )
+	if (obj == NULL)
 		obj = &obj_i;
 
 	sieve_code_mark(denv);
 
-	if ( !sieve_operand_read(denv->sblock, address, NULL, &operand) ) {
-		return FALSE;
-	}
-
-	if ( !sieve_opr_object_read_data
-		(denv->sblock, &operand, opclass, address, obj) )
+	if (!sieve_operand_read(denv->sblock, address, NULL, &operand))
 		return FALSE;
 
-	if ( operand.def->class == NULL )
+	if (!sieve_opr_object_read_data(denv->sblock, &operand, opclass,
+					address, obj))
+		return FALSE;
+
+	if (operand.def->class == NULL)
 		class = "OBJECT";
 	else
 		class = operand.def->class->name;
 
 	sieve_code_dumpf(denv, "%s: %s", class, obj->def->identifier);
-
 	return TRUE;
 }
-
