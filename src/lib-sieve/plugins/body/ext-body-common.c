73540932588b94092a00d81332938137c35408e8
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-03-13 01:34:20 +0100

    lib-sieve: plugins: body: Reformat ext-body-common.c

--- src/lib-sieve/plugins/body/ext-body-common.c
+++ src/lib-sieve/plugins/body/ext-body-common.c
@@ -22,10 +22,10 @@
  * Body part stringlist
  */
 
-static int ext_body_stringlist_next_item
-	(struct sieve_stringlist *_strlist, string_t **str_r);
-static void ext_body_stringlist_reset
-	(struct sieve_stringlist *_strlist);
+static int
+ext_body_stringlist_next_item(struct sieve_stringlist *_strlist,
+			      string_t **str_r);
+static void ext_body_stringlist_reset(struct sieve_stringlist *_strlist);
 
 struct ext_body_stringlist {
 	struct sieve_stringlist strlist;
@@ -34,31 +34,33 @@ struct ext_body_stringlist {
 	struct sieve_message_part_data *body_parts_iter;
 };
 
-int ext_body_get_part_list
-(const struct sieve_runtime_env *renv, enum tst_body_transform transform,
-	const char * const *content_types, struct sieve_stringlist **strlist_r)
+int ext_body_get_part_list(const struct sieve_runtime_env *renv,
+			   enum tst_body_transform transform,
+			   const char *const *content_types,
+			   struct sieve_stringlist **strlist_r)
 {
-	static const char * const _no_content_types[] = { "", NULL };
+	static const char *const _no_content_types[] = { "", NULL };
 	struct ext_body_stringlist *strlist;
 	struct sieve_message_part_data *body_parts = NULL;
 	int ret;
 
 	*strlist_r = NULL;
 
-	if ( content_types == NULL ) content_types = _no_content_types;
+	if (content_types == NULL)
+		content_types = _no_content_types;
 
-	switch ( transform ) {
+	switch (transform) {
 	case TST_BODY_TRANSFORM_RAW:
-		if ( (ret=sieve_message_body_get_raw(renv, &body_parts)) <= 0 )
+		if ((ret = sieve_message_body_get_raw(renv, &body_parts)) <= 0)
 			return ret;
 		break;
 	case TST_BODY_TRANSFORM_CONTENT:
-		if ( (ret=sieve_message_body_get_content
-			(renv, content_types, &body_parts)) <= 0 )
+		if ((ret = sieve_message_body_get_content(renv, content_types,
+							  &body_parts)) <= 0)
 			return ret;
 		break;
 	case TST_BODY_TRANSFORM_TEXT:
-		if ( (ret=sieve_message_body_get_text(renv, &body_parts)) <= 0 )
+		if ((ret = sieve_message_body_get_text(renv, &body_parts)) <= 0)
 			return ret;
 		break;
 	default:
@@ -76,24 +78,26 @@ int ext_body_get_part_list
 	return SIEVE_EXEC_OK;
 }
 
-static int ext_body_stringlist_next_item
-(struct sieve_stringlist *_strlist, string_t **str_r)
+static int
+ext_body_stringlist_next_item(struct sieve_stringlist *_strlist,
+			      string_t **str_r)
 {
 	struct ext_body_stringlist *strlist =
 		(struct ext_body_stringlist *)_strlist;
 
 	*str_r = NULL;
 
-	if ( strlist->body_parts_iter->content == NULL ) return 0;
+	if (strlist->body_parts_iter->content == NULL)
+		return 0;
 
-	*str_r = t_str_new_const
-		(strlist->body_parts_iter->content, strlist->body_parts_iter->size);
+	*str_r = t_str_new_const(strlist->body_parts_iter->content,
+				 strlist->body_parts_iter->size);
 	strlist->body_parts_iter++;
 	return 1;
 }
 
-static void ext_body_stringlist_reset
-(struct sieve_stringlist *_strlist)
+static void
+ext_body_stringlist_reset(struct sieve_stringlist *_strlist)
 {
 	struct ext_body_stringlist *strlist =
 		(struct ext_body_stringlist *)_strlist;
