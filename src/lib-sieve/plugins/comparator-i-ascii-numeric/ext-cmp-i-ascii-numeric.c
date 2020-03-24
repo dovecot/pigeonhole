f007d249f668bd6bf930fd8d67c84599632039cb
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-03-24 17:09:23 +0100

    lib-sieve: plugins: comparator-i-ascii-numeric: Reformat ext-cmp-i-ascii-numeric.c

--- src/lib-sieve/plugins/comparator-i-ascii-numeric/ext-cmp-i-ascii-numeric.c
+++ src/lib-sieve/plugins/comparator-i-ascii-numeric/ext-cmp-i-ascii-numeric.c
@@ -33,8 +33,9 @@ const struct sieve_comparator_def i_ascii_numeric_comparator;
  * Extension
  */
 
-static bool ext_cmp_i_ascii_numeric_validator_load
-	(const struct sieve_extension *ext, struct sieve_validator *validator);
+static bool
+ext_cmp_i_ascii_numeric_validator_load(const struct sieve_extension *ext,
+				       struct sieve_validator *validator);
 
 const struct sieve_extension_def comparator_i_ascii_numeric_extension = {
 	.name = "comparator-i;ascii-numeric",
@@ -42,8 +43,9 @@ const struct sieve_extension_def comparator_i_ascii_numeric_extension = {
 	SIEVE_EXT_DEFINE_OPERAND(my_comparator_operand)
 };
 
-static bool ext_cmp_i_ascii_numeric_validator_load
-(const struct sieve_extension *ext, struct sieve_validator *validator)
+static bool
+ext_cmp_i_ascii_numeric_validator_load(const struct sieve_extension *ext,
+				       struct sieve_validator *validator)
 {
 	sieve_comparator_register(validator, ext, &i_ascii_numeric_comparator);
 	return TRUE;
@@ -60,7 +62,7 @@ static const struct sieve_operand_def my_comparator_operand = {
 	.name = "comparator-i;ascii-numeric",
 	.ext_def = &comparator_i_ascii_numeric_extension,
 	.class = &sieve_comparator_operand_class,
-	.interface = &ext_comparators
+	.interface = &ext_comparators,
 };
 
 /*
@@ -69,26 +71,27 @@ static const struct sieve_operand_def my_comparator_operand = {
 
 /* Forward declarations */
 
-static int cmp_i_ascii_numeric_compare
-	(const struct sieve_comparator *cmp,
-		const char *val1, size_t val1_size, const char *val2, size_t val2_size);
+static int
+cmp_i_ascii_numeric_compare(const struct sieve_comparator *cmp,
+			    const char *val1, size_t val1_size,
+			    const char *val2, size_t val2_size);
 
 /* Comparator object */
 
 const struct sieve_comparator_def i_ascii_numeric_comparator = {
-	SIEVE_OBJECT("i;ascii-numeric",
-		&my_comparator_operand, 0),
+	SIEVE_OBJECT("i;ascii-numeric", &my_comparator_operand, 0),
 	.flags =
 		SIEVE_COMPARATOR_FLAG_ORDERING |
 		SIEVE_COMPARATOR_FLAG_EQUALITY,
-	.compare = cmp_i_ascii_numeric_compare
+	.compare = cmp_i_ascii_numeric_compare,
 };
 
 /* Comparator implementation */
 
-static int cmp_i_ascii_numeric_compare
-	(const struct sieve_comparator *cmp ATTR_UNUSED,
-		const char *val, size_t val_size, const char *key, size_t key_size)
+static int
+cmp_i_ascii_numeric_compare(const struct sieve_comparator *cmp ATTR_UNUSED,
+			    const char *val, size_t val_size,
+			    const char *key, size_t key_size)
 {
 	const char *vend = val + val_size;
 	const char *kend = key + key_size;
@@ -97,15 +100,15 @@ static int cmp_i_ascii_numeric_compare
 	int digits, i;
 
 	/* RFC 4790: All input is valid; strings that do not start with a digit
-	 * represent positive infinity.
+	   represent positive infinity.
 	 */
-	if ( !i_isdigit(*vp) ) {
-		if ( i_isdigit(*kp) ) {
+	if (!i_isdigit(*vp)) {
+		if (i_isdigit(*kp)) {
 			/* Value is greater */
 			return 1;
 		}
 	} else {
-		if ( !i_isdigit(*kp) ) {
+		if (!i_isdigit(*kp)) {
 			/* Value is less */
 			return -1;
 		}
@@ -113,23 +116,22 @@ static int cmp_i_ascii_numeric_compare
 
 	/* Ignore leading zeros */
 
-	while ( *vp == '0' && vp < vend )
+	while (*vp == '0' && vp < vend)
 		vp++;
-
-	while ( *kp == '0' && kp < kend )
+	while (*kp == '0' && kp < kend)
 		kp++;
 
 	/* Check whether both numbers are equally long in terms of digits */
 
 	digits = 0;
-	while ( vp < vend && kp < kend && i_isdigit(*vp) && i_isdigit(*kp) ) {
+	while (vp < vend && kp < kend && i_isdigit(*vp) && i_isdigit(*kp)) {
 		vp++;
 		kp++;
 		digits++;
 	}
 
-	if ( vp == vend || !i_isdigit(*vp) ) {
-		if ( kp != kend && i_isdigit(*kp) ) {
+	if (vp == vend || !i_isdigit(*vp)) {
+		if (kp != kend && i_isdigit(*kp)) {
 			/* Value is less */
 			return -1;
 		}
@@ -143,10 +145,10 @@ static int cmp_i_ascii_numeric_compare
 	vp -= digits;
 	kp -= digits;
 	i = 0;
-	while ( i < digits ) {
-		if ( *vp > *kp )
+	while (i < digits) {
+		if (*vp > *kp)
 			return 1;
-		else if ( *vp < *kp )
+		else if (*vp < *kp)
 			return -1;
 
 		kp++;
@@ -156,4 +158,3 @@ static int cmp_i_ascii_numeric_compare
 
 	return 0;
 }
-
