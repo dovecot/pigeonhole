92496f8cbf3fd11a5411b6c0af66eb7c72c6c7d0
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-03-24 17:49:55 +0100

    lib-sieve: plugins: date: Reformat ext-date-common.h

--- src/lib-sieve/plugins/date/ext-date-common.h
+++ src/lib-sieve/plugins/date/ext-date-common.h
@@ -11,9 +11,9 @@
 
 extern const struct sieve_extension_def date_extension;
 
-bool ext_date_interpreter_load
-	(const struct sieve_extension *ext, const struct sieve_runtime_env *renv,
-		sieve_size_t *address ATTR_UNUSED);
+bool ext_date_interpreter_load(const struct sieve_extension *ext,
+			       const struct sieve_runtime_env *renv,
+			       sieve_size_t *address ATTR_UNUSED);
 
 /*
  * Tests
@@ -28,7 +28,7 @@ extern const struct sieve_command_def currentdate_test;
 
 enum ext_date_opcode {
 	EXT_DATE_OPERATION_DATE,
-	EXT_DATE_OPERATION_CURRENTDATE
+	EXT_DATE_OPERATION_CURRENTDATE,
 };
 
 extern const struct sieve_operation_def date_operation;
@@ -44,8 +44,8 @@ bool ext_date_parse_timezone(const char *zone, int *zone_offset_r);
  * Current date
  */
 
-time_t ext_date_get_current_date
-	(const struct sieve_runtime_env *renv, int *zone_offset_r);
+time_t ext_date_get_current_date(const struct sieve_runtime_env *renv,
+				 int *zone_offset_r);
 
 /*
  * Date part
@@ -59,8 +59,8 @@ struct ext_date_part {
 
 const struct ext_date_part *ext_date_part_find(const char *part);
 
-const char *ext_date_part_extract
-	(const struct ext_date_part *dpart, struct tm *tm, int zone_offset);
+const char *ext_date_part_extract(const struct ext_date_part *dpart,
+				  struct tm *tm, int zone_offset);
 
 /*
  * Date stringlist
@@ -71,10 +71,9 @@ enum ext_date_timezone_special {
 	EXT_DATE_TIMEZONE_ORIGINAL = 101
 };
 
-struct sieve_stringlist *ext_date_stringlist_create
-(const struct sieve_runtime_env *renv, struct sieve_stringlist *field_values,
-	int time_zone, const struct ext_date_part *dpart);
-
-
+struct sieve_stringlist *
+ext_date_stringlist_create(const struct sieve_runtime_env *renv,
+			   struct sieve_stringlist *field_values,
+			   int time_zone, const struct ext_date_part *dpart);
 
 #endif
