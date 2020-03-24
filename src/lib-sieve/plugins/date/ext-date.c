c54dfd878ae62795c8cbc48443c8278fb0d171d5
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-03-24 17:28:52 +0100

    lib-sieve: plugins: date: Reformat ext-date.c

--- src/lib-sieve/plugins/date/ext-date.c
+++ src/lib-sieve/plugins/date/ext-date.c
@@ -33,29 +33,29 @@
  * Extension
  */
 
-static bool ext_date_validator_load
-(const struct sieve_extension *ext, struct sieve_validator *validator);
+static bool
+ext_date_validator_load(const struct sieve_extension *ext,
+			struct sieve_validator *validator);
 
 const struct sieve_operation_def *ext_date_operations[] = {
 	&date_operation,
-	&currentdate_operation
+	&currentdate_operation,
 };
 
 const struct sieve_extension_def date_extension = {
 	.name = "date",
 	.validator_load = ext_date_validator_load,
 	.interpreter_load = ext_date_interpreter_load,
-	SIEVE_EXT_DEFINE_OPERATIONS(ext_date_operations)
+	SIEVE_EXT_DEFINE_OPERATIONS(ext_date_operations),
 };
 
-static bool ext_date_validator_load
-(const struct sieve_extension *ext, struct sieve_validator *valdtr)
+static bool
+ext_date_validator_load(const struct sieve_extension *ext,
+			struct sieve_validator *valdtr)
 {
-	/* Register new test */
+	/* Register new tests */
 	sieve_validator_register_command(valdtr, ext, &date_test);
 	sieve_validator_register_command(valdtr, ext, &currentdate_test);
 
 	return TRUE;
 }
-
-
