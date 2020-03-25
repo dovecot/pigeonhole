e361c3ed942909928526edf16a35d13bc12ac1b1
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-03-25 20:33:21 +0100

    lib-sieve: plugins: editheader: Reformat ext-editheader.c

--- src/lib-sieve/plugins/editheader/ext-editheader.c
+++ src/lib-sieve/plugins/editheader/ext-editheader.c
@@ -34,26 +34,28 @@
 
 const struct sieve_operation_def *editheader_operations[] = {
 	&addheader_operation,
-	&deleteheader_operation
+	&deleteheader_operation,
 };
 
 /*
  * Extension
  */
 
-static bool ext_editheader_validator_load
-	(const struct sieve_extension *ext, struct sieve_validator *validator);
+static bool
+ext_editheader_validator_load(const struct sieve_extension *ext,
+			      struct sieve_validator *validator);
 
 const struct sieve_extension_def editheader_extension = {
 	.name = "editheader",
 	.load = ext_editheader_load,
 	.unload = ext_editheader_unload,
 	.validator_load = ext_editheader_validator_load,
-	SIEVE_EXT_DEFINE_OPERATIONS(editheader_operations)
+	SIEVE_EXT_DEFINE_OPERATIONS(editheader_operations),
 };
 
-static bool ext_editheader_validator_load
-(const struct sieve_extension *ext, struct sieve_validator *validator)
+static bool
+ext_editheader_validator_load(const struct sieve_extension *ext,
+			      struct sieve_validator *validator)
 {
 	/* Register new commands */
 	sieve_validator_register_command(validator, ext, &addheader_command);
