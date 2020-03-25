4187a868f8d52b01c99c5bac0dd48a85b0ebf531
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-03-25 20:36:15 +0100

    lib-sieve: plugins: editheader: Reformat ext-editheader-common.h.

--- src/lib-sieve/plugins/editheader/ext-editheader-common.h
+++ src/lib-sieve/plugins/editheader/ext-editheader-common.h
@@ -14,7 +14,7 @@ extern const struct sieve_command_def deleteheader_command;
 
 enum ext_imap4flags_opcode {
 	EXT_EDITHEADER_OPERATION_ADDHEADER,
-	EXT_EDITHEADER_OPERATION_DELETEHEADER
+	EXT_EDITHEADER_OPERATION_DELETEHEADER,
 };
 
 extern const struct sieve_operation_def addheader_operation;
@@ -26,24 +26,23 @@ extern const struct sieve_operation_def deleteheader_operation;
 
 extern const struct sieve_extension_def editheader_extension;
 
-bool ext_editheader_load
-	(const struct sieve_extension *ext, void **context);
+bool ext_editheader_load(const struct sieve_extension *ext, void **context);
 void ext_editheader_unload(const struct sieve_extension *ext);
 
 /*
  * Protected headers
  */
 
-bool ext_editheader_header_allow_add
-	(const struct sieve_extension *ext, const char *hname);
-bool ext_editheader_header_allow_delete
-	(const struct sieve_extension *ext, const char *hname);
+bool ext_editheader_header_allow_add(const struct sieve_extension *ext,
+				     const char *hname);
+bool ext_editheader_header_allow_delete(const struct sieve_extension *ext,
+					const char *hname);
 
 /*
  * Limits
  */
 
-bool ext_editheader_header_too_large
-	(const struct sieve_extension *ext, size_t size);
+bool ext_editheader_header_too_large(const struct sieve_extension *ext,
+				     size_t size);
 
 #endif
