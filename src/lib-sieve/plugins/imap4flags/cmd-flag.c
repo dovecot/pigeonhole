f60181401cd03b0c475201aa17b908e94520d205
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-04-05 22:36:09 +0200

    lib-sieve: plugins: imap4flags: Reformat cmd-flag.c

--- src/lib-sieve/plugins/imap4flags/cmd-flag.c
+++ src/lib-sieve/plugins/imap4flags/cmd-flag.c
@@ -18,8 +18,9 @@
 
 /* Forward declarations */
 
-static bool cmd_flag_generate
-	(const struct sieve_codegen_env *cgenv, struct sieve_command *ctx);
+static bool
+cmd_flag_generate(const struct sieve_codegen_env *cgenv,
+		  struct sieve_command *ctx);
 
 /* Setflag command
  *
@@ -35,7 +36,7 @@ const struct sieve_command_def cmd_setflag = {
 	.block_allowed = FALSE,
 	.block_required = FALSE,
 	.validate = ext_imap4flags_command_validate,
-	.generate = cmd_flag_generate
+	.generate = cmd_flag_generate,
 };
 
 /* Addflag command
@@ -52,7 +53,7 @@ const struct sieve_command_def cmd_addflag = {
 	.block_allowed = FALSE,
 	.block_required = FALSE,
 	.validate = ext_imap4flags_command_validate,
-	.generate = cmd_flag_generate
+	.generate = cmd_flag_generate,
 };
 
 
@@ -70,7 +71,7 @@ const struct sieve_command_def cmd_removeflag = {
 	.block_allowed = FALSE,
 	.block_required = FALSE,
 	.validate = ext_imap4flags_command_validate,
-	.generate = cmd_flag_generate
+	.generate = cmd_flag_generate,
 };
 
 /*
@@ -79,10 +80,11 @@ const struct sieve_command_def cmd_removeflag = {
 
 /* Forward declarations */
 
-bool cmd_flag_operation_dump
-	(const struct sieve_dumptime_env *denv, sieve_size_t *address);
-static int cmd_flag_operation_execute
-	(const struct sieve_runtime_env *renv, sieve_size_t *address);
+bool cmd_flag_operation_dump(const struct sieve_dumptime_env *denv,
+			     sieve_size_t *address);
+static int
+cmd_flag_operation_execute(const struct sieve_runtime_env *renv,
+			   sieve_size_t *address);
 
 /* Setflag operation */
 
@@ -91,7 +93,7 @@ const struct sieve_operation_def setflag_operation = {
 	.ext_def = &imap4flags_extension,
 	.code = EXT_IMAP4FLAGS_OPERATION_SETFLAG,
 	.dump = cmd_flag_operation_dump,
-	.execute = cmd_flag_operation_execute
+	.execute = cmd_flag_operation_execute,
 };
 
 /* Addflag operation */
@@ -101,7 +103,7 @@ const struct sieve_operation_def addflag_operation = {
 	.ext_def = &imap4flags_extension,
 	.code = EXT_IMAP4FLAGS_OPERATION_ADDFLAG,
 	.dump = cmd_flag_operation_dump,
-	.execute = cmd_flag_operation_execute
+	.execute = cmd_flag_operation_execute,
 };
 
 /* Removeflag operation */
@@ -111,39 +113,44 @@ const struct sieve_operation_def removeflag_operation = {
 	.ext_def = &imap4flags_extension,
 	.code = EXT_IMAP4FLAGS_OPERATION_REMOVEFLAG,
 	.dump = cmd_flag_operation_dump,
-	.execute = cmd_flag_operation_execute
+	.execute = cmd_flag_operation_execute,
 };
 
 /*
  * Code generation
  */
 
-static bool cmd_flag_generate
-(const struct sieve_codegen_env *cgenv, struct sieve_command *cmd)
+static bool
+cmd_flag_generate(const struct sieve_codegen_env *cgenv,
+		  struct sieve_command *cmd)
 {
 	struct sieve_ast_argument *arg1, *arg2;
 
 	/* Emit operation */
-	if ( sieve_command_is(cmd, cmd_setflag) )
-		sieve_operation_emit(cgenv->sblock, cmd->ext, &setflag_operation);
-	else if ( sieve_command_is(cmd, cmd_addflag) )
-		sieve_operation_emit(cgenv->sblock, cmd->ext, &addflag_operation);
-	else if ( sieve_command_is(cmd, cmd_removeflag) )
-		sieve_operation_emit(cgenv->sblock, cmd->ext, &removeflag_operation);
+	if (sieve_command_is(cmd, cmd_setflag)) {
+		sieve_operation_emit(cgenv->sblock, cmd->ext,
+				     &setflag_operation);
+	} else if (sieve_command_is(cmd, cmd_addflag)) {
+		sieve_operation_emit(cgenv->sblock, cmd->ext,
+				     &addflag_operation);
+	} else if (sieve_command_is(cmd, cmd_removeflag)) {
+		sieve_operation_emit(cgenv->sblock, cmd->ext,
+				     &removeflag_operation);
+	}
 
 	arg1 = cmd->first_positional;
 	arg2 = sieve_ast_argument_next(arg1);
 
-	if ( arg2 == NULL ) {
+	if (arg2 == NULL) {
 		/* No variable */
 		sieve_opr_omitted_emit(cgenv->sblock);
-		if ( !sieve_generate_argument(cgenv, arg1, cmd) )
+		if (!sieve_generate_argument(cgenv, arg1, cmd))
 			return FALSE;
 	} else {
 		/* Full command */
-		if ( !sieve_generate_argument(cgenv, arg1, cmd) )
+		if (!sieve_generate_argument(cgenv, arg1, cmd))
 			return FALSE;
-		if ( !sieve_generate_argument(cgenv, arg2, cmd) )
+		if (!sieve_generate_argument(cgenv, arg2, cmd))
 			return FALSE;
 	}
 	return TRUE;
@@ -153,8 +160,8 @@ static bool cmd_flag_generate
  * Code dump
  */
 
-bool cmd_flag_operation_dump
-(const struct sieve_dumptime_env *denv, sieve_size_t *address)
+bool cmd_flag_operation_dump(const struct sieve_dumptime_env *denv,
+			     sieve_size_t *address)
 {
 	struct sieve_operand oprnd;
 
@@ -162,27 +169,27 @@ bool cmd_flag_operation_dump
 	sieve_code_descend(denv);
 
 	sieve_code_mark(denv);
-	if ( !sieve_operand_read(denv->sblock, address, NULL, &oprnd) ) {
+	if (!sieve_operand_read(denv->sblock, address, NULL, &oprnd)) {
 		sieve_code_dumpf(denv, "ERROR: INVALID OPERAND");
 		return FALSE;
 	}
 
-	if ( !sieve_operand_is_omitted(&oprnd) ) {
-		return
-			sieve_opr_string_dump_data(denv, &oprnd, address, "variable name") &&
-			sieve_opr_stringlist_dump(denv, address, "list of flags");
+	if (!sieve_operand_is_omitted(&oprnd)) {
+		return (sieve_opr_string_dump_data(denv, &oprnd,
+						   address, "variable name") &&
+			sieve_opr_stringlist_dump(denv, address,
+						  "list of flags"));
 	}
-
-	return
-		sieve_opr_stringlist_dump(denv, address, "list of flags");
+	return sieve_opr_stringlist_dump(denv, address, "list of flags");
 }
 
 /*
  * Code execution
  */
 
-static int cmd_flag_operation_execute
-(const struct sieve_runtime_env *renv, sieve_size_t *address)
+static int
+cmd_flag_operation_execute(const struct sieve_runtime_env *renv,
+			   sieve_size_t *address)
 {
 	const struct sieve_operation *op = renv->oprtn;
 	struct sieve_operand oprnd;
@@ -197,20 +204,23 @@ static int cmd_flag_operation_execute
 	 */
 
 	/* Read bare operand (two types possible) */
-	if ( (ret=sieve_operand_runtime_read
-		(renv, address, NULL, &oprnd)) <= 0 )
+	ret = sieve_operand_runtime_read(renv, address, NULL, &oprnd);
+	if (ret <= 0)
 		return ret;
 
 	/* Variable operand (optional) */
-	if ( !sieve_operand_is_omitted(&oprnd) ) {
+	if (!sieve_operand_is_omitted(&oprnd)) {
 		/* Read the variable operand */
-		if ( (ret=sieve_variable_operand_read_data
-			(renv, &oprnd, address, "variable", &storage, &var_index)) <= 0 )
+		ret = sieve_variable_operand_read_data(
+			renv, &oprnd, address, "variable",
+			&storage, &var_index);
+		if (ret <= 0)
 			return ret;
 
 		/* Read flag list */
-		if ( (ret=sieve_opr_stringlist_read(renv, address, "flag-list", &flag_list))
-			<= 0 )
+		ret = sieve_opr_stringlist_read(renv, address, "flag-list",
+						&flag_list);
+		if (ret <= 0)
 			return ret;
 
 	/* Flag-list operand */
@@ -219,8 +229,9 @@ static int cmd_flag_operation_execute
 		var_index = 0;
 
 		/* Read flag list */
-		if ( (ret=sieve_opr_stringlist_read(renv, address,
-			"flag-list", &flag_list)) <= 0 )
+		ret = sieve_opr_stringlist_read(renv, address, "flag-list",
+						&flag_list);
+		if (ret <= 0)
 			return ret;
 	}
 
@@ -230,14 +241,17 @@ static int cmd_flag_operation_execute
 
 	/* Determine what to do */
 
-	if ( sieve_operation_is(op, setflag_operation) ) {
-		sieve_runtime_trace(renv, SIEVE_TRLVL_COMMANDS, "setflag command");
+	if (sieve_operation_is(op, setflag_operation)) {
+		sieve_runtime_trace(renv, SIEVE_TRLVL_COMMANDS,
+				    "setflag command");
 		flag_op = sieve_ext_imap4flags_set_flags;
-	} else if ( sieve_operation_is(op, addflag_operation) ) {
-		sieve_runtime_trace(renv, SIEVE_TRLVL_COMMANDS, "addflag command");
+	} else if (sieve_operation_is(op, addflag_operation)) {
+		sieve_runtime_trace(renv, SIEVE_TRLVL_COMMANDS,
+				    "addflag command");
 		flag_op = sieve_ext_imap4flags_add_flags;
-	} else if ( sieve_operation_is(op, removeflag_operation) ) {
-		sieve_runtime_trace(renv, SIEVE_TRLVL_COMMANDS, "removeflag command");
+	} else if (sieve_operation_is(op, removeflag_operation)) {
+		sieve_runtime_trace(renv, SIEVE_TRLVL_COMMANDS,
+				    "removeflag command");
 		flag_op = sieve_ext_imap4flags_remove_flags;
 	} else {
 		i_unreached();
