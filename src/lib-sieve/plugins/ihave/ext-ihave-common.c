23d2a289e4f5b939c6eabf6d7518bc33b8bdfcb3
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-04-05 22:27:23 +0200

    lib-sieve: plugins: ihave: Reformat ext-ihave-common.c

--- src/lib-sieve/plugins/ihave/ext-ihave-common.c
+++ src/lib-sieve/plugins/ihave/ext-ihave-common.c
@@ -13,14 +13,16 @@
  * AST context management
  */
 
-struct ext_ihave_ast_context *ext_ihave_get_ast_context
-(const struct sieve_extension *this_ext, struct sieve_ast *ast)
+struct ext_ihave_ast_context *
+ext_ihave_get_ast_context(const struct sieve_extension *this_ext,
+			  struct sieve_ast *ast)
 {
-	struct ext_ihave_ast_context *actx = (struct ext_ihave_ast_context *)
-		sieve_ast_extension_get_context(ast, this_ext);
+	struct ext_ihave_ast_context *actx =
+		(struct ext_ihave_ast_context *)
+			sieve_ast_extension_get_context(ast, this_ext);
 	pool_t pool;
 
-	if ( actx != NULL )
+	if (actx != NULL)
 		return actx;
 
 	pool = sieve_ast_pool(ast);
@@ -28,13 +30,12 @@ struct ext_ihave_ast_context *ext_ihave_get_ast_context
 	p_array_init(&actx->missing_extensions, pool, 64);
 
 	sieve_ast_extension_set_context(ast, this_ext, (void *) actx);
-
 	return actx;
 }
 
-void ext_ihave_ast_add_missing_extension
-(const struct sieve_extension *this_ext, struct sieve_ast *ast,
-	const char *ext_name)
+void ext_ihave_ast_add_missing_extension(const struct sieve_extension *this_ext,
+					 struct sieve_ast *ast,
+					 const char *ext_name)
 {
 	struct ext_ihave_ast_context *actx =
 		ext_ihave_get_ast_context(this_ext, ast);
@@ -42,11 +43,10 @@ void ext_ihave_ast_add_missing_extension
 	unsigned int i, count;
 
 	exts = array_get(&actx->missing_extensions, &count);
-	for ( i = 0; i < count; i++ ) {
-		if ( strcmp(exts[i], ext_name) == 0 )
+	for (i = 0; i < count; i++) {
+		if (strcmp(exts[i], ext_name) == 0)
 			return;
 	}
 
 	array_append(&actx->missing_extensions, &ext_name, 1);
 }
-
