76bf72d8b403883c0a01220c3e11ed7dce15e95d
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-04-05 22:04:41 +0200

    lib-sieve: plugins: environment: Reformat sieve-ext-environment.h

--- src/lib-sieve/plugins/environment/sieve-ext-environment.h
+++ src/lib-sieve/plugins/environment/sieve-ext-environment.h
@@ -12,24 +12,19 @@
 extern const struct sieve_extension_def environment_extension;
 
 static inline const struct sieve_extension *
-sieve_ext_environment_get_extension
-(struct sieve_instance *svinst)
+sieve_ext_environment_get_extension(struct sieve_instance *svinst)
 {
-	return sieve_extension_register
-		(svinst, &environment_extension, FALSE);
+	return sieve_extension_register(svinst, &environment_extension, FALSE);
 }
 
 static inline const struct sieve_extension *
-sieve_ext_environment_require_extension
-(struct sieve_instance *svinst)
+sieve_ext_environment_require_extension(struct sieve_instance *svinst)
 {
-	return sieve_extension_require
-		(svinst, &environment_extension, TRUE);
+	return sieve_extension_require(svinst, &environment_extension, TRUE);
 }
 
-bool sieve_ext_environment_is_active
-	(const struct sieve_extension *env_ext,
-		struct sieve_interpreter *interp);
+bool sieve_ext_environment_is_active(const struct sieve_extension *env_ext,
+				     struct sieve_interpreter *interp);
 
 /*
  * Environment item
@@ -40,15 +35,16 @@ struct sieve_environment_item {
 	bool prefix;
 
 	const char *value;
-	const char *(*get_value)
-		(const struct sieve_runtime_env *renv, const char *name);
+	const char *(*get_value)(const struct sieve_runtime_env *renv,
+				 const char *name);
 };
 
-void sieve_environment_item_register
-	(const struct sieve_extension *env_ext, struct sieve_interpreter *interp,
-		const struct sieve_environment_item *item);
-const char *ext_environment_item_get_value
-	(const struct sieve_extension *env_ext,
-		const struct sieve_runtime_env *renv, const char *name);
+void sieve_environment_item_register(const struct sieve_extension *env_ext,
+				     struct sieve_interpreter *interp,
+				     const struct sieve_environment_item *item);
+const char *
+ext_environment_item_get_value(const struct sieve_extension *env_ext,
+			       const struct sieve_runtime_env *renv,
+			       const char *name);
 
 #endif
