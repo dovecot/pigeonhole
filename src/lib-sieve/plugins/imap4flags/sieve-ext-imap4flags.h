aa89026e15d0a43ea045b40bc6b7efe0c29ea087
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-04-05 23:10:25 +0200

    lib-sieve: plugins: imap4flags: Reformat sieve-ext-imap4flags.h

--- src/lib-sieve/plugins/imap4flags/sieve-ext-imap4flags.h
+++ src/lib-sieve/plugins/imap4flags/sieve-ext-imap4flags.h
@@ -14,23 +14,21 @@ extern const struct sieve_interpreter_extension
 	imap4flags_interpreter_extension;
 
 static inline const struct sieve_extension *
-sieve_ext_imap4flags_require_extension
-(struct sieve_instance *svinst)
+sieve_ext_imap4flags_require_extension(struct sieve_instance *svinst)
 {
-	return sieve_extension_require
-		(svinst, &imap4flags_extension, TRUE);
+	return sieve_extension_require(svinst, &imap4flags_extension, TRUE);
 }
 
-void sieve_ext_imap4flags_interpreter_load
-(const struct sieve_extension *ext,
+void sieve_ext_imap4flags_interpreter_load(
+	const struct sieve_extension *ext,
 	const struct sieve_runtime_env *renv);
 
 /*
  * Action side-effect
  */
 
-void sieve_ext_imap4flags_register_side_effect
-(struct sieve_validator *valdtr, const struct sieve_extension *flg_ext,
+void sieve_ext_imap4flags_register_side_effect(
+	struct sieve_validator *valdtr, const struct sieve_extension *flg_ext,
         const char *command);
 
 /*
@@ -43,32 +41,30 @@ bool sieve_ext_imap4flags_flag_is_valid(const char *flag);
  * Flag manipulation
  */
 
-int sieve_ext_imap4flags_set_flags
-(const struct sieve_runtime_env *renv,
-	const struct sieve_extension *flg_ext,
-	struct sieve_variable_storage *storage,
-	unsigned int var_index,
-	struct sieve_stringlist *flags) ATTR_NULL(3);
-int sieve_ext_imap4flags_add_flags
-(const struct sieve_runtime_env *renv,
-	const struct sieve_extension *flg_ext,
-	struct sieve_variable_storage *storage,
-	unsigned int var_index,
-	struct sieve_stringlist *flags) ATTR_NULL(3);
-int sieve_ext_imap4flags_remove_flags
-(const struct sieve_runtime_env *renv,
-	const struct sieve_extension *flg_ext,
-	struct sieve_variable_storage *storage,
-	unsigned int var_index,
-	struct sieve_stringlist *flags) ATTR_NULL(3);
+int sieve_ext_imap4flags_set_flags(const struct sieve_runtime_env *renv,
+				   const struct sieve_extension *flg_ext,
+				   struct sieve_variable_storage *storage,
+				   unsigned int var_index,
+				   struct sieve_stringlist *flags) ATTR_NULL(3);
+int sieve_ext_imap4flags_add_flags(const struct sieve_runtime_env *renv,
+				   const struct sieve_extension *flg_ext,
+				   struct sieve_variable_storage *storage,
+				   unsigned int var_index,
+				   struct sieve_stringlist *flags) ATTR_NULL(3);
+int sieve_ext_imap4flags_remove_flags(const struct sieve_runtime_env *renv,
+				      const struct sieve_extension *flg_ext,
+				      struct sieve_variable_storage *storage,
+				      unsigned int var_index,
+				      struct sieve_stringlist *flags)
+				      ATTR_NULL(3);
 
 /*
  * Flag retrieval
  */
 
-struct sieve_stringlist *sieve_ext_imap4flags_get_flags
-(const struct sieve_runtime_env *renv,
-	const struct sieve_extension *flg_ext,
-	struct sieve_stringlist *flags_list);
+struct sieve_stringlist *
+sieve_ext_imap4flags_get_flags(const struct sieve_runtime_env *renv,
+			       const struct sieve_extension *flg_ext,
+			       struct sieve_stringlist *flags_list);
 
 #endif
