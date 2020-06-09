f5a7bbb020091c8528db348525336e191b7cd4b9
Stephan Bosch <stephan.bosch@open-xchange.com>
2020-06-09 02:30:42 +0200

    testsuite: Reformat testsuite-common.h.

--- src/testsuite/testsuite-common.h
+++ src/testsuite/testsuite-common.h
@@ -29,8 +29,8 @@ struct testsuite_validator_context {
 };
 
 bool testsuite_validator_context_initialize(struct sieve_validator *valdtr);
-struct testsuite_validator_context *testsuite_validator_context_get
-	(struct sieve_validator *valdtr);
+struct testsuite_validator_context *
+testsuite_validator_context_get(struct sieve_validator *valdtr);
 
 /*
  * Generator context
@@ -40,8 +40,8 @@ struct testsuite_generator_context {
 	struct sieve_jumplist *exit_jumps;
 };
 
-bool testsuite_generator_context_initialize
-	(struct sieve_generator *gentr, const struct sieve_extension *this_ext);
+bool testsuite_generator_context_initialize(
+	struct sieve_generator *gentr, const struct sieve_extension *this_ext);
 
 /*
  * Interpreter context
@@ -51,10 +51,12 @@ struct testsuite_interpreter_context {
 	struct sieve_binary *compiled_script;
 };
 
-bool testsuite_interpreter_context_initialize
-	(struct sieve_interpreter *interp, const struct sieve_extension *this_ext);
-struct testsuite_interpreter_context *testsuite_interpreter_context_get
-	(struct sieve_interpreter *interp, const struct sieve_extension *this_ext);
+bool testsuite_interpreter_context_initialize(
+	struct sieve_interpreter *interp,
+	const struct sieve_extension *this_ext);
+struct testsuite_interpreter_context *
+testsuite_interpreter_context_get(struct sieve_interpreter *interp,
+				  const struct sieve_extension *this_ext);
 
 /*
  * Commands
@@ -179,8 +181,8 @@ const char *testsuite_tmp_dir_get(void);
  * Testsuite init/deinit
  */
 
-void testsuite_init
-	(struct sieve_instance *svinst, const char *test_path, bool log_stdout);
+void testsuite_init(struct sieve_instance *svinst, const char *test_path,
+		    bool log_stdout);
 void testsuite_deinit(void);
 
 #endif
