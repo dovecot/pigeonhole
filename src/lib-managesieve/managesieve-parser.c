d40dd502fdba7426ad426b13b12499f9a46afd7e
Stephan Bosch <stephan.bosch@open-xchange.com>
2021-06-03 19:52:58 +0200

    lib-managesieve: managesieve-parser - Fix 8 bit atom check to use unsigned char.

--- src/lib-managesieve/managesieve-parser.c
+++ src/lib-managesieve/managesieve-parser.c
@@ -206,7 +206,7 @@ static bool is_valid_atom_char(struct managesieve_parser *parser, char chr)
 	if (IS_ATOM_SPECIAL((unsigned char)chr)) {
 		parser->error = "Invalid characters in atom";
 		return FALSE;
-	} else if ((chr & 0x80) != 0) {
+	} else if ((((unsigned char)chr) & 0x80) != 0) {
 		parser->error = "8bit data in atom";
 		return FALSE;
 	}
