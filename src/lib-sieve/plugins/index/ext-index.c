4901c12b718fdfae25cf4a86a608ae36b157b4d9
Stephan Bosch <stephan.bosch@open-xchange.com>
2021-06-21 22:50:35 +0200

    lib-sieve: plugins: index: Reformat ext-index.c

--- src/lib-sieve/plugins/index/ext-index.c
+++ src/lib-sieve/plugins/index/ext-index.c
@@ -30,39 +30,38 @@
  * Extension
  */
 
-static bool ext_index_validator_load
-(const struct sieve_extension *ext, struct sieve_validator *validator);
+static bool
+ext_index_validator_load(const struct sieve_extension *ext,
+			 struct sieve_validator *validator);
 
 const struct sieve_extension_def index_extension = {
 	.name = "index",
 	.validator_load = ext_index_validator_load,
-	SIEVE_EXT_DEFINE_OPERAND(index_operand)
+	SIEVE_EXT_DEFINE_OPERAND(index_operand),
 };
 
 static bool ext_index_validator_load
 (const struct sieve_extension *ext, struct sieve_validator *valdtr)
 {
-	/* Register :index and :last tags with header, address and date test commands
-	 * and we don't care whether these command are registered or even whether
-	 * these will be registered at all. The validator handles either situation
-	 * gracefully.
+	/* Register :index and :last tags with header, address and date test
+	   commands and we don't care whether these command are registered or
+	   even whether these will be registered at all. The validator handles
+	   either situation gracefully.
 	 */
-	sieve_validator_register_external_tag
-		(valdtr, "header", ext, &index_tag, SIEVE_OPT_MESSAGE_OVERRIDE);
-	sieve_validator_register_external_tag
-		(valdtr, "header", ext, &last_tag, 0);
+	sieve_validator_register_external_tag(
+		valdtr, "header", ext, &index_tag, SIEVE_OPT_MESSAGE_OVERRIDE);
+	sieve_validator_register_external_tag(
+		valdtr, "header", ext, &last_tag, 0);
 
-	sieve_validator_register_external_tag
-		(valdtr, "address", ext, &index_tag, SIEVE_OPT_MESSAGE_OVERRIDE);
-	sieve_validator_register_external_tag
-		(valdtr, "address", ext, &last_tag, 0);
+	sieve_validator_register_external_tag(
+		valdtr, "address", ext,	&index_tag, SIEVE_OPT_MESSAGE_OVERRIDE);
+	sieve_validator_register_external_tag(
+		valdtr, "address", ext, &last_tag, 0);
 
-	sieve_validator_register_external_tag
-		(valdtr, "date", ext, &index_tag, SIEVE_OPT_MESSAGE_OVERRIDE);
-	sieve_validator_register_external_tag
-		(valdtr, "date", ext, &last_tag, 0);
+	sieve_validator_register_external_tag(
+		valdtr, "date", ext, &index_tag, SIEVE_OPT_MESSAGE_OVERRIDE);
+	sieve_validator_register_external_tag(
+		valdtr, "date", ext, &last_tag, 0);
 
 	return TRUE;
 }
-
-
