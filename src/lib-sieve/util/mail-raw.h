4f8b015f274b34918b672a768d1fe47877707843
Stephan Bosch <stephan.bosch@open-xchange.com>
2021-06-21 21:26:08 +0200

    lib-sieve: util: Reformat mail-raw.h

--- src/lib-sieve/util/mail-raw.h
+++ src/lib-sieve/util/mail-raw.h
@@ -12,16 +12,15 @@ struct mail_raw {
 	struct mailbox_transaction_context *trans;
 };
 
-struct mail_user *mail_raw_user_create
-	(struct mail_user *mail_user);
+struct mail_user *mail_raw_user_create(struct mail_user *mail_user);
 
-struct mail_raw *mail_raw_open_stream
-	(struct mail_user *ruser, struct istream *input);
-struct mail_raw *mail_raw_open_file
-	(struct mail_user *ruser, const char *path);
-struct mail_raw *mail_raw_open_data
-	(struct mail_user *ruser, string_t *mail_data);
+struct mail_raw *
+mail_raw_open_stream(struct mail_user *ruser, struct istream *input);
+struct mail_raw *
+mail_raw_open_file(struct mail_user *ruser, const char *path);
+struct mail_raw *
+mail_raw_open_data(struct mail_user *ruser, string_t *mail_data);
+
 void mail_raw_close(struct mail_raw **mailr);
-
 
 #endif
