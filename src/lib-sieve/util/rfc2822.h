8a4439d736c3500db9071e71cf608976236abb1c
Stephan Bosch <stephan.bosch@open-xchange.com>
2021-06-21 22:17:16 +0200

    lib-sieve: util: Reformat rfc2822.h

--- src/lib-sieve/util/rfc2822.h
+++ src/lib-sieve/util/rfc2822.h
@@ -9,10 +9,9 @@
  * Verification
  */
 
-bool rfc2822_header_field_name_verify
-	(const char *field_name, unsigned int len);
-bool rfc2822_header_field_body_verify
-	(const char *field_body, unsigned int len, bool allow_crlf, bool allow_utf8);
+bool rfc2822_header_field_name_verify(const char *field_name, unsigned int len);
+bool rfc2822_header_field_body_verify(const char *field_body, unsigned int len,
+				      bool allow_crlf, bool allow_utf8);
 
 /*
  *
@@ -24,23 +23,22 @@ const char *rfc2822_header_field_name_sanitize(const char *name);
  * Message composition
  */
 
-unsigned int rfc2822_header_append
-	(string_t *header, const char *name, const char *body, bool crlf,
-		uoff_t *body_offset_r);
+unsigned int
+rfc2822_header_append(string_t *header, const char *name, const char *body,
+		      bool crlf, uoff_t *body_offset_r);
 
-static inline void rfc2822_header_write
-(string_t *header, const char *name, const char *body)
+static inline void
+rfc2822_header_write(string_t *header, const char *name, const char *body)
 {
 	(void)rfc2822_header_append(header, name, body, TRUE, NULL);
 }
 
-void rfc2822_header_printf
-	(string_t *header, const char *name, const char *fmt, ...) ATTR_FORMAT(3, 4);
-void rfc2822_header_utf8_printf
-	(string_t *header, const char *name, const char *fmt, ...) ATTR_FORMAT(3, 4);
+void rfc2822_header_printf(string_t *header, const char *name,
+			   const char *fmt, ...) ATTR_FORMAT(3, 4);
+void rfc2822_header_utf8_printf(string_t *header, const char *name,
+				const char *fmt, ...) ATTR_FORMAT(3, 4);
 
-void rfc2822_header_write_address(string_t *header,
-	const char *name, const char *address);
-
+void rfc2822_header_write_address(string_t *header, const char *name,
+				  const char *address);
 
 #endif
