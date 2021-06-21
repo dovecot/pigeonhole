6830d098f2c71645ca8c97596c24cc07d23c8f57
Stephan Bosch <stephan.bosch@open-xchange.com>
2021-06-21 23:58:17 +0200

    lib-sieve: plugins: mime: Reformat ext-extracttext.c

--- src/lib-sieve/plugins/mime/ext-extracttext.c
+++ src/lib-sieve/plugins/mime/ext-extracttext.c
@@ -30,43 +30,43 @@
  * Extension
  */
 
-static bool ext_extracttext_load
-	(const struct sieve_extension *ext, void **context);
-static void ext_extracttext_unload
-	(const struct sieve_extension *ext);
-static bool ext_extracttext_validator_load
-	(const struct sieve_extension *ext, struct sieve_validator *valdtr);
+static bool
+ext_extracttext_load(const struct sieve_extension *ext, void **context);
+static void
+ext_extracttext_unload(const struct sieve_extension *ext);
+static bool
+ext_extracttext_validator_load(const struct sieve_extension *ext,
+			       struct sieve_validator *valdtr);
 
 const struct sieve_extension_def extracttext_extension = {
 	.name = "extracttext",
 	.load = ext_extracttext_load,
 	.unload = ext_extracttext_unload,
 	.validator_load = ext_extracttext_validator_load,
-	SIEVE_EXT_DEFINE_OPERATION(extracttext_operation)
+	SIEVE_EXT_DEFINE_OPERATION(extracttext_operation),
 };
 
-static bool ext_extracttext_load
-(const struct sieve_extension *ext, void **context)
+static bool
+ext_extracttext_load(const struct sieve_extension *ext, void **context)
 {
 	struct sieve_instance *svinst = ext->svinst;
 	struct ext_extracttext_context *ectx;
 
-	if ( *context != NULL )
+	if (*context != NULL)
 		ext_extracttext_unload(ext);
 
 	ectx = i_new(struct ext_extracttext_context, 1);
 	ectx->var_ext = sieve_ext_variables_get_extension(ext->svinst);
-	ectx->fep_ext = sieve_extension_register
-		(svinst, &foreverypart_extension, FALSE);
+	ectx->fep_ext = sieve_extension_register(
+		svinst, &foreverypart_extension, FALSE);
 	*context = (void *)ectx;
 	return TRUE;
 }
 
-static void ext_extracttext_unload
-(const struct sieve_extension *ext)
+static void ext_extracttext_unload(const struct sieve_extension *ext)
 {
 	struct ext_extracttext_context *ctx =
-		(struct ext_extracttext_context *) ext->context;
+		(struct ext_extracttext_context *)ext->context;
 
 	i_free(ctx);
 }
@@ -75,24 +75,25 @@ static void ext_extracttext_unload
  * Extension validation
  */
 
-static bool ext_extracttext_validator_validate
-	(const struct sieve_extension *ext,
-		struct sieve_validator *valdtr, void *context,
-		struct sieve_ast_argument *require_arg,
-		bool required);
+static bool
+ext_extracttext_validator_validate(
+	const struct sieve_extension *ext, struct sieve_validator *valdtr,
+	void *context, struct sieve_ast_argument *require_arg, bool required);
 
 const struct sieve_validator_extension
 extracttext_validator_extension = {
 	.ext = &extracttext_extension,
-	.validate = ext_extracttext_validator_validate
+	.validate = ext_extracttext_validator_validate,
 };
 
-static bool ext_extracttext_validator_load
-(const struct sieve_extension *ext, struct sieve_validator *valdtr)
+static bool
+ext_extracttext_validator_load(const struct sieve_extension *ext,
+			       struct sieve_validator *valdtr)
 {
-	/* Register validator extension to check for conflict with eextracttext */
-	sieve_validator_extension_register
-		(valdtr, ext, &extracttext_validator_extension, NULL);
+	/* Register validator extension to check for conflict with eextracttext.
+	 */
+	sieve_validator_extension_register(
+		valdtr, ext, &extracttext_validator_extension, NULL);
 
 	/* Register new commands */
 	sieve_validator_register_command(valdtr, ext, &cmd_extracttext);
@@ -100,31 +101,31 @@ static bool ext_extracttext_validator_load
 	return TRUE;
 }
 
-static bool ext_extracttext_validator_validate
-(const struct sieve_extension *ext,
-	struct sieve_validator *valdtr, void *context ATTR_UNUSED,
-	struct sieve_ast_argument *require_arg,
-	bool required ATTR_UNUSED)
+static bool
+ext_extracttext_validator_validate(const struct sieve_extension *ext,
+				   struct sieve_validator *valdtr,
+				   void *context ATTR_UNUSED,
+				   struct sieve_ast_argument *require_arg,
+				   bool required ATTR_UNUSED)
 {
 	struct ext_extracttext_context *ectx =
 		(struct ext_extracttext_context *)ext->context;
 
-	if ( ectx->var_ext == NULL ||
-		!sieve_ext_variables_is_active
-			(ectx->var_ext, valdtr) ) {
-		sieve_argument_validate_error(valdtr, require_arg,
+	if (ectx->var_ext == NULL ||
+	    !sieve_ext_variables_is_active(ectx->var_ext, valdtr)) {
+		sieve_argument_validate_error(
+			valdtr, require_arg,
 			"extracttext extension cannot be used "
 			"without variables extension");
 		return FALSE;
 	}
-	if ( ectx->fep_ext == NULL ||
-		!sieve_validator_extension_loaded
-	    (valdtr, ectx->fep_ext) ) {
-		sieve_argument_validate_error(valdtr, require_arg,
+	if (ectx->fep_ext == NULL ||
+	    !sieve_validator_extension_loaded(valdtr, ectx->fep_ext)) {
+		sieve_argument_validate_error(
+			valdtr, require_arg,
 			"extracttext extension cannot be used "
 			"without foreverypart extension");
 		return FALSE;
 	}
-
 	return TRUE;
 }
