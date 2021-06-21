bf3d37d9530f9229d51c5e9c4b7d0019c9095633
Stephan Bosch <stephan.bosch@open-xchange.com>
2021-06-22 01:26:26 +0200

    lib-sieve: plugins: relational: Reformat mcht-value.c

--- src/lib-sieve/plugins/relational/mcht-value.c
+++ src/lib-sieve/plugins/relational/mcht-value.c
@@ -23,17 +23,16 @@
  */
 
 const struct sieve_match_type_def value_match_type = {
-	SIEVE_OBJECT("value",
-		&rel_match_type_operand, RELATIONAL_VALUE),
-	.validate = mcht_relational_validate
+	SIEVE_OBJECT("value", &rel_match_type_operand, RELATIONAL_VALUE),
+	.validate = mcht_relational_validate,
 };
 
-#define VALUE_MATCH_TYPE(name, rel_match)                       \
-const struct sieve_match_type_def rel_match_value_ ## name = {  \
-	SIEVE_OBJECT("value-" #name,                                  \
-		&rel_match_type_operand,                                    \
-		REL_MATCH_INDEX(RELATIONAL_VALUE, rel_match)),              \
-	.match_key = mcht_value_match_key,                            \
+#define VALUE_MATCH_TYPE(name, rel_match)                           \
+const struct sieve_match_type_def rel_match_value_ ## name = {      \
+	SIEVE_OBJECT("value-" #name,                                \
+		     &rel_match_type_operand,                       \
+		     REL_MATCH_INDEX(RELATIONAL_VALUE, rel_match)), \
+	.match_key = mcht_value_match_key,                          \
 }
 
 VALUE_MATCH_TYPE(gt, REL_MATCH_GREATER);
@@ -47,33 +46,30 @@ VALUE_MATCH_TYPE(ne, REL_MATCH_NOT_EQUAL);
  * Match-type implementation
  */
 
-int mcht_value_match_key
-(struct sieve_match_context *mctx, const char *val, size_t val_size,
-	const char *key, size_t key_size)
+int mcht_value_match_key(struct sieve_match_context *mctx,
+			 const char *val, size_t val_size,
+			 const char *key, size_t key_size)
 {
 	const struct sieve_match_type *mtch = mctx->match_type;
 	unsigned int rel_match = REL_MATCH(mtch->object.def->code);
 	int cmp_result;
 
-	cmp_result = mctx->comparator->def->
-		compare(mctx->comparator, val, val_size, key, key_size);
+	cmp_result = mctx->comparator->def->compare(
+		mctx->comparator, val, val_size, key, key_size);
 
-	switch ( rel_match ) {
+	switch (rel_match) {
 	case REL_MATCH_GREATER:
-		return ( cmp_result > 0 ? 1 : 0 );
+		return (cmp_result > 0 ? 1 : 0);
 	case REL_MATCH_GREATER_EQUAL:
-		return ( cmp_result >= 0 ? 1 : 0 );
+		return (cmp_result >= 0 ? 1 : 0);
 	case REL_MATCH_LESS:
-		return ( cmp_result < 0 ? 1 : 0 );
+		return (cmp_result < 0 ? 1 : 0);
 	case REL_MATCH_LESS_EQUAL:
-		return ( cmp_result <= 0 ? 1 : 0 );
+		return (cmp_result <= 0 ? 1 : 0);
 	case REL_MATCH_EQUAL:
-		return ( cmp_result == 0 ? 1 : 0);
+		return (cmp_result == 0 ? 1 : 0);
 	case REL_MATCH_NOT_EQUAL:
-		return ( cmp_result != 0 ? 1 : 0);
+		return (cmp_result != 0 ? 1 : 0);
 	}
-
 	i_unreached();
 }
-
-
