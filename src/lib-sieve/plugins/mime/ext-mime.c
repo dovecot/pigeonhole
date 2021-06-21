aaa97f15e2a4ec63f6395a83c76fd39393bcb740
Stephan Bosch <stephan.bosch@open-xchange.com>
2021-06-22 00:16:50 +0200

    lib-sieve: plugins: mime: Reformat ext-mime.c

--- src/lib-sieve/plugins/mime/ext-mime.c
+++ src/lib-sieve/plugins/mime/ext-mime.c
@@ -28,49 +28,51 @@
  * Extension
  */
 
-static bool ext_mime_validator_load
-	(const struct sieve_extension *ext, struct sieve_validator *valdtr);
+static bool
+ext_mime_validator_load(const struct sieve_extension *ext,
+			struct sieve_validator *valdtr);
 
 const struct sieve_extension_def mime_extension = {
 	.name = "mime",
 	.validator_load = ext_mime_validator_load,
-	SIEVE_EXT_DEFINE_OPERAND(mime_operand)
+	SIEVE_EXT_DEFINE_OPERAND(mime_operand),
 };
 
 /*
  * Extension validation
  */
 
-static bool ext_mime_validator_load
-(const struct sieve_extension *ext, struct sieve_validator *valdtr)
+static bool
+ext_mime_validator_load(const struct sieve_extension *ext,
+			struct sieve_validator *valdtr)
 {
 	/* Register :mime tag and friends with header, address and exists test
-	 * commands and we don't care whether these command are registered or
-	 * even whether these will be registered at all. The validator handles
-	 * either situation gracefully.
+	   commands and we don't care whether these command are registered or
+	   even whether these will be registered at all. The validator handles
+	   either situation gracefully.
 	 */
-	sieve_validator_register_external_tag
-		(valdtr, "header", ext, &mime_tag, SIEVE_OPT_MESSAGE_OVERRIDE);
-	sieve_validator_register_external_tag
-		(valdtr, "header", ext, &mime_anychild_tag, 0);
-	sieve_validator_register_external_tag
-		(valdtr, "header", ext, &mime_type_tag, 0);
-	sieve_validator_register_external_tag
-		(valdtr, "header", ext, &mime_subtype_tag, 0);
-	sieve_validator_register_external_tag
-		(valdtr, "header", ext, &mime_contenttype_tag, 0);
-	sieve_validator_register_external_tag
-		(valdtr, "header", ext, &mime_param_tag, 0);
+	sieve_validator_register_external_tag(
+		valdtr, "header", ext, &mime_tag, SIEVE_OPT_MESSAGE_OVERRIDE);
+	sieve_validator_register_external_tag(
+		valdtr, "header", ext, &mime_anychild_tag, 0);
+	sieve_validator_register_external_tag(
+		valdtr, "header", ext, &mime_type_tag, 0);
+	sieve_validator_register_external_tag(
+		valdtr, "header", ext, &mime_subtype_tag, 0);
+	sieve_validator_register_external_tag(
+		valdtr, "header", ext, &mime_contenttype_tag, 0);
+	sieve_validator_register_external_tag(
+		valdtr, "header", ext, &mime_param_tag, 0);
 
-	sieve_validator_register_external_tag
-		(valdtr, "address", ext, &mime_tag, SIEVE_OPT_MESSAGE_OVERRIDE);
-	sieve_validator_register_external_tag
-		(valdtr, "address", ext, &mime_anychild_tag, 0);
+	sieve_validator_register_external_tag(
+		valdtr, "address", ext, &mime_tag, SIEVE_OPT_MESSAGE_OVERRIDE);
+	sieve_validator_register_external_tag(
+		valdtr, "address", ext, &mime_anychild_tag, 0);
 
-	sieve_validator_register_external_tag
-		(valdtr, "exists", ext, &mime_tag, SIEVE_OPT_MESSAGE_OVERRIDE);
-	sieve_validator_register_external_tag
-		(valdtr, "exists", ext, &mime_anychild_tag, 0);
+	sieve_validator_register_external_tag(
+		valdtr, "exists", ext, &mime_tag, SIEVE_OPT_MESSAGE_OVERRIDE);
+	sieve_validator_register_external_tag(
+		valdtr, "exists", ext, &mime_anychild_tag, 0);
 
 	return TRUE;
 }
