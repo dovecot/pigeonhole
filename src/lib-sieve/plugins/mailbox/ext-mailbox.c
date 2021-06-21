9f4333bbb524001de1cb1d28b7695601a04d9d2a
Stephan Bosch <stephan.bosch@open-xchange.com>
2021-06-21 23:04:34 +0200

    lib-sieve: plugins: mailbox: Reformat ext-mailbox.c

--- src/lib-sieve/plugins/mailbox/ext-mailbox.c
+++ src/lib-sieve/plugins/mailbox/ext-mailbox.c
@@ -27,13 +27,14 @@
  * Tag registration
  */
 
-void sieve_ext_mailbox_register_create_tag
-(struct sieve_validator *valdtr, const struct sieve_extension *mailbox_ext,
-	const char *command)
+void sieve_ext_mailbox_register_create_tag(
+	struct sieve_validator *valdtr,
+	const struct sieve_extension *mailbox_ext, const char *command)
 {
-	if ( sieve_validator_extension_loaded(valdtr, mailbox_ext) ) {
-		sieve_validator_register_external_tag(valdtr, command,
-			mailbox_ext, &mailbox_create_tag, SIEVE_OPT_SIDE_EFFECT);
+	if (sieve_validator_extension_loaded(valdtr, mailbox_ext)) {
+		sieve_validator_register_external_tag(
+			valdtr, command, mailbox_ext, &mailbox_create_tag,
+			SIEVE_OPT_SIDE_EFFECT);
 	}
 }
 
@@ -42,30 +43,31 @@ void sieve_ext_mailbox_register_create_tag
  * Extension
  */
 
-static bool ext_mailbox_validator_load
-(const struct sieve_extension *ext, struct sieve_validator *valdtr);
+static bool
+ext_mailbox_validator_load(const struct sieve_extension *ext,
+			   struct sieve_validator *valdtr);
 
 const struct sieve_extension_def mailbox_extension = {
 	.name = "mailbox",
 	.validator_load = ext_mailbox_validator_load,
 	SIEVE_EXT_DEFINE_OPERATION(mailboxexists_operation),
-	SIEVE_EXT_DEFINE_OPERAND(mailbox_create_operand)
+	SIEVE_EXT_DEFINE_OPERAND(mailbox_create_operand),
 };
 
-static bool ext_mailbox_validator_load
-(const struct sieve_extension *ext, struct sieve_validator *valdtr)
+static bool
+ext_mailbox_validator_load(const struct sieve_extension *ext,
+			   struct sieve_validator *valdtr)
 {
-	/* Register :create tag with fileinto command and we don't care whether this
-	 * command is registered or even whether it will be registered at all. The
-	 * validator handles either situation gracefully
+	/* Register :create tag with fileinto command and we don't care whether
+	   this command is registered or even whether it will be registered at
+	   all. The validator handles either situation gracefully.
 	 */
-	sieve_validator_register_external_tag
-		(valdtr, "fileinto", ext, &mailbox_create_tag, SIEVE_OPT_SIDE_EFFECT);
+	sieve_validator_register_external_tag(
+		valdtr, "fileinto", ext, &mailbox_create_tag,
+		SIEVE_OPT_SIDE_EFFECT);
 
 	/* Register new test */
 	sieve_validator_register_command(valdtr, ext, &mailboxexists_test);
 
 	return TRUE;
 }
-
-
