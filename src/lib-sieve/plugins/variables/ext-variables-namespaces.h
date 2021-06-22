ae25c50ff4c241ce51b0c316a1e889d3884a2fab
Stephan Bosch <stephan.bosch@open-xchange.com>
2021-06-22 11:56:14 +0200

    lib-sieve: plugins: variables: Reformat ext-variables-namespaces.h

--- src/lib-sieve/plugins/variables/ext-variables-namespaces.h
+++ src/lib-sieve/plugins/variables/ext-variables-namespaces.h
@@ -10,29 +10,32 @@
  * Namespace registry
  */
 
-bool ext_variables_namespace_exists
-	(const struct sieve_extension *var_ext, struct sieve_validator *valdtr,
-		const char *identifier);
-const struct sieve_variables_namespace *ext_variables_namespace_create_instance
-	(const struct sieve_extension *var_ext, struct sieve_validator *valdtr,
-		struct sieve_command *cmd, const char *identifier);
+bool ext_variables_namespace_exists(const struct sieve_extension *var_ext,
+				    struct sieve_validator *valdtr,
+				    const char *identifier);
+const struct sieve_variables_namespace *
+ext_variables_namespace_create_instance(const struct sieve_extension *var_ext,
+					struct sieve_validator *valdtr,
+					struct sieve_command *cmd,
+					const char *identifier);
 
-void ext_variables_register_core_namespaces
-	(const struct sieve_extension *var_ext,
-		struct ext_variables_validator_context *ctx);
+void ext_variables_register_core_namespaces(
+	const struct sieve_extension *var_ext,
+	struct ext_variables_validator_context *ctx);
 
 /*
  * Namespace argument
  */
 
-struct sieve_ast_argument *ext_variables_namespace_argument_create
-	(const struct sieve_extension *this_ext,
-		struct sieve_validator *valdtr, struct sieve_ast_argument *parent_arg,
-		struct sieve_command *cmd, ARRAY_TYPE(sieve_variable_name) *var_name);
-bool ext_variables_namespace_argument_activate
-	(const struct sieve_extension *this_ext, struct sieve_validator *valdtr,
-		struct sieve_ast_argument *arg, struct sieve_command *cmd,
-		ARRAY_TYPE(sieve_variable_name) *var_name, bool assignment);
+struct sieve_ast_argument *
+ext_variables_namespace_argument_create(
+	const struct sieve_extension *this_ext, struct sieve_validator *valdtr,
+	struct sieve_ast_argument *parent_arg, struct sieve_command *cmd,
+	ARRAY_TYPE(sieve_variable_name) *var_name);
+bool ext_variables_namespace_argument_activate(
+	const struct sieve_extension *this_ext, struct sieve_validator *valdtr,
+	struct sieve_ast_argument *arg, struct sieve_command *cmd,
+	ARRAY_TYPE(sieve_variable_name) *var_name, bool assignment);
 
 /*
  * Namespace operand
