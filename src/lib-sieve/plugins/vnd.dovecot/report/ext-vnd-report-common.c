182d7f66f35ce5c0d04d25e2f775d6a2d4144043
Stephan Bosch <stephan.bosch@open-xchange.com>
2021-06-22 12:53:39 +0200

    lib-sieve: plugins: vnd.dovecot: report: Reformat ext-vnd-report-common.c

--- src/lib-sieve/plugins/vnd.dovecot/report/ext-vnd-report-common.c
+++ src/lib-sieve/plugins/vnd.dovecot/report/ext-vnd-report-common.c
@@ -10,30 +10,29 @@
 
 #include "ext-vnd-report-common.h"
 
-bool ext_report_load
-(const struct sieve_extension *ext, void **context)
+bool ext_report_load(const struct sieve_extension *ext, void **context)
 {
 	struct sieve_instance *svinst = ext->svinst;
 	struct ext_report_config *config;
 
 	config = p_new(svinst->pool, struct ext_report_config, 1);
 
-	(void)sieve_address_source_parse_from_setting(svinst,
-		svinst->pool, "sieve_report_from", &config->report_from);
+	(void)sieve_address_source_parse_from_setting(
+		svinst, svinst->pool, "sieve_report_from",
+		&config->report_from);
 
 	*context = (void *) config;
 	return TRUE;
 }
 
-const char *
-ext_vnd_report_parse_feedback_type(const char *feedback_type)
+const char *ext_vnd_report_parse_feedback_type(const char *feedback_type)
 {
 	struct rfc822_parser_context parser;
 	string_t *token;
 
 	/* Initialize parsing */
-	rfc822_parser_init(&parser,
-		(const unsigned char *)feedback_type, strlen(feedback_type), NULL);
+	rfc822_parser_init(&parser, (const unsigned char *)feedback_type,
+			   strlen(feedback_type), NULL);
 	(void)rfc822_skip_lwsp(&parser);
 
 	/* Parse MIME token */
@@ -41,9 +40,10 @@ ext_vnd_report_parse_feedback_type(const char *feedback_type)
 	if (rfc822_parse_mime_token(&parser, token) < 0)
 		return NULL;
 
-	/* Content-type value must end here, otherwise it is invalid after all */
+	/* Content-type value must end here, otherwise it is invalid after all.
+	 */
 	(void)rfc822_skip_lwsp(&parser);
-	if ( parser.data != parser.end )
+	if (parser.data != parser.end)
 		return NULL;
 
 	/* Success */
