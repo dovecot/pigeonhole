13ad6f910aabced2026256504462e889f409eed4
Stephan Bosch <stephan.bosch@open-xchange.com>
2021-06-22 02:26:51 +0200

    lib-sieve: plugins: vacation: Reformat ext-vacation-common.c

--- src/lib-sieve/plugins/vacation/ext-vacation-common.c
+++ src/lib-sieve/plugins/vacation/ext-vacation-common.c
@@ -10,8 +10,7 @@
 
 #include "ext-vacation-common.h"
 
-bool ext_vacation_load
-(const struct sieve_extension *ext, void **context)
+bool ext_vacation_load(const struct sieve_extension *ext, void **context)
 {
 	struct sieve_instance *svinst = ext->svinst;
 	struct ext_vacation_config *config;
@@ -20,28 +19,22 @@ bool ext_vacation_load
 		to_header_ignore_envelope;
 	const char *default_subject, *default_subject_template;
 
-	if ( *context != NULL ) {
+	if (*context != NULL)
 		ext_vacation_unload(ext);
-	}
 
-	if ( !sieve_setting_get_duration_value
-		(svinst, "sieve_vacation_min_period", &min_period) ) {
+	if (!sieve_setting_get_duration_value(
+		svinst, "sieve_vacation_min_period", &min_period))
 		min_period = EXT_VACATION_DEFAULT_MIN_PERIOD;
-	}
-
-	if ( !sieve_setting_get_duration_value
-		(svinst, "sieve_vacation_max_period", &max_period) ) {
+	if (!sieve_setting_get_duration_value(
+		svinst, "sieve_vacation_max_period", &max_period))
 		max_period = EXT_VACATION_DEFAULT_MAX_PERIOD;
-	}
-
-	if ( !sieve_setting_get_duration_value
-		(svinst, "sieve_vacation_default_period", &default_period) ) {
+	if (!sieve_setting_get_duration_value(
+		svinst, "sieve_vacation_default_period", &default_period))
 		default_period = EXT_VACATION_DEFAULT_PERIOD;
-	}
 
-	if ( max_period > 0
-		&& (min_period > max_period || default_period < min_period
-			|| default_period > max_period) ) {
+	if (max_period > 0 &&
+	    (min_period > max_period || default_period < min_period ||
+	     default_period > max_period)) {
 		min_period = EXT_VACATION_DEFAULT_MIN_PERIOD;
 		max_period = EXT_VACATION_DEFAULT_MAX_PERIOD;
 		default_period = EXT_VACATION_DEFAULT_PERIOD;
@@ -58,48 +51,43 @@ bool ext_vacation_load
 	default_subject_template = sieve_setting_get(
 		svinst, "sieve_vacation_default_subject_template");
 
-	if ( !sieve_setting_get_bool_value
-		(svinst, "sieve_vacation_use_original_recipient", &use_original_recipient) ) {
+	if (!sieve_setting_get_bool_value(
+		svinst, "sieve_vacation_use_original_recipient",
+		&use_original_recipient))
 		use_original_recipient = FALSE;
-	}
-
-	if ( !sieve_setting_get_bool_value
-		(svinst, "sieve_vacation_dont_check_recipient", &dont_check_recipient) ) {
+	if (!sieve_setting_get_bool_value(
+		svinst, "sieve_vacation_dont_check_recipient",
+		&dont_check_recipient))
 		dont_check_recipient = FALSE;
-	}
-
-	if ( !sieve_setting_get_bool_value
-		(svinst, "sieve_vacation_send_from_recipient", &send_from_recipient) ) {
+	if (!sieve_setting_get_bool_value(
+		svinst, "sieve_vacation_send_from_recipient",
+		&send_from_recipient))
 		send_from_recipient = FALSE;
-	}
-
-	if ( !sieve_setting_get_bool_value(svinst,
-		"sieve_vacation_to_header_ignore_envelope",
-		&to_header_ignore_envelope) ) {
+	if (!sieve_setting_get_bool_value(
+		svinst, "sieve_vacation_to_header_ignore_envelope",
+		&to_header_ignore_envelope) )
 		to_header_ignore_envelope = FALSE;
-	}
 
 	config = i_new(struct ext_vacation_config, 1);
 	config->min_period = min_period;
 	config->max_period = max_period;
 	config->default_period = default_period;
 	config->default_subject = i_strdup_empty(default_subject);
-	config->default_subject_template = i_strdup_empty(default_subject_template);
+	config->default_subject_template =
+		i_strdup_empty(default_subject_template);
 	config->use_original_recipient = use_original_recipient;
 	config->dont_check_recipient = dont_check_recipient;
 	config->send_from_recipient = send_from_recipient;
 	config->to_header_ignore_envelope = to_header_ignore_envelope;
 
-	*context = (void *) config;
-
+	*context = (void *)config;
 	return TRUE;
 }
 
-void ext_vacation_unload
-(const struct sieve_extension *ext)
+void ext_vacation_unload(const struct sieve_extension *ext)
 {
 	struct ext_vacation_config *config =
-		(struct ext_vacation_config *) ext->context;
+		(struct ext_vacation_config *)ext->context;
 
 	i_free(config->default_subject);
 	i_free(config->default_subject_template);
