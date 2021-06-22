91ee76900b33bea465264881426544632ed56c42
Stephan Bosch <stephan.bosch@open-xchange.com>
2021-06-22 16:41:08 +0200

    managesieve-login: Reformat client-authenticate.h.

--- src/managesieve-login/client-authenticate.h
+++ src/managesieve-login/client-authenticate.h
@@ -3,19 +3,18 @@
 
 struct managesieve_arg;
 
-const char *client_authenticate_get_capabilities
-	(struct client *client);
+const char *client_authenticate_get_capabilities(struct client *client);
 
-void managesieve_client_auth_result
-	(struct client *client, enum client_auth_result result,
-		const struct client_auth_reply *reply, const char *text);
+void managesieve_client_auth_result(struct client *client,
+				    enum client_auth_result result,
+				    const struct client_auth_reply *reply,
+				    const char *text);
 
-void managesieve_client_auth_send_challenge
-	(struct client *client, const char *data);
-void managesieve_client_auth_parse_response
-	(struct client *client);
+void managesieve_client_auth_send_challenge(struct client *client,
+					    const char *data);
+void managesieve_client_auth_parse_response(struct client *client);
 
-int cmd_authenticate
-	(struct managesieve_client *client, const struct managesieve_arg *args);
+int cmd_authenticate(struct managesieve_client *client,
+		     const struct managesieve_arg *args);
 
 #endif
