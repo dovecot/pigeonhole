f282f274600e4e077bfdb2fb469b0d441b4865a3
Stephan Bosch <stephan.bosch@open-xchange.com>
2021-06-22 13:05:35 +0200

    lib-managesieve: Reformat managesieve-quote.h

--- src/lib-managesieve/managesieve-quote.h
+++ src/lib-managesieve/managesieve-quote.h
@@ -4,14 +4,15 @@
 /* Return value suitable for sending to client, either as quoted-string or
    literal. Note that this also converts TABs into spaces, multiple spaces
    into single space and NULs to #128. */
-char *managesieve_quote(pool_t pool, const unsigned char *value, size_t value_len);
+char *managesieve_quote(pool_t pool, const unsigned char *value,
+			size_t value_len);
 
 /* Append to existing string. */
 void managesieve_quote_append(string_t *str, const unsigned char *value,
-		       size_t value_len, bool compress_lwsp);
+			      size_t value_len, bool compress_lwsp);
 
 #define managesieve_quote_append_string(str, value, compress_lwsp) \
 	managesieve_quote_append(str, (const unsigned char *)(value), \
-			  (size_t)-1, compress_lwsp)
+				 (size_t)-1, compress_lwsp)
 
 #endif
