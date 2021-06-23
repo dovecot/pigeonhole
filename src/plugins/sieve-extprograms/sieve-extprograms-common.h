19f650e114b9af28c0a47fcf0b494b40b5fd9bcf
Stephan Bosch <stephan.bosch@open-xchange.com>
2021-06-23 02:33:21 +0200

    plugins: sieve-extprograms: Reformat sieve-extprograms-common.h

--- src/plugins/sieve-extprograms/sieve-extprograms-common.h
+++ src/plugins/sieve-extprograms/sieve-extprograms-common.h
@@ -24,10 +24,10 @@ struct sieve_extprograms_config {
 	unsigned int execute_timeout;
 };
 
-struct sieve_extprograms_config *sieve_extprograms_config_init
-	(const struct sieve_extension *ext);
-void sieve_extprograms_config_deinit
-	(struct sieve_extprograms_config **ext_config);
+struct sieve_extprograms_config *
+sieve_extprograms_config_init(const struct sieve_extension *ext);
+void sieve_extprograms_config_deinit(
+	struct sieve_extprograms_config **ext_config);
 
 /*
  * Extensions
@@ -37,8 +37,8 @@ extern const struct sieve_extension_def sieve_ext_vnd_pipe;
 extern const struct sieve_extension_def sieve_ext_vnd_filter;
 extern const struct sieve_extension_def sieve_ext_vnd_execute;
 
-/* 
- * Commands 
+/*
+ * Commands
  */
 
 extern const struct sieve_command_def sieve_cmd_pipe;
@@ -64,43 +64,43 @@ bool sieve_extprogram_name_is_valid(string_t *name);
  * Command validation
  */
 
-bool sieve_extprogram_command_validate
-	(struct sieve_validator *valdtr, struct sieve_command *cmd);
+bool sieve_extprogram_command_validate(struct sieve_validator *valdtr,
+				       struct sieve_command *cmd);
 
 /*
  * Common command operands
  */
 
-int sieve_extprogram_command_read_operands
-	(const struct sieve_runtime_env *renv, sieve_size_t *address,
-		string_t **pname_r, struct sieve_stringlist **args_list_r);
+int sieve_extprogram_command_read_operands(
+	const struct sieve_runtime_env *renv, sieve_size_t *address,
+	string_t **pname_r, struct sieve_stringlist **args_list_r);
 
 /*
  * Running external programs
  */
 
-void sieve_extprogram_exec_error
-	(struct sieve_error_handler *ehandler, const char *location,
-		const char *fmt, ...) ATTR_FORMAT(3, 4);
+void sieve_extprogram_exec_error(struct sieve_error_handler *ehandler,
+				 const char *location, const char *fmt, ...)
+				 ATTR_FORMAT(3, 4);
 
-struct sieve_extprogram *sieve_extprogram_create
-	(const struct sieve_extension *ext, const struct sieve_script_env *senv,
-		const struct sieve_message_data *msgdata, const char *action,
-		const char *program_name, const char * const *args,
-		enum sieve_error *error_r);
+struct sieve_extprogram *
+sieve_extprogram_create(const struct sieve_extension *ext,
+			const struct sieve_script_env *senv,
+			const struct sieve_message_data *msgdata,
+			const char *action, const char *program_name,
+			const char *const *args, enum sieve_error *error_r);
 void sieve_extprogram_destroy(struct sieve_extprogram **_sprog);
 
-void sieve_extprogram_set_output
-	(struct sieve_extprogram *sprog, struct ostream *output);
-void sieve_extprogram_set_output_seekable
-	(struct sieve_extprogram *sprog);
-struct istream *sieve_extprogram_get_output_seekable
-	(struct sieve_extprogram *sprog);
+void sieve_extprogram_set_output(struct sieve_extprogram *sprog,
+				 struct ostream *output);
+void sieve_extprogram_set_output_seekable(struct sieve_extprogram *sprog);
+struct istream *sieve_extprogram_get_output_seekable(
+	struct sieve_extprogram *sprog);
 
-void sieve_extprogram_set_input
-	(struct sieve_extprogram *sprog, struct istream *input);
-int sieve_extprogram_set_input_mail
-	(struct sieve_extprogram *sprog, struct mail *mail);
+void sieve_extprogram_set_input(struct sieve_extprogram *sprog,
+				struct istream *input);
+int sieve_extprogram_set_input_mail(struct sieve_extprogram *sprog,
+				    struct mail *mail);
 
 int sieve_extprogram_run(struct sieve_extprogram *sprog);
 
