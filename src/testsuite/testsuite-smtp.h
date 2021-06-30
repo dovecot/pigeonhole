c48172b899d602d23f01b98e764a2820eb662aa3
Stephan Bosch <stephan.bosch@open-xchange.com>
2021-06-30 14:16:04 +0200

    testsuite: Reformat testsuite-smtp.h

--- src/testsuite/testsuite-smtp.h
+++ src/testsuite/testsuite-smtp.h
@@ -9,27 +9,23 @@ void testsuite_smtp_reset(void);
  * Simulated SMTP out
  */
 
-void *testsuite_smtp_start
-	(const struct sieve_script_env *senv ATTR_UNUSED,
-		const struct smtp_address *mail_from);
-void testsuite_smtp_add_rcpt
-	(const struct sieve_script_env *senv ATTR_UNUSED,
-		void *handle, const struct smtp_address *rcpt_to);
-struct ostream *testsuite_smtp_send
-	(const struct sieve_script_env *senv ATTR_UNUSED,
-		void *handle);
-void testsuite_smtp_abort
-	(const struct sieve_script_env *senv ATTR_UNUSED,
-		void *handle);
-int testsuite_smtp_finish
-	(const struct sieve_script_env *senv ATTR_UNUSED,
-		void *handle, const char **error_r);
+void *testsuite_smtp_start(const struct sieve_script_env *senv ATTR_UNUSED,
+			   const struct smtp_address *mail_from);
+void testsuite_smtp_add_rcpt(const struct sieve_script_env *senv ATTR_UNUSED,
+			     void *handle, const struct smtp_address *rcpt_to);
+struct ostream *
+testsuite_smtp_send(const struct sieve_script_env *senv ATTR_UNUSED,
+		    void *handle);
+void testsuite_smtp_abort(const struct sieve_script_env *senv ATTR_UNUSED,
+			  void *handle);
+int testsuite_smtp_finish(const struct sieve_script_env *senv ATTR_UNUSED,
+			  void *handle, const char **error_r);
 
 /*
  * Access
  */
 
-bool testsuite_smtp_get
-	(const struct sieve_runtime_env *renv, unsigned int index);
+bool testsuite_smtp_get(const struct sieve_runtime_env *renv,
+			unsigned int index);
 
 #endif
