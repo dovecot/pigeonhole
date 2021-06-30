39d9ec425123d09ad62ba8b238e6d0fb6b84d2f9
Stephan Bosch <stephan.bosch@open-xchange.com>
2021-06-30 13:53:19 +0200

    testsuite: Reformat testsuite-arguments.c

--- src/testsuite/testsuite-arguments.c
+++ src/testsuite/testsuite-arguments.c
@@ -23,9 +23,10 @@
  * Testsuite string argument
  */
 
-static bool arg_testsuite_string_validate
-	(struct sieve_validator *validator, struct sieve_ast_argument **arg,
-		struct sieve_command *context);
+static bool
+arg_testsuite_string_validate(struct sieve_validator *validator,
+			      struct sieve_ast_argument **arg,
+			      struct sieve_command *context);
 
 const struct sieve_argument_def testsuite_string_argument = {
 	.identifier = "@testsuite-string",
@@ -33,9 +34,10 @@ const struct sieve_argument_def testsuite_string_argument = {
 	.generate = sieve_arg_catenated_string_generate,
 };
 
-static bool arg_testsuite_string_validate
-(struct sieve_validator *valdtr, struct sieve_ast_argument **arg,
-	struct sieve_command *cmd)
+static bool
+arg_testsuite_string_validate(struct sieve_validator *valdtr,
+			      struct sieve_ast_argument **arg,
+			      struct sieve_command *cmd)
 {
 	enum { ST_NONE, ST_OPEN, ST_SUBSTITUTION, ST_PARAM, ST_CLOSE } state =
 		ST_NONE;
@@ -54,12 +56,11 @@ static bool arg_testsuite_string_validate
 
 		p = strval;
 		strstart = p;
-		while ( result && p < strend ) {
-			switch ( state ) {
-
+		while (result && p < strend) {
+			switch (state) {
 			/* Nothing found yet */
 			case ST_NONE:
-				if ( *p == '%' ) {
+				if (*p == '%') {
 					substart = p;
 					state = ST_OPEN;
 					str_truncate(subs_name, 0);
@@ -67,10 +68,9 @@ static bool arg_testsuite_string_validate
 				}
 				p++;
 				break;
-
 			/* Got '%' */
 			case ST_OPEN:
-				if ( *p == '{' ) {
+				if (*p == '{') {
 					state = ST_SUBSTITUTION;
 					p++;
 				} else
@@ -81,8 +81,8 @@ static bool arg_testsuite_string_validate
 			case ST_SUBSTITUTION:
 				state = ST_PARAM;
 
-				while ( *p != '}' && *p != ':' ) {
-					if ( !i_isalnum(*p) ) {
+				while (*p != '}' && *p != ':') {
+					if (!i_isalnum(*p)) {
 						state = ST_NONE;
 						break;
 					}
@@ -90,63 +90,61 @@ static bool arg_testsuite_string_validate
 					p++;
 				}
 				break;
-
 			/* Got '%{name' */
 			case ST_PARAM:
-				if ( *p == ':' ) {
+				if (*p == ':') {
 					p++;
-					while ( *p != '}' ) {
+					while (*p != '}') {
 						str_append_c(subs_param, *p);
 						p++;
 					}
 				}
 				state = ST_CLOSE;
 				break;
-
 			/* Finished parsing param, expecting '}' */
 			case ST_CLOSE:
-				if ( *p == '}' ) {
+				if (*p == '}') {
 					struct sieve_ast_argument *strarg;
 
 					/* We now know that the substitution is valid */
 
-					if ( catstr == NULL ) {
+					if (catstr == NULL)
 						catstr = sieve_arg_catenated_string_create(*arg);
-					}
 
 					/* Add the substring that is before the substitution to the
-					 * variable-string AST.
+					   variable-string AST.
 					 */
-					if ( substart > strstart ) {
+					if (substart > strstart) {
 						string_t *newstr = str_new(pool, substart - strstart);
 						str_append_data(newstr, strstart, substart - strstart);
 
-						strarg = sieve_ast_argument_string_create_raw
-							((*arg)->ast, newstr, (*arg)->source_line);
+						strarg = sieve_ast_argument_string_create_raw(
+							(*arg)->ast, newstr, (*arg)->source_line);
 						sieve_arg_catenated_string_add_element(catstr, strarg);
 
-						/* Give other substitution extensions a chance to do their work */
-						if ( !sieve_validator_argument_activate_super
-							(valdtr, cmd, strarg, FALSE) ) {
+						/* Give other substitution extensions a chance to do
+						   their work */
+						if (!sieve_validator_argument_activate_super(
+							valdtr, cmd, strarg, FALSE)) {
 							result = FALSE;
 							break;
 						}
 					}
 
-					strarg = testsuite_substitution_argument_create
-						(valdtr, (*arg)->ast, (*arg)->source_line, str_c(subs_name),
-							str_c(subs_param));
-
-					if ( strarg != NULL )
+					strarg = testsuite_substitution_argument_create(
+						valdtr, (*arg)->ast, (*arg)->source_line,
+						str_c(subs_name), str_c(subs_param));
+					if (strarg != NULL)
 						sieve_arg_catenated_string_add_element(catstr, strarg);
 					else {
-						sieve_argument_validate_error(valdtr, *arg,
-							"unknown testsuite substitution type '%s'", str_c(subs_name));
+						sieve_argument_validate_error(
+							valdtr, *arg,
+							"unknown testsuite substitution type '%s'",
+							str_c(subs_name));
 					}
 
 					strstart = p + 1;
 					substart = strstart;
-
 					p++;
 				}
 
@@ -157,33 +155,35 @@ static bool arg_testsuite_string_validate
 	} T_END;
 
 	/* Bail out early if substitution is invalid */
-	if ( !result ) return FALSE;
+	if (!result)
+		return FALSE;
 
 	/* Check whether any substitutions were found */
-	if ( catstr == NULL ) {
-		/* No substitutions in this string, pass it on to any other substution
-		 * extension.
+	if (catstr == NULL) {
+		/* No substitutions in this string, pass it on to any other
+		   substution extension.
 		 */
-		return sieve_validator_argument_activate_super(valdtr, cmd, *arg, TRUE);
+		return sieve_validator_argument_activate_super(
+			valdtr, cmd, *arg, TRUE);
 	}
 
 	/* Add the final substring that comes after the last substitution to the
-	 * variable-string AST.
+	   variable-string AST.
 	 */
-	if ( strend > strstart ) {
+	if (strend > strstart) {
 		struct sieve_ast_argument *strarg;
 		string_t *newstr = str_new(pool, strend - strstart);
 		str_append_data(newstr, strstart, strend - strstart);
 
-		strarg = sieve_ast_argument_string_create_raw
-			((*arg)->ast, newstr, (*arg)->source_line);
+		strarg = sieve_ast_argument_string_create_raw(
+			(*arg)->ast, newstr, (*arg)->source_line);
 		sieve_arg_catenated_string_add_element(catstr, strarg);
 
-		/* Give other substitution extensions a chance to do their work */
-		if ( !sieve_validator_argument_activate_super
-			(valdtr, cmd, strarg, FALSE) )
+		/* Give other substitution extensions a chance to do their work.
+		 */
+		if (!sieve_validator_argument_activate_super(valdtr, cmd,
+							     strarg, FALSE))
 			return FALSE;
 	}
-
 	return TRUE;
 }
