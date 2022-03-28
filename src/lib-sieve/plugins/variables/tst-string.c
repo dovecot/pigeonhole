745eb64dd4bed57cfe3b2634d5f669d485155d83
Stephan Bosch <stephan.bosch@open-xchange.com>
2022-03-28 23:31:19 +0200

    lib-sieve: plugins: variables: tst-string - Fix default comparator to be "i;ascii-casemap".
    It was "i;octet" previously, which is not standard.

--- src/lib-sieve/plugins/variables/tst-string.c
+++ src/lib-sieve/plugins/variables/tst-string.c
@@ -96,7 +96,7 @@ static bool tst_string_validate
 	const struct sieve_match_type mcht_default =
 		SIEVE_MATCH_TYPE_DEFAULT(is_match_type);
 	const struct sieve_comparator cmp_default =
-		SIEVE_COMPARATOR_DEFAULT(i_octet_comparator);
+		SIEVE_COMPARATOR_DEFAULT(i_ascii_casemap_comparator);
 
 	if ( !sieve_validate_positional_argument
 		(valdtr, tst, arg, "source", 1, SAAT_STRING_LIST) ) {
@@ -230,7 +230,7 @@ static int tst_string_operation_execute
 	struct sieve_match_type mcht =
 		SIEVE_MATCH_TYPE_DEFAULT(is_match_type);
 	struct sieve_comparator cmp =
-		SIEVE_COMPARATOR_DEFAULT(i_octet_comparator);
+		SIEVE_COMPARATOR_DEFAULT(i_ascii_casemap_comparator);
 	struct sieve_stringlist *source, *value_list, *key_list;
 	int match, ret;
 
