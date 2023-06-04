2dd443179ded938c12e8faab06841a2dffcbd914
Timo Sirainen <timo.sirainen@open-xchange.com>
2023-06-05 01:22:33 +0300

    global: Adjust to default service settings changes

--- src/managesieve/managesieve-settings.c
+++ src/managesieve/managesieve-settings.c
@@ -30,12 +30,8 @@ struct service_settings managesieve_settings_service_settings = {
 
 	.drop_priv_before_exec = FALSE,
 
-	.process_min_avail = 0,
-	.process_limit = 0,
 	.client_limit = 1,
 	.service_count = 1,
-	.idle_kill = 0,
-	.vsz_limit = (uoff_t)-1,
 
 	.unix_listeners = ARRAY_INIT,
 	.fifo_listeners = ARRAY_INIT,
