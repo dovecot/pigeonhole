8c1aa3cd340813bf8fac6ecfdb8a9e7998737e37
Stephan Bosch <stephan.bosch@open-xchange.com>
2024-08-19 22:15:00 +0200

    plugins: settings: Expand settings plugin to collect all settings infos from lib-sieve and plugins

--- src/plugins/settings/pigeonhole-settings.c
+++ src/plugins/settings/pigeonhole-settings.c
@@ -5,8 +5,9 @@
 #include "pigeonhole-config.h"
 #include "pigeonhole-version.h"
 
-/* This is currently just a dummy plugin that adds a Pigeonhole version banner
-   the doveconf output.
+#include "pigeonhole-settings.h"
+
+/* This adds a Pigeonhole version banner the doveconf output.
  */
 
 const char *pigeonhole_settings_version = DOVECOT_ABI_VERSION;
