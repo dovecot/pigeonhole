ba89102403567394c991cfd1d01c0dd0277c4575
Stephan Bosch <stephan.bosch@open-xchange.com>
2024-09-28 21:29:04 +0200

    testsuite: Reformat testsuite-binary.c

--- src/testsuite/testsuite-binary.c
+++ src/testsuite/testsuite-binary.c
@@ -32,11 +32,11 @@ static char *testsuite_binary_tmp = NULL;
 
 void testsuite_binary_init(void)
 {
-	testsuite_binary_tmp = i_strconcat
-		(testsuite_tmp_dir_get(), "/binaries", NULL);
+	testsuite_binary_tmp = i_strconcat(testsuite_tmp_dir_get(),
+					   "/binaries", NULL);
 
-	if ( mkdir(testsuite_binary_tmp, 0700) < 0 ) {
-		i_fatal("failed to create temporary directory '%s': %m.",
+	if (mkdir(testsuite_binary_tmp, 0700) < 0) {
+		i_fatal("failed to create temporary directory '%s': %m",
 			testsuite_binary_tmp);
 	}
 }
@@ -45,9 +45,10 @@ void testsuite_binary_deinit(void)
 {
 	const char *error;
 
-	if ( unlink_directory(testsuite_binary_tmp, UNLINK_DIRECTORY_FLAG_RMDIR, &error) < 0 ) {
-		i_warning("failed to remove temporary directory '%s': %s.",
-			testsuite_binary_tmp, error);
+	if (unlink_directory(testsuite_binary_tmp, UNLINK_DIRECTORY_FLAG_RMDIR,
+			     &error) < 0) {
+		i_warning("failed to remove temporary directory '%s': %s",
+			  testsuite_binary_tmp, error);
 	}
 
 	i_free(testsuite_binary_tmp);
@@ -65,18 +66,17 @@ void testsuite_binary_reset(void)
 
 bool testsuite_binary_save(struct sieve_binary *sbin, const char *name)
 {
-	return ( sieve_save_as(sbin, t_strdup_printf
-		("%s/%s", testsuite_binary_tmp, sieve_binfile_from_name(name)), TRUE,
-			0600, NULL) > 0 );
+	return (sieve_save_as(sbin,
+			      t_strdup_printf("%s/%s", testsuite_binary_tmp,
+					      sieve_binfile_from_name(name)),
+			      TRUE, 0600, NULL) > 0);
 }
 
 struct sieve_binary *testsuite_binary_load(const char *name)
 {
 	struct sieve_instance *svinst = testsuite_sieve_instance;
 
-	return sieve_load(svinst, t_strdup_printf
-		("%s/%s", testsuite_binary_tmp, sieve_binfile_from_name(name)), NULL);
+	return sieve_load(svinst,
+			  t_strdup_printf("%s/%s", testsuite_binary_tmp,
+					  sieve_binfile_from_name(name)), NULL);
 }
-
-
-
