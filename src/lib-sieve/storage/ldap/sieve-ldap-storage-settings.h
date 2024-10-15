72e6aa17d6e3ee70e5a201e1550a822a1300b56d
Stephan Bosch <stephan.bosch@open-xchange.com>
2024-10-16 00:03:47 +0200

    lib-sieve: storage: ldap: Move struct sieve_ldap_storage_settings to sieve-ldap-storage-settings.h

---
+++ src/lib-sieve/storage/ldap/sieve-ldap-storage-settings.h
@@ -0,0 +1,39 @@
+#ifndef SIEVE_LDAP_STORAGE_SETTINGS_H
+#define SIEVE_LDAP_STORAGE_SETTINGS_H
+
+struct sieve_ldap_storage_settings {
+	const char *hosts;
+	const char *uris;
+	const char *dn;
+	const char *dnpass;
+
+	bool tls;
+	bool sasl_bind;
+	const char *sasl_mech;
+	const char *sasl_realm;
+	const char *sasl_authz_id;
+
+	const char *tls_ca_cert_file;
+	const char *tls_ca_cert_dir;
+	const char *tls_cert_file;
+	const char *tls_key_file;
+	const char *tls_cipher_suite;
+	const char *tls_require_cert;
+
+	const char *deref;
+	const char *scope;
+	const char *base;
+	unsigned int ldap_version;
+
+	const char *ldaprc_path;
+	const char *debug_level;
+
+	const char *sieve_ldap_script_attr;
+	const char *sieve_ldap_mod_attr;
+	const char *sieve_ldap_filter;
+
+	/* ... */
+	int ldap_deref, ldap_scope, ldap_tls_require_cert;
+};
+
+#endif
