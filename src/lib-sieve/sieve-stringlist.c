2a1a547e36883880c1d4bfbd1ed99cc696e13518
Stephan Bosch <stephan.bosch@open-xchange.com>
2024-11-09 05:59:29 +0100

    lib-sieve: Reformat sieve-stringlist.c

--- src/lib-sieve/sieve-stringlist.c
+++ src/lib-sieve/sieve-stringlist.c
@@ -12,11 +12,10 @@
  * Default implementation
  */
 
-int sieve_stringlist_read_all
-(struct sieve_stringlist *strlist, pool_t pool,
-	const char * const **list_r)
+int sieve_stringlist_read_all(struct sieve_stringlist *strlist, pool_t pool,
+			      const char *const **list_r)
 {
-	if ( strlist->read_all == NULL ) {
+	if (strlist->read_all == NULL) {
 		ARRAY(const char *) items;
 		string_t *item;
 		int ret;
@@ -26,7 +25,7 @@ int sieve_stringlist_read_all
 		p_array_init(&items, pool, 4);
 
 		item = NULL;
-		while ( (ret=sieve_stringlist_next_item(strlist, &item)) > 0 ) {
+		while ((ret = sieve_stringlist_next_item(strlist, &item)) > 0) {
 			const char *stritem = p_strdup(pool, str_c(item));
 
 			array_append(&items, &stritem, 1);
@@ -35,27 +34,26 @@ int sieve_stringlist_read_all
 		(void)array_append_space(&items);
 		*list_r = array_idx(&items, 0);
 
-		return ( ret < 0 ? -1 : 1 );
+		return (ret < 0 ? -1 : 1);
 	}
 
 	return strlist->read_all(strlist, pool, list_r);
 }
 
-int sieve_stringlist_get_length
-(struct sieve_stringlist *strlist)
+int sieve_stringlist_get_length(struct sieve_stringlist *strlist)
 {
-	if ( strlist->get_length == NULL ) {
+	if (strlist->get_length == NULL) {
 		string_t *item;
 		int count = 0;
 		int ret;
 
 		sieve_stringlist_reset(strlist);
-		while ( (ret=sieve_stringlist_next_item(strlist, &item)) > 0 ) {
+		while ((ret = sieve_stringlist_next_item(strlist, &item)) > 0)
 			count++;
-		}
+
 		sieve_stringlist_reset(strlist);
 
-		return ( ret < 0 ? -1 : count );
+		return (ret < 0 ? -1 : count);
 	}
 
 	return strlist->get_length(strlist);
@@ -67,12 +65,12 @@ int sieve_stringlist_get_length
 
 /* Object */
 
-static int sieve_single_stringlist_next_item
-	(struct sieve_stringlist *_strlist, string_t **str_r);
-static void sieve_single_stringlist_reset
-	(struct sieve_stringlist *_strlist);
-static int sieve_single_stringlist_get_length
-	(struct sieve_stringlist *_strlist);
+static int
+sieve_single_stringlist_next_item(struct sieve_stringlist *_strlist,
+				  string_t **str_r);
+static void sieve_single_stringlist_reset(struct sieve_stringlist *_strlist);
+static int
+sieve_single_stringlist_get_length(struct sieve_stringlist *_strlist);
 
 struct sieve_single_stringlist {
 	struct sieve_stringlist strlist;
@@ -83,8 +81,9 @@ struct sieve_single_stringlist {
 	bool count_empty:1;
 };
 
-struct sieve_stringlist *sieve_single_stringlist_create
-(const struct sieve_runtime_env *renv, string_t *str, bool count_empty)
+struct sieve_stringlist *
+sieve_single_stringlist_create(const struct sieve_runtime_env *renv,
+			       string_t *str, bool count_empty)
 {
 	struct sieve_single_stringlist *strlist;
 
@@ -100,8 +99,9 @@ struct sieve_stringlist *sieve_single_stringlist_create
 	return &strlist->strlist;
 }
 
-struct sieve_stringlist *sieve_single_stringlist_create_cstr
-(const struct sieve_runtime_env *renv, const char *cstr, bool count_empty)
+struct sieve_stringlist *
+sieve_single_stringlist_create_cstr(const struct sieve_runtime_env *renv,
+				    const char *cstr, bool count_empty)
 {
 	string_t *str = t_str_new_const(cstr, strlen(cstr));
 
@@ -110,13 +110,14 @@ struct sieve_stringlist *sieve_single_stringlist_create_cstr
 
 /* Implementation */
 
-static int sieve_single_stringlist_next_item
-(struct sieve_stringlist *_strlist, string_t **str_r)
+static int
+sieve_single_stringlist_next_item(struct sieve_stringlist *_strlist,
+				  string_t **str_r)
 {
 	struct sieve_single_stringlist *strlist =
 		(struct sieve_single_stringlist *)_strlist;
 
-	if ( strlist->end ) {
+	if (strlist->end) {
 		*str_r = NULL;
 		return 0;
 	}
@@ -126,8 +127,7 @@ static int sieve_single_stringlist_next_item
 	return 1;
 }
 
-static void sieve_single_stringlist_reset
-(struct sieve_stringlist *_strlist)
+static void sieve_single_stringlist_reset(struct sieve_stringlist *_strlist)
 {
 	struct sieve_single_stringlist *strlist =
 		(struct sieve_single_stringlist *)_strlist;
@@ -135,13 +135,12 @@ static void sieve_single_stringlist_reset
 	strlist->end = FALSE;
 }
 
-static int sieve_single_stringlist_get_length
-(struct sieve_stringlist *_strlist)
+static int sieve_single_stringlist_get_length(struct sieve_stringlist *_strlist)
 {
 	struct sieve_single_stringlist *strlist =
 		(struct sieve_single_stringlist *)_strlist;
 
-	return ( strlist->count_empty || str_len(strlist->value) > 0 ? 1 : 0 );
+	return (strlist->count_empty || str_len(strlist->value) > 0 ? 1 : 0);
 }
 
 /*
@@ -150,14 +149,13 @@ static int sieve_single_stringlist_get_length
 
 /* Object */
 
-static int sieve_index_stringlist_next_item
-	(struct sieve_stringlist *_strlist, string_t **str_r);
-static void sieve_index_stringlist_reset
-	(struct sieve_stringlist *_strlist);
-static int sieve_index_stringlist_get_length
-	(struct sieve_stringlist *_strlist);
-static void sieve_index_stringlist_set_trace
-	(struct sieve_stringlist *strlist, bool trace);
+static int
+sieve_index_stringlist_next_item(struct sieve_stringlist *_strlist,
+				 string_t **str_r);
+static void sieve_index_stringlist_reset(struct sieve_stringlist *_strlist);
+static int sieve_index_stringlist_get_length(struct sieve_stringlist *_strlist);
+static void
+sieve_index_stringlist_set_trace(struct sieve_stringlist *strlist, bool trace);
 
 struct sieve_index_stringlist {
 	struct sieve_stringlist strlist;
@@ -168,9 +166,9 @@ struct sieve_index_stringlist {
 	bool end:1;
 };
 
-struct sieve_stringlist *sieve_index_stringlist_create
-(const struct sieve_runtime_env *renv, struct sieve_stringlist *source,
-	int index)
+struct sieve_stringlist *
+sieve_index_stringlist_create(const struct sieve_runtime_env *renv,
+			      struct sieve_stringlist *source, int index)
 {
 	struct sieve_index_stringlist *strlist;
 
@@ -189,20 +187,22 @@ struct sieve_stringlist *sieve_index_stringlist_create
 
 /* Implementation */
 
-static int sieve_index_stringlist_next_item
-(struct sieve_stringlist *_strlist, string_t **str_r)
+static int
+sieve_index_stringlist_next_item(struct sieve_stringlist *_strlist,
+				 string_t **str_r)
 {
 	struct sieve_index_stringlist *strlist =
 		(struct sieve_index_stringlist *)_strlist;
 	int index, ret;
 
-	if ( strlist->end ) {
+	if (strlist->end) {
 		*str_r = NULL;
 		return 0;
 	}
 
-	if ( strlist->index < 0 ) {
+	if (strlist->index < 0) {
 		int len = sieve_stringlist_get_length(strlist->source);
+
 		if (len < 0) {
 			_strlist->exec_status = strlist->source->exec_status;
 			return -1;
@@ -219,22 +219,24 @@ static int sieve_index_stringlist_next_item
 	}
 
 	i_assert(index > 0);
-	while ( index > 0 ) {
-		if ( (ret=sieve_stringlist_next_item(strlist->source, str_r)) <= 0 ) {
-			if (ret < 0)
-				_strlist->exec_status = strlist->source->exec_status;
+	while (index > 0) {
+		ret = sieve_stringlist_next_item(strlist->source, str_r);
+		if (ret <= 0) {
+			if (ret < 0) {
+				_strlist->exec_status =
+					strlist->source->exec_status;
+			}
 			return ret;
 		}
-	
+
 		index--;
 	}
-		
+
 	strlist->end = TRUE;
 	return 1;
 }
 
-static void sieve_index_stringlist_reset
-(struct sieve_stringlist *_strlist)
+static void sieve_index_stringlist_reset(struct sieve_stringlist *_strlist)
 {
 	struct sieve_index_stringlist *strlist =
 		(struct sieve_index_stringlist *)_strlist;
@@ -243,8 +245,7 @@ static void sieve_index_stringlist_reset
 	strlist->end = FALSE;
 }
 
-static int sieve_index_stringlist_get_length
-(struct sieve_stringlist *_strlist)
+static int sieve_index_stringlist_get_length(struct sieve_stringlist *_strlist)
 {
 	struct sieve_index_stringlist *strlist =
 		(struct sieve_index_stringlist *)_strlist;
@@ -256,18 +257,18 @@ static int sieve_index_stringlist_get_length
 		return -1;
 	}
 
-	if ( strlist->index < 0 ) {
-		if ( -strlist->index >= len )
+	if (strlist->index < 0) {
+		if (-strlist->index >= len)
 			return 0;
-	} else if ( strlist->index >= len ) {
+	} else if (strlist->index >= len) {
 			return 0;
 	}
 
 	return 1;
 }
 
-static void sieve_index_stringlist_set_trace
-(struct sieve_stringlist *_strlist, bool trace)
+static void
+sieve_index_stringlist_set_trace(struct sieve_stringlist *_strlist, bool trace)
 {
 	struct sieve_index_stringlist *strlist =
 		(struct sieve_index_stringlist *)_strlist;
