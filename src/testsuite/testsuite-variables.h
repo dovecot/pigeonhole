152bafc29658fac7ab16b5e6d293b9c291896f2f
Stephan Bosch <stephan.bosch@open-xchange.com>
2024-11-09 16:02:50 +0100

    testsuite: Reformat testsuite-variables.h

--- src/testsuite/testsuite-variables.h
+++ src/testsuite/testsuite-variables.h
@@ -3,9 +3,7 @@
 
 extern const struct sieve_operand_def testsuite_namespace_operand;
 
-void testsuite_variables_init
-	(const struct sieve_extension *this_ext, struct sieve_validator *valdtr);
+void testsuite_variables_init(const struct sieve_extension *this_ext,
+			      struct sieve_validator *valdtr);
 
 #endif
-
-
