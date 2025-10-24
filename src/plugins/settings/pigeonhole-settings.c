3c4c9fc45935d438df67f3db66406dd0edc0e567
Timo Sirainen <timo.sirainen@open-xchange.com>
2025-10-24 16:01:26 +0300

    plugins: settings: Add support for tracking settings history

--- src/plugins/settings/pigeonhole-settings.c
+++ src/plugins/settings/pigeonhole-settings.c
@@ -2,13 +2,25 @@
  */
 
 #include "lib.h"
+#include "settings-history.h"
 #include "pigeonhole-config.h"
 #include "pigeonhole-version.h"
 
+#include "settings-history-pigeonhole.h"
 #include "pigeonhole-settings.h"
 
-/* This adds a Pigeonhole version banner the doveconf output.
- */
+/* This adds a Pigeonhole version banner the doveconf output and registers
+   settings history. */
+
+void pigeonhole_settings_init(void);
+
+void pigeonhole_settings_init(void)
+{
+	settings_history_register_renames(settings_history_pigeonhole_renames,
+		N_ELEMENTS(settings_history_pigeonhole_renames));
+	settings_history_register_defaults(settings_history_pigeonhole_defaults,
+		N_ELEMENTS(settings_history_pigeonhole_defaults));
+}
 
 const char *pigeonhole_settings_version = DOVECOT_ABI_VERSION;
 const char *pigeonhole_settings_doveconf_banner =
